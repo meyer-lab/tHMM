d46ae3cafbd477f7fc075eb9300cbe857ca6a040
Shakthi Visagan <shak360@gmail.com>
2018-10-11 15:05:14 -0700

    setting up new branch and work environment, creating cell class

---
+++ lineage/lineage-init/Cell.py
@@ -0,0 +1,17 @@
+# author : shakthi visagan (shak360), adam weiner (adamcweiner)
+# description: a file to hold the cell class
+
+class Cell:
+    def __init__(self, name)
+
+
+
+
+
+
+
+
+
+
+
+family1 = 
