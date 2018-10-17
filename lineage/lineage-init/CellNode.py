257f9eeaf498e2054b65a0c9ab4f6756d30f5476
Shakthi Visagan <shak360@gmail.com>
2018-10-16 21:16:13 -0700

    removing generate file to add under cell node class file, adding stop condition to generate

--- lineage/lineage-init/CellNode.py
+++ lineage/lineage-init/CellNode.py
@@ -3,6 +3,8 @@
 
 import sys
 import math
+import numpy as np
+import scipy.stats as sp
 
 class CellNode:
     def __init__(self, key, startT=0, endT=float('nan'), fate=True, left=None, right=None, parent=None):
@@ -77,3 +79,29 @@ class CellNode:
 
 
 
+
+def generate(numCells, locBern, cGom):
+    #create first cell
+    cell0 = CellNode(key=1, startT=0)
+    
+    # put first cell in list
+    out = [cell0]
+    
+    # have cell divide/die according to distribution
+    for cell in out:   # for all cells (cap at numCells)
+        if len(out) >= numCells:
+            break
+        if cell.isUnfinished():
+            cell.tau = sp.gompertz.rvs(cGom)
+            cell.endT = cell.startT + cell.tau
+            cell.fate = sp.bernoulli.rvs(locBern) # assign fate
+            if cell.fate == 1:
+                temp1, temp2 = cell.divide(cell.endT) # cell divides
+                # append to list
+                out.append(temp1)
+                out.append(temp2)
+            else:
+                cell.die(cell.endT)
+                
+    # return the list at end
+    return out
