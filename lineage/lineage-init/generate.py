42cdaa99c8eed523f77e2b67eded17c119c38e12
adamcweiner <adamcweiner@gmail.com>
2018-10-16 19:03:00 -0700

    bug seems to be with `isUnfinished`

--- lineage/lineage-init/generate.py
+++ lineage/lineage-init/generate.py
@@ -9,24 +9,29 @@ def generate(numCells, locBern, cGom, locGom):
     # put first cell in list
     out = [cell0]
     
-    print("length of out: " + str(len(out)))
-    
     # have cell divide/die according to distribution
-    for j in range(len(out)):   # for all cells (cap at numCells)
+    for j in range(0, len(out)):   # for all cells (cap at numCells)
+        print("j: " + str(j))
         if j == numCells:
+            print("breaking from numCells")
             break
         x = out[j]
+        print(x.isUnfinished())
         if x.isUnfinished():
+            print("unfinished")
             x.tau = sp.gompertz.rvs(cGom)
             x.endT = x.startT + x.tau
             x.fate = sp.bernoulli.rvs(locBern) # assign fate
+            print("assigned distributions")
             if x.fate == 1:
+                print("dividing")
                 temp1, temp2 = x.divide(x.endT) # cell divides
                 # append to list
                 out.append(temp1)
                 out.append(temp2)
             else:
                 x.die(x.endT)
+                print("dying")
                 
     # return the list at end
     return out
