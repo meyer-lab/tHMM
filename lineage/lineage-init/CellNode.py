a3cf1b2856dfceca53df219a0e234e9a3da3eeaa
Shakthi Visagan <shak360@gmail.com>
2018-10-18 02:47:34 -0700

    finalize plotting routine

--- lineage/lineage-init/CellNode.py
+++ lineage/lineage-init/CellNode.py
@@ -7,9 +7,10 @@ import numpy as np
 import scipy.stats as sp
 
 class CellNode:
-    def __init__(self, key, startT=0, endT=float('nan'), fate=True, left=None, right=None, parent=None):
+    def __init__(self, key, gen=1, startT=0, endT=float('nan'), fate=True, left=None, right=None, parent=None, plotVal=0):
         ''' Instantiates a cell node. Only requires a key '''
         self.key = key
+        self.gen = gen
         self.startT = startT
         self.endT = endT
         self.tau = self.endT - self.startT # avoiding self.t, since that is a common function (i.e. transposing matrices)
@@ -17,35 +18,35 @@ class CellNode:
         self.left = left
         self.right = right
         self.parent = parent
-    
-    def hasLeft(self):
-        return self.left
-
-    def hasRight(self):
-        return self.right
+        self.plotVal = plotVal
 
     def isLeft(self):
-        return self.parent and self.parent.left == self
+        if self.isRootParent():
+            isLeft = False
+        else:
+            isLeft = self.parent.left is self and self.parent.right is not self
+        return isLeft # is keyword checks whether two things are the same object, avoid for checking if two things are the same value
 
     def isRight(self):
-        return self.parent and self.parent.right == self
-
+        if self.isRootParent():
+            isRight = False
+        else:
+            isRight = self.parent.right is self and self.parent.left is not self
+        return isRight 
+    
     def isParent(self):
-        return not self.parent
+        return self.left.parent is self and self.right.parent is self
 
     def isChild(self):
-        return not (self.left or self.right)
-
-    def hasAnyChildren(self):
-        return self.right or self.left
-
-    def hasBothChildren(self):
-        return self.right and self.left
+        return self.parent.isParent()
+    
+    def isRootParent(self):
+        return (self.gen == 1 and self.parent is None)
 
     def calcTau(self):
         self.tau = self.endT - self.startT   # calculate tau here
-        if self.tau == float('nan'):
-            print("Warning: your cell lifetime {} is a non-positive number".format(self.tau))
+        if math.isnan(self.tau):
+            print("Warning: your cell lifetime {} is a nan".format(self.tau))
     
     def isUnfinished(self):
         return math.isnan(self.endT)   # returns true when cell is still alive
@@ -60,13 +61,16 @@ class CellNode:
         self.endT = endT
         self.fate = True   # division
 
-        # two daughter cells emerge at this time... not sure about 1st and 3rd arguments bc I don't know what "key" is and don't know what python's statement for "this" is.
-        # self is Python's way of denoting this, self can actually be any word, but common practice is to denote it as self
         # key is a binary number, basically if a parent's key is 1, then it's two daughters will have value 10 and 11
         # if a parent has key 11001, then it's two daughter's will have values 110010 and 110011
         
-        self.left = CellNode(key=(self.key<<1), startT=endT, parent=self)
-        self.right = CellNode(key=((self.key<<1)+1), startT=endT, parent=self)
+        if self.isRootParent():
+            self.left = CellNode(key=(self.key<<1), gen=self.gen+1, startT=endT, parent=self, plotVal=self.plotVal+0.5)
+            self.right = CellNode(key=((self.key<<1)+1), gen=self.gen+1, startT=endT, parent=self, plotVal=self.plotVal-0.5)
+        else:
+            self.left = CellNode(key=(self.key<<1), gen=self.gen+1, startT=endT, parent=self, plotVal=self.plotVal+(0.24**(self.gen))*2.49**(self.gen))
+            self.right = CellNode(key=((self.key<<1)+1), gen=self.gen+1, startT=endT, parent=self, plotVal=self.plotVal-(0.24**(self.gen))*2.49**(self.gen))
+            
         
         return (self.left, self.right)
 
