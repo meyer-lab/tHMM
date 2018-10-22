37688d06de1292398d3473ebbc5138a9a5e38eea
adamcweiner <adamcweiner@gmail.com>
2018-10-22 16:58:54 -0700

    start adding to divide test

--- lineage/tests/test_cell.py
+++ lineage/tests/test_cell.py
@@ -8,7 +8,7 @@ class TestModel(unittest.TestCase):
 
     def test_lifetime(self):
         """ Make sure cell lifetime variables behave properly. """
-        cell1 = c(key=0, startT=20.)
+        cell1 = c(key=0, startT=20)
         
         # nan before setting death time
         self.assertTrue(math.isnan(cell1.endT))
@@ -16,12 +16,24 @@ class TestModel(unittest.TestCase):
         self.assertTrue(cell1.isUnfinished())
         
         # correct life span after setting endT
-        cell1.endT = 500.
-        self.assertTrue(cell1.endT == 500.)
+        cell1.endT = 500
+        self.assertTrue(cell1.endT == 500)
         cell1.calcTau()
-        self.assertTrue(cell1.tau == 480.)
-        self.assertFalse(cell1.isUnfinished())
+        self.assertTrue(cell1.tau == 480)
+        self.assertFalse(cell1.isUnfinished()) # cell is dead
 
     def test_divide(self):
         """ Make sure cells divide properly with proper parent/child member variables. """
+        cell1 = c(key=0, startT=20)
+        cell2, cell3 = cell1.divide(40)
+        
+        self.assertFalse(cell1.isUnfinished()) # cell is dead
+        
+        # left and right children exist for cell1 with proper linking
+        self.assertTrue(cell1.left == cell2)
+        self.assertTrue(cell1.right == cell3)
+        self.assertTrue(cell2.parent == cell1)
+        self.assertTrue(cell3.parent == cell1)
+        
+        
         
