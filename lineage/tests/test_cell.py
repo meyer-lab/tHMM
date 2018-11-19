91bc5ce0eae4b9949bb1e8c55f8d4e83f9116115
adamcweiner <adamcweiner@gmail.com>
2018-11-19 11:51:26 -0800

    increase num back to 15

--- lineage/tests/test_cell.py
+++ lineage/tests/test_cell.py
@@ -136,7 +136,7 @@ class TestModel(unittest.TestCase):
 
     def test_doubleT(self):
         """ Check that cells double quicker when they always divide. """
-        num = 5
+        num = 15
         dt_fast = np.zeros((num))
         dt_slow = dt_fast.copy()
 
