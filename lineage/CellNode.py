7e1c49c4f612e5862f9fb6bd6f2360cedc77b428
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2018-11-26 23:03:40 -0800

    Doubling time (#22)
    * add argument for initial number of cells to generate()
    
    * change all unit tests to start with 1 cell
    
    * test for multiple cells
    
    * add function for doubling time
    
    * cells almost never double when bern=0.5
    
    * remove conflicts in unit tests
    
    * populations cannot access individual cells
    
    * re-work skeleton
    
    * shouldn't be iterating through each lineage seperately
    
    * add comments
    
    * working copy of function exists
    
    * more informative print statements in test
    
    * look at stdev and mean for several doubleT args
    
    * reduce unit test to ~6 sec
    
    * remove trailing whitespace and add docstrings
    
    * more linting
    
    * Update test_cell.py
    
    Removing testing for variance of doubling time of certain division being greater than the variance of 0.6 chance of division.
    
    * two cells fail per lineage
    
    * increase num back to 15
    
    * there's a bug
    
    * different print statement verifies that we're doubling the cell time
    
    * re-arrange if statements for generateLineageWithTime
    
    * control initCells in generateLineageWithTime
    
    * make calls to GenerateLinWithTime compatible
    
    * seems like cells are growing logistically
    
    * psuedocode for rest of doubleTime
    
    * store counts from multiple lineages
    
    * all exponenial fitting code
    
    * ~i think~ everything works but unit tests take too long to run
    
    * add print statements and reduce init to 100
    
    * more reasonable parameters reduce test time to ~7 sec
    
    * change generatePopulationWithTime
    
    * delete generateLineageWithTime
    
    * delete lineage class
    
    * delete plotting code from doublingTime
    
    * starting to edit unit tests
    
    * more reasonable parameters
    
    * add new test for bernoulli

--- lineage/CellNode.py
+++ lineage/CellNode.py
@@ -4,6 +4,9 @@
 import sys
 import math
 import scipy.stats as sp
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
 class CellNode:
     def __init__(self, gen=1, startT=0, endT=float('nan'), fate=None, left=None, right=None, parent=None, plotVal=0):
@@ -19,22 +22,33 @@ class CellNode:
         self.plotVal = plotVal
 
     def isParent(self):
+        """ Return the parent of the current cell. """
         return self.left.parent is self and self.right.parent is self
 
     def isChild(self):
+        """ Returns true if this cell has a known parent. """
         return self.parent.isParent()
-    
+
     def isRootParent(self):
+        """Returns true if this cell has no documented parent. """
         return (self.gen == 1 and self.parent is None)
 
     def calcTau(self):
+        """ Find the cell's lifetime. """
         self.tau = self.endT - self.startT   # calculate tau here
         if math.isnan(self.tau):
             print("Warning: your cell lifetime {} is a nan".format(self.tau))
 
     def isUnfinished(self):
+        """ See if the cell is living or has already died/divided. """
         return math.isnan(self.endT) and self.fate is None   # returns true when cell is still alive
 
+    def setUnfinished(self):
+        """ Set a finished cell back to being unfinished. """
+        self.endT = float('nan')
+        self.fate = None
+        self.tau = float('nan')
+    
     def die(self, endT):
         """ Cell dies without dividing. """
         self.fate = False   # no division
@@ -56,57 +70,58 @@ class CellNode:
 
         return (self.left, self.right)
 
-
-def generateLineageWithNum(numCells, locBern, cGom, scaleGom):
-    ''' generates list given a maximum number of cells, a Bernoulli parameter for dividing/dying and a Gompertz parameter for cell lifetime'''
-    #create first cell
-    cell0 = CellNode(startT=0)
-    
-    # put first cell in list
-    lineage = [cell0]
-    
-    # have cell divide/die according to distribution
-    for cell in lineage:   # for all cells (cap at numCells)
-        if len(lineage) >= numCells:
-            break
-        if cell.isUnfinished():
-            cell.tau = sp.gompertz.rvs(cGom, scale=scaleGom)
-            cell.endT = cell.startT + cell.tau
-            cell.fate = sp.bernoulli.rvs(locBern) # assign fate
-            if cell.fate:
-                temp1, temp2 = cell.divide(cell.endT) # cell divides
-                # append to list
-                lineage.append(temp1)
-                lineage.append(temp2)
-            else:
-                cell.die(cell.endT)
-                
-    # return the list at end
-    return lineage
-
-def generateLineageWithTime(experimentTime, locBern, cGom, scaleGom):
+def generateLineageWithTime(initCells, experimentTime, locBern, cGom, scaleGom):
     ''' generates list given an experimental end time, a Bernoulli parameter for dividing/dying and a Gompertz parameter for cell lifetime'''
-    #create first cell
-    cell0 = CellNode(startT=0)
-    
-    # put first cell in list
-    lineage = [cell0]
-    
+    #create an empty lineage
+    lineage = []
+
+    # initialize the list with cells
+    for ii in range(initCells):
+        lineage.append(CellNode(startT=0))
+
     # have cell divide/die according to distribution
     for cell in lineage:   # for all cells (cap at numCells)
         if cell.isUnfinished():
             cell.tau = sp.gompertz.rvs(cGom, scale=scaleGom)
             cell.endT = cell.startT + cell.tau
-            cell.fate = sp.bernoulli.rvs(locBern) # assign fate
-            if cell.endT >= experimentTime:
-                break
-            if cell.fate:
-                temp1, temp2 = cell.divide(cell.endT) # cell divides
-                # append to list
-                lineage.append(temp1)
-                lineage.append(temp2)
-            else:
-                cell.die(cell.endT)
-    
+            if cell.endT < experimentTime: # determine fate only if endT is within range
+                cell.fate = sp.bernoulli.rvs(locBern) # assign fate
+                if cell.fate:
+                    temp1, temp2 = cell.divide(cell.endT) # cell divides
+                    # append to list
+                    lineage.append(temp1)
+                    lineage.append(temp2)
+                else:
+                    cell.die(cell.endT)
+            else: # if the endT is past the experimentTime
+                cell.setUnfinished() # reset cell to be unfinished and move to next cell
+
     # return the list at end
     return lineage
+
+def doublingTime(initCells, locBern, cGom, scaleGom):
+    ''' calculates the doubling time of a homogeneous cell population given the three parameters and an initial cell count. '''
+    numAlive = [] # list that stores the number of alive cells for each experimentTime
+    experimentTimes = np.logspace(start=0, stop=2, num=49)
+    experimentTimes = [0] + experimentTimes
+
+    for experimentTime in experimentTimes:
+        lineage = generateLineageWithTime(initCells, experimentTime, locBern, cGom, scaleGom)
+        count = 0
+        for cell in lineage:
+            if cell.isUnfinished():
+                count += 1
+        numAlive.append(count)
+
+    # Fit to exponential curve and find exponential coefficient. 
+    def expFunc(experimentTimes, *expParam):
+        """ Calculates the exponential."""
+        return(initCells * np.exp(expParam[0] * experimentTimes))
+
+    expY = lambda experimentTimes, expParam: expFunc(experimentTimes, expParam)
+
+    fitExpParam, _ = curve_fit(expY, experimentTimes, numAlive, p0=[0]) # fit an exponential curve to generated data
+
+    doubleT = np.log(2) / fitExpParam[0] # relationship between doubling time and exponential function
+
+    return doubleT
