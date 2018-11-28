4abc94856c4f06720dbfbcad38c934a62c59b0ca
Shakthi Visagan <shak360@gmail.com>
2018-11-27 21:12:15 -0800

    need to investigate what it means to be finished

--- lineage/CellNode.py
+++ lineage/CellNode.py
@@ -46,6 +46,8 @@ class CellNode:
 
     def isUnfinished(self):
         """ See if the cell is living or has already died/divided. """
+        # is this the right way to go about doing this
+        # we might have cells with an endT but no fate?
         return math.isnan(self.endT) and self.fate is None   # returns true when cell is still alive
 
     def setUnfinished(self):
