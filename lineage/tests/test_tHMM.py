ab5567cc028829726eddb7bd9e98c7a50c84a624
adamcweiner <adamcweiner@gmail.com>
2019-01-15 08:47:41 -0800

    clean test

--- lineage/tests/test_tHMM.py
+++ lineage/tests/test_tHMM.py
@@ -8,26 +8,19 @@ from ..tHMM_start import tHMM as t, remove_NaNs
 class TestModel(unittest.TestCase):
     """ Here are the unit tests. """
     def test_remove_NaNs(self):
-        """ Checks to see that cells with a NaN of tau are eliminated from the list. """
+        """ Checks to see that cells with a NaN of tau are eliminated from a population list. """
         experimentTime = 100.
         initCells = [50, 50]
         locBern = [0.6, 0.8]
         cGom = [2, 0.5]
         scaleGom = [40, 50]
-        X = gpt(experimentTime, initCells, locBern, cGom, scaleGom)
+        X = gpt(experimentTime, initCells, locBern, cGom, scaleGom) # generate a population
+        X = remove_NaNs(X) # remove unfinished cells
         num_NAN = 0
         for cell in X:
             if cell.isUnfinished():
                 num_NAN += 1
-        print("Number of initial NANs: " + str(num_NAN))
 
-        Y = remove_NaNs(X)
-        num_NAN = 0
-        for cell in Y:
-            if cell.isUnfinished():
-                num_NAN += 1
-        print("Number of NANs left at end: " + str(num_NAN))
-
-        self.assertTrue(num_NAN == 0)
+        self.assertTrue(num_NAN == 0) # there should be no unfinished cells left
                 
         
