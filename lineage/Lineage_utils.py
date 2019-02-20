4113b0bd7465c8636eef21701f3700d48586091d
adamcweiner <adamcweiner@gmail.com>
2019-02-19 19:31:26 -0800

    just needed to add a comment

--- lineage/Lineage_utils.py
+++ lineage/Lineage_utils.py
@@ -156,7 +156,7 @@ def gompertzAnalytical(X):
         """ Returns the square root of the squared error between left and right terms. """
         return np.sqrt((left_term(b) - right_term(b))**2)
 
-    res = minimize(error_b, x0=(1./40.), bounds=(0,10), method="SLSQP", options={'maxiter': 1e7})   
+    res = minimize(error_b, x0=[(1./40.)], bounds=((0,10),), method="SLSQP", options={'maxiter': 1e7})   
     b = res.x
     # solve for a in terms of b
     a = b / ((help_exp(b) / n) - 1.0)
