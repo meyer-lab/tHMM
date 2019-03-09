07372c1daf735efb1095ead3546ac6a5b39ccca6
Nikan <nikan.namiri@gmail.com>
2019-03-08 20:39:54 -0800

    making the lineage plotter into one function

--- lineage/plotting_utils.py
+++ lineage/plotting_utils.py
@@ -2,9 +2,96 @@
 
 import networkx as nx
 
+def make_colormap_graph(X, X_like):
+    '''Takes in a list of cells, and then outputs a color_map list and a list of cell indices'''
+    G = nx.Graph()
+    color_map = []
+    for cell in X:
+        if X_like = None:
+            plotter = cell.true_state
+        else:
+            plotter = X.index(cell)
+
+        if plotter:
+            color_map.append('red')
+        else:
+            color_map.append('green')
+        cell_idx = X.index(cell)
+        G.add_node(cell_idx)
+        parent_cell_idx = None
+        if cell_idx == 0:
+            pass
+        else:
+            parent_cell_idx = X.index(cell.parent)
+            G.add_edge(parent_cell_idx, cell_idx)
+    return(G, color_map)
+
+def lineageProb2Graph(X, prob, state, scale=300):
+    '''
+        Plots an object (list) that has the same shape as the original lineage of cells
+        where the values are probabilities between 0 and 1. Usually, this can come from any
+        of the several values calculated in the upward or downward recursions like betas, gammas, or
+        stored in the tHMM class like MSD, EL, or used in Viterbi, like deltas.
+
+        Example usage: (X is a lineage, tHMMobj.EL[0][:,0] for the prob argument are likelihoods, state 0 is being used
+        and scale is to appropriately size the nodes)
+
+        G, cmap, node_size_map = lineageProb2Graph(X,tHMMobj.EL[0][:,0],0, scale=150)
+        pos=graphviz_layout(G, prog='dot')
+        plt.figure(figsize=(31,10))
+        nx.draw(G, pos, node_color = cmap, node_size=node_size_map)
+
+    '''
+    G = nx.Graph()
+    color_map = []
+    node_size_map = []
+    for cell in X:
+        cell_idx = X.index(cell)
+        if state:
+            color_map.append('red')
+        else:
+            color_map.append('green')
+        node_size_map.append(prob[cell_idx]*scale)
+        G.add_node(cell_idx)
+        parent_cell_idx = None
+        if cell_idx == 0:
+            pass
+        else:
+            parent_cell_idx = X.index(cell.parent)
+            G.add_edge(parent_cell_idx, cell_idx)
+    return(G, color_map, node_size_map)
+
+def make_colormap_graph(X, X_like, prob, state):
+    '''Takes in a list of cells, and then outputs a color_map list and a list of cell indices'''
+    G = nx.Graph()
+    color_map = []
+    node_size_map = []
+    for cell in X:
+        if prob == not None and state == not None and X_like == None: #plot cell true state
+            plotter = cell.true_state
+        elif X_like == not None: #plot viterbi
+            plotter = X_like[cell_idx]
+        elif X_like == None and prob == None and state == None: #plot likelihoods
+            plotter = state
+
+        if plotter:
+            color_map.append('red')
+        else:
+            color_map.append('green')
+        cell_idx = X.index(cell)
+        G.add_node(cell_idx)
+        parent_cell_idx = None
+        if cell_idx == 0:
+            pass
+        else:
+            parent_cell_idx = X.index(cell.parent)
+            G.add_edge(parent_cell_idx, cell_idx)
+    return(G, color_map)
+
+'''
 def lineage2Graph(X):
     '''
-        Plots a lineage of cells.
+    '''    Plots a lineage of cells.
 
         Example usage: (X is a lineage)
 
@@ -12,6 +99,7 @@ def lineage2Graph(X):
         pos=graphviz_layout(G, prog='dot')
         plt.figure(figsize=(31,10))
         nx.draw(G, pos, node_color = cmap)
+    '''
     '''
     G = nx.Graph()
     color_map = []
@@ -30,7 +118,10 @@ def lineage2Graph(X):
             G.add_edge(parent_cell_idx, cell_idx)
     return(G, color_map)
 
+
+
 def lineageLike2Graph(X, X_like):
+    '''
     '''
         Plots an object (list) that has the same shape as the original lineage of cells
         where the values represent states (usually all_states[i]).
@@ -41,6 +132,7 @@ def lineageLike2Graph(X, X_like):
         pos=graphviz_layout(G, prog='dot')
         plt.figure(figsize=(31,10))
         nx.draw(G, pos, node_color = cmap)
+    '''
     '''
     G = nx.Graph()
     color_map = []
@@ -58,7 +150,7 @@ def lineageLike2Graph(X, X_like):
             parent_cell_idx = X.index(cell.parent)
             G.add_edge(parent_cell_idx, cell_idx)
     return(G, color_map)
-
+'''
 def lineageProb2Graph(X, prob, state, scale=300):
     '''
         Plots an object (list) that has the same shape as the original lineage of cells
