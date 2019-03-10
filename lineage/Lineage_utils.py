12ff93d9f74625adc7711182645599dbe780e815
Shakthi Visagan <shak360@gmail.com>
2019-03-09 20:04:58 -0800

    removing weird print statement

--- lineage/Lineage_utils.py
+++ lineage/Lineage_utils.py
@@ -31,7 +31,6 @@ def generatePopulationWithTime(experimentTime, initCells, locBern, cGom, scaleGo
             if FOM == 'G':
                 temp = generateLineageWithTime(initCells[ii], experimentTime, locBern[ii], cGom[ii], scaleGom[ii], switchT, bern2[ii], cG2[ii], scaleG2[ii], FOM='G')
             elif FOM == 'E':
-                print("making a heterogeneous exponential lineage")
                 temp = generateLineageWithTime(initCells[ii], experimentTime, locBern[ii], cGom[ii], scaleGom[ii], switchT, bern2[ii], cG2[ii], scaleG2[ii], FOM='E', betaExp=betaExp[ii], betaExp2=betaExp2[ii])
             # create a temporary lineage
             for cell in temp:
