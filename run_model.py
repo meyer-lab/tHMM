20e1490919122ee1a058913cff06e31c349c0b0f
adamcweiner <adamcweiner@gmail.com>
2019-03-09 18:31:38 -0800

    bump

--- run_model.py
+++ run_model.py
@@ -1,6 +1,5 @@
 """ This file serves to import the cellular data from a CSV, convert it into the appropriate data types, and then run the tHMM on said data. To run this file from terminal for a given CSV file, use the command "python3 run_model.py data/[FILENAME].csv" """
 import sys
-import numpy as np
 from lineage.parse_csv import load_data, process
 
 data = load_data(sys.argv[1]) # i.e. 'data/2019_02_21_true.csv'
