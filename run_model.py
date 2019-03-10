c11dd8e86bdf0c1d37ca1aae572492bcd51f19bf
adamcweiner <adamcweiner@gmail.com>
2019-03-09 18:30:05 -0800

    clean up for PR

--- run_model.py
+++ run_model.py
@@ -4,40 +4,6 @@ import numpy as np
 from lineage.parse_csv import load_data, process
 
 data = load_data(sys.argv[1]) # i.e. 'data/2019_02_21_true.csv'
-print("true length of pop:", data.shape)
+print("true length of pop:", data.shape[0])
 pop1 = process(data, 10.0) # create a population of cells given the data and the imaging frequency
-found_IDs = list()
-for cell in pop1:
-    found_IDs.append(cell.trackID)
-    '''
-    print("\n")
-    print("trackID: " + str(cell.trackID))
-    print("linID: " + str(cell.linID))
-    print("true_state: " + str(cell.true_state))
-    print("gen: " + str(cell.gen))
-    print("startT: " + str(cell.startT))
-    print("endT: " + str(cell.endT))
-    print("tau: " + str(cell.tau))
-    print("fate: " + str(cell.fate))
-    if cell.left is not None:
-        print("left.trackID: " + str(cell.left.trackID))
-    if cell.right is not None:
-        print("right.trackID: " + str(cell.right.trackID))
-    if cell.parent is not None:
-        print("parent.trackID: " + str(cell.parent.trackID))
-'''
-print("length of pop found: " + str(len(pop1)))
-num_mult = 0
-true_num = data.shape[0] # the true number of cells
-print(true_num)
-num_IDs = np.zeros((true_num))
-for ID in found_IDs:
-    for ii in range(true_num):
-        if ID == data[ii, 0]:
-            num_IDs[ii] += 1
-            if num_IDs[ii] >= 2:
-                print("position", ii, "has multple matches")
-                print("cell ID:", data[ii, 0])
-                num_mult += 1
-
-print(num_mult)
+print("found pop length:", len(pop1))
