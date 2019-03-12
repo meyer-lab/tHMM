8c144ed9241c5eadaed49d960131259a4332d29c
adamcweiner <adamcweiner@gmail.com>
2019-03-11 19:53:59 -0700

    playing around with things

--- run_model.py
+++ run_model.py
@@ -4,7 +4,7 @@ from lineage.parse_csv import load_data, process
 from lineage.tHMM import tHMM
 from lineage.BaumWelch import fit
 from lineage.Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
-from lineage.plotting_utils import plot_experiments
+from lineage.plotting_utils import plot_experiments, plot_population
 
 data = load_data(sys.argv[1]) # i.e. 'data/2019_02_21_true.csv'
 print("true length of pop:", data.shape[0])
@@ -14,7 +14,11 @@ print("found pop length:", len(X))
 numStates = 2
 tHMMobj = tHMM(X, numStates=numStates, FOM='G') # build the tHMM class with X
 fdir = './Figures/'
+
 print("number of lineages =", len(tHMMobj.population))
+
+plot_population(tHMMobj.population, fdir+"exp_pop.png")
+"""
 for ii, lin in enumerate(tHMMobj.population):
     plot_experiments(lin, fdir+"lineage"+str(ii+1)+".png")
 
@@ -24,4 +28,4 @@ deltas, state_ptrs = get_leaf_deltas(tHMMobj) # gets the deltas matrix
 get_nonleaf_deltas(tHMMobj, deltas, state_ptrs)
 all_states = Viterbi(tHMMobj, deltas, state_ptrs)
 print(all_states)
-
+"""
