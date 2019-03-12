e3d75115d3a8ac0c475f92de19f2317f41137ace
adamcweiner <adamcweiner@gmail.com>
2019-03-11 17:12:39 -0700

    make all cells have a deathObserved member var

--- run_model.py
+++ run_model.py
@@ -1,8 +1,20 @@
 """ This file serves to import the cellular data from a CSV, convert it into the appropriate data types, and then run the tHMM on said data. To run this file from terminal for a given CSV file, use the command "python3 run_model.py data/[FILENAME].csv" """
 import sys
 from lineage.parse_csv import load_data, process
+from lineage.tHMM import tHMM
+from lineage.BaumWelch import fit
+from lineage.Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
 
 data = load_data(sys.argv[1]) # i.e. 'data/2019_02_21_true.csv'
 print("true length of pop:", data.shape[0])
-pop1 = process(data, 10.0) # create a population of cells given the data and the imaging frequency
-print("found pop length:", len(pop1))
+X = process(data, 10.0) # create a population of cells given the data and the imaging frequency
+print("found pop length:", len(X))
+
+numStates = 2
+tHMMobj = tHMM(X, numStates=numStates, FOM='G') # build the tHMM class with X
+tHMMobj, NF, betas, gammas = fit(tHMMobj, max_iter=100, verbose=False)
+
+deltas, state_ptrs = get_leaf_deltas(tHMMobj) # gets the deltas matrix
+get_nonleaf_deltas(tHMMobj, deltas, state_ptrs)
+all_states = Viterbi(tHMMobj, deltas, state_ptrs)
+print(all_states)
