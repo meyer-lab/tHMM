cd5506c323aa450a20bd33a76b179388723f93e0
Aaron Meyer <a@ameyer.me>
2019-03-26 13:49:18 -0700

    Add figure files.

---
+++ genFigures.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+import matplotlib
+matplotlib.use('AGG')
+
+fdir = './Manuscript/Figures/'
+
+
+if __name__ == '__main__':
+    nameOut = 'figure' + sys.argv[1]
+
+    exec('from ckine.figures import ' + nameOut)
+    ff = eval(nameOut + '.makeFigure()')
+    ff.savefig(fdir + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
+
+    print(nameOut + ' is done.')
