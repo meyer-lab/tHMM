2a0ea985edbd7d48551547f6990c4c29a4122598
Nikan <nikan.namiri@gmail.com>
2019-03-27 12:29:29 -0700

    linting

--- lineage/Analyze.py
+++ lineage/Analyze.py
@@ -2,17 +2,18 @@
 
 import numpy as np
 
-from BaumWelch import fit
-from Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
-from UpwardRecursion import get_leaf_Normalizing_Factors, get_leaf_betas, get_nonleaf_NF_and_betas, calculate_log_likelihood
-from tHMM import tHMM
+from .BaumWelch import fit
+from .Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
+from .UpwardRecursion import get_leaf_Normalizing_Factors, get_leaf_betas, get_nonleaf_NF_and_betas, calculate_log_likelihood
+from .tHMM import tHMM
 
 def Analyze(X, numStates, keepBern=True):
+    '''Runs a tHMM and outputs state classification from viterbi, thmm object, normalizing factor, log likelihood, and deltas'''
     run = True
-    while run == True:
+    while run:
         tHMMobj = tHMM(X, numStates=numStates, keepBern=True) # build the tHMM class with X
         fit(tHMMobj, max_iter=200, verbose=True)
-        if tHMMobj.paramlist[0]["E"][0,1] < 1000 and tHMMobj.paramlist[0]["E"][1,1] < 1000:
+        if tHMMobj.paramlist[0]["E"][0, 1] < 1000 and tHMMobj.paramlist[0]["E"][1, 1] < 1000:
             run = False
     deltas, state_ptrs = get_leaf_deltas(tHMMobj) # gets the deltas matrix
     get_nonleaf_deltas(tHMMobj, deltas, state_ptrs)
