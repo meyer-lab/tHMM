8b8793939a9853f626baae5ef5748037064f3af6
Nikan <nikan.namiri@gmail.com>
2019-03-27 19:47:20 -0700

    linting fig matplotlib

--- lineage/figures/Matplot_gen.py
+++ lineage/figures/Matplot_gen.py
@@ -5,44 +5,45 @@ import matplotlib
 import matplotlib.ticker
 matplotlib.use('Agg')
 
-def Matplot_gen(x,acc_h1,bern_MAS_h1,bern_2_h1,MASlocBern,locBern2,cGom_MAS_h1,cGom_2_h1,MAScGom,
-                cGom2,scaleGom_MAS_h1,scaleGom_2_h1,MASscaleGom,scaleGom2, xlabel, title, save_name):
-    
+def Matplot_gen(x, acc_h1, bern_MAS_h1, bern_2_h1, MASlocBern, locBern2, cGom_MAS_h1, cGom_2_h1, MAScGom, 
+                cGom2, scaleGom_MAS_h1, scaleGom_2_h1, MASscaleGom, scaleGom2, xlabel, title, save_name):
+    '''Creates 4 subpanles for model estimation'''
+
     fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True)
-    ax = axs[0,0]
-    ax.set_ylim(0,110)
-    l1 = ax.errorbar(x, acc_h1, fmt='o', c='b',marker="*",fillstyle='none', label = 'Accuracy')
+    ax = axs[0, 0]
+    ax.set_ylim(0, 110)
+    l1 = ax.errorbar(x, acc_h1, fmt='o', c='b', marker="*", fillstyle='none', label = 'Accuracy')
     ax.axhline(y=100, linestyle = (0, (3, 5, 1, 5, 1, 5)), linewidth=1, color='b',) #linestyle is dashdotdotted
-    ax.set_ylabel('Accuracy (%)',rotation=90)
+    ax.set_ylabel('Accuracy (%)', rotation=90)
     vals = ax.get_yticks()
 
-    ax = axs[0,1]
-    l2 = ax.errorbar(x, bern_MAS_h1, fmt='o', c='g',marker="^",fillstyle='none', label = 'State 1')
-    l3 = ax.errorbar(x, bern_2_h1, fmt='o', c='r',marker="^",fillstyle='none', label = 'State 2')
+    ax = axs[0, 1]
+    l2 = ax.errorbar(x, bern_MAS_h1, fmt='o', c='g', marker="^", fillstyle='none', label = 'State 1')
+    l3 = ax.errorbar(x, bern_2_h1, fmt='o', c='r', marker="^", fillstyle='none', label = 'State 2')
     ax.set_ylabel('Bernoulli', rotation=90)
     ax.axhline(y= MASlocBern, linestyle = (0, (3, 5, 1, 5, 1, 5)), linewidth=1, color='g')
     ax.axhline(y=locBern2, linestyle = (0, (3, 5, 1, 5, 1, 5)), linewidth=1, color='r')
 
-    ax = axs[1,0]
+    ax = axs[1, 0]
     ax.set_xlabel(xlabel)
     ax.set_xscale("log", nonposx='clip')
-    ax.errorbar(x,cGom_MAS_h1, fmt='o',c='g',marker="^",fillstyle='none', label = 'State 1')
-    ax.errorbar(x,cGom_2_h1, fmt='o',c='r',marker="^",fillstyle='none', label = 'State 2')
+    ax.errorbar(x, cGom_MAS_h1, fmt='o',c='g', marker="^", fillstyle='none', label = 'State 1')
+    ax.errorbar(x, cGom_2_h1, fmt='o',c='r', marker="^", fillstyle='none', label = 'State 2')
     ax.axhline(y=MAScGom, linestyle = (0, (3, 5, 1, 5, 1, 5)), linewidth=1, color='g')
     ax.axhline(y=cGom2, linestyle = (0, (3, 5, 1, 5, 1, 5)), linewidth=1, color='r')
-    ax.set_ylabel('Gompertz C',rotation=90)
+    ax.set_ylabel('Gompertz C', rotation=90)
     ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
     ax.get_xaxis().set_minor_formatter(matplotlib.ticker.NullFormatter())
 
 
-    ax = axs[1,1]
+    ax = axs[1, 1]
     ax.set_xlabel(xlabel)
     ax.set_xscale("log", nonposx='clip')
-    ax.errorbar(x,scaleGom_MAS_h1, fmt='o',c='g',marker="^",fillstyle='none', label = 'State 1')
-    ax.errorbar(x,scaleGom_2_h1, fmt='o',c='r',marker="^",fillstyle='none', label = 'State 2')
+    ax.errorbar(x, scaleGom_MAS_h1, fmt='o', c='g',marker="^", fillstyle='none', label = 'State 1')
+    ax.errorbar(x, scaleGom_2_h1, fmt='o', c='r',marker="^", fillstyle='none', label = 'State 2')
     ax.axhline(y=MASscaleGom, linestyle = (0, (3, 5, 1, 5, 1, 5)), linewidth=1, color='g')
     ax.axhline(y=scaleGom2, linestyle = (0, (3, 5, 1, 5, 1, 5)), linewidth=1, color='r')
-    ax.set_ylabel('Gompertz Scale',rotation=90)
+    ax.set_ylabel('Gompertz Scale', rotation=90)
     ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
     ax.get_xaxis().set_minor_formatter(matplotlib.ticker.NullFormatter())
 
