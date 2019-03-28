e9ec0a0e5eb793b9d341cc0a2fe77b0b05dd17d9
Nikan <nikan.namiri@gmail.com>
2019-03-28 10:07:49 -0700

    more linting

--- lineage/figures/Fig_Gen.py
+++ lineage/figures/Fig_Gen.py
@@ -7,7 +7,7 @@ from .Depth_Two_State_Lineage import Depth_Two_State_Lineage
 from ..Analyze import Analyze
 from .Matplot_gen import Matplot_gen
 
-from ..tHMM_utils.py import getAccuracy
+from ..tHMM_utils import getAccuracy
 from ..Lineage_utils import remove_NaNs
 
 def Lineage_Length(T_MAS=130, T_2=61, reps=20, MASinitCells=[1], MASlocBern=[0.8], MAScGom=[1.6], MASscaleGom=[40], initCells2=[1], locBern2=[0.99], cGom2=[1.6], scaleGom2=[18], numStates=2, max_lin_length=1500, min_lin_length=100, verbose=False):
@@ -46,7 +46,7 @@ def Lineage_Length(T_MAS=130, T_2=61, reps=20, MASinitCells=[1], MASlocBern=[0.8
             T = tHMMobj.paramlist[lin]["T"]
             E = tHMMobj.paramlist[lin]["E"]
             pi = tHMMobj.paramlist[lin]["pi"]
-                
+
             acc_h2.append(accuracy*100)
             cell_h2.append(len(lineage))
             bern_MAS_h2.append(E[state_1, 0])
@@ -70,9 +70,9 @@ def Lineage_Length(T_MAS=130, T_2=61, reps=20, MASinitCells=[1], MASlocBern=[0.8
         cGom_2_h1.extend(cGom_2_h2)
         scaleGom_MAS_h1.extend(scaleGom_MAS_h2)
         scaleGom_2_h1.extend(scaleGom_2_h2)
-    
+
     x = cell_h1
-    Matplot_gen(x, acc_h1, bern_MAS_h1, bern_2_h1, MASlocBern, locBern2, cGom_MAS_h1, cGom_2_h1, MAScGom,            cGom2, scaleGom_MAS_h1, scaleGom_2_h1, MASscaleGom, scaleGom2, xlabel='Number of Cells', title='Cells in a Lineage', save_name='Lineage_Length_Figure.png')
+    Matplot_gen(x, acc_h1, bern_MAS_h1, bern_2_h1, MASlocBern, locBern2, cGom_MAS_h1, cGom_2_h1, MAScGom, cGom2, scaleGom_MAS_h1, scaleGom_2_h1, MASscaleGom, scaleGom2, xlabel='Number of Cells', title='Cells in a Lineage', save_name='Lineage_Length_Figure.png')
     data = np.array([x,acc_h1,bern_MAS_h1,bern_2_h1,MASlocBern,locBern2,cGom_MAS_h1,cGom_2_h1,MAScGom,           cGom2,scaleGom_MAS_h1,scaleGom_2_h1,MASscaleGom,scaleGom2])
 
     return data
@@ -130,7 +130,7 @@ def Lineages_per_Population_Figure(lineage_start=1, lineage_end=2, reps=1, numSt
                 T = tHMMobj.paramlist[lin]["T"]
                 E = tHMMobj.paramlist[lin]["E"]
                 pi = tHMMobj.paramlist[lin]["pi"]
-            
+
                 acc_h3.append(accuracy)
                 cell_h3.append(len(lineage))
                 bern_MAS_h3.append(E[state_1, 0])
@@ -139,7 +139,7 @@ def Lineages_per_Population_Figure(lineage_start=1, lineage_end=2, reps=1, numSt
                 cGom_2_h3.append(E[state_2, 1])
                 scaleGom_MAS_h3.append(E[state_1, 2])
                 scaleGom_2_h3.append(E[state_2, 2])
-                
+
                 if verbose:
                     print('pi', pi)
                     print('T', T)
@@ -166,41 +166,30 @@ def Lineages_per_Population_Figure(lineage_start=1, lineage_end=2, reps=1, numSt
         scaleGom_2_h1.append(np.mean(scaleGom_2_h2))
         lineage_h1.append(lineage_num)
 
-    x=lineage_h1
+    x = lineage_h1
     Matplot_gen(x, acc_h1, bern_MAS_h1, bern_2_h1, MASlocBern, locBern2, cGom_MAS_h1, cGom_2_h1, MAScGom,
-                    cGom2, scaleGom_MAS_h1, scaleGom_2_h1, MASscaleGom, scaleGom2, xlabel='Number of Lineages', title='Lineages in a Population', save_name='Figure2.png')
+                cGom2, scaleGom_MAS_h1, scaleGom_2_h1, MASscaleGom, scaleGom2, xlabel='Number of Lineages', title='Lineages in a Population', save_name='Figure2.png')
 
 def AIC_Figure(T_MAS=130, T_2=61, state1=1, state2=4, reps=1, MASinitCells=[1], MASlocBern=[0.8], MAScGom=[1.6], MASscaleGom=[40], initCells2=[1], locBern2=[0.99], cGom2=[1.6], scaleGom2=[18], max_lin_length=1500, min_lin_length=100, verbose=False):
     '''Calculates and plots an AIC for all inputted states'''
-    
-    states = range(state1,state2+1)
+
+    states = range(state1, state2+1)
     acc_h1 = [] #list of lists of lists
     cell_h1 = []
-    bern_MAS_h1 = []
-    bern_2_h1 = []
-    cGom_MAS_h1 = []
-    cGom_2_h1 = []
-    scaleGom_MAS_h1 = []
-    scaleGom_2_h1 = []
     AIC_h1 = []
     X, masterLineage, newLineage = Depth_Two_State_Lineage(T_MAS, MASinitCells, MASlocBern, MAScGom, MASscaleGom, T_2, initCells2, locBern2, cGom2, scaleGom2)
-    
+
     while len(newLineage) > max_lin_length or len(masterLineage) < min_lin_length or (len(newLineage)-len(masterLineage)) < min_lin_length:
-            X, masterLineage, newLineage = Depth_Two_State_Lineage(T_MAS, MASinitCells, MASlocBern, MAScGom, MASscaleGom, T_2, initCells2, locBern2, cGom2, scaleGom2)
-            
-    print(len(masterLineage), len(newLineage))
+        X, masterLineage, newLineage = Depth_Two_State_Lineage(T_MAS, MASinitCells, MASlocBern, MAScGom, MASscaleGom, T_2, initCells2, locBern2, cGom2, scaleGom2)
+    
+    if verbose:
+        print(len(masterLineage), len(newLineage))
 
     for numStates in states: #a pop with num number of lineages
         print('numstates', numStates)
         acc_h2 = []
         cell_h2 = []
-        bern_h2 = []
-        bern_MAS_h2 = []
-        bern_2_h2 = []
-        cGom_MAS_h2 = []
-        cGom_2_h2 = []
-        scaleGom_MAS_h2 = []
-        scaleGom_2_h2 = []
+        AIC_h2 = []
 
         for rep in range(reps):
             numStates = numStates
@@ -208,13 +197,6 @@ def AIC_Figure(T_MAS=130, T_2=61, state1=1, state2=4, reps=1, MASinitCells=[1], 
             AIC_value_holder_rel_0, param_num_cost, AIC_degrees_of_freedom_holder = getAIC(tHMMobj, LL)
             acc_h3 = []
             cell_h3 = []
-            bern_h3 = []
-            bern_MAS_h3 = []
-            bern_2_h3 = []
-            cGom_MAS_h3 = []
-            cGom_2_h3 = []
-            scaleGom_MAS_h3 = []
-            scaleGom_2_h3 = []
 
             for lin in range(tHMMobj.numLineages):
                 cell_h3.append(len(lineage))
@@ -225,14 +207,12 @@ def AIC_Figure(T_MAS=130, T_2=61, state1=1, state2=4, reps=1, MASinitCells=[1], 
 
         acc_h1.extend(acc_h2)
         cell_h1.extend(cell_h2)
-        AIC_h2 = []
         AIC_h1.extend(AIC_h2)
-        
+
         if verbose:
             print('h1',cell_h1)
         
-
-    x=states
+    x = states
     fig, axs = plt.subplots(nrows=1, ncols=1, sharex=True)
     ax = axs
     ax.errorbar(x, AIC_h1, fmt='o', c='b', marker="*", fillstyle='none')
