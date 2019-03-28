606d2ff6699cbca41a44235fa55a66950f472a02
Nikan <nikan.namiri@gmail.com>
2019-03-27 19:37:42 -0700

    linted breadth

--- lineage/figures/Breadth_Two_State_Lineage.py
+++ lineage/figures/Breadth_Two_State_Lineage.py
@@ -3,17 +3,16 @@
 from lineage.Lineage_utils import remove_NaNs
 from lineage.Lineage_utils import generatePopulationWithTime as gpt
 
-def Breadth_Two_State_Lineage(experimentTime, initCells, locBern, cGom, scaleGom, switchT, bern2, cG2, scaleG2, switchT, verbose=False):
+def Breadth_Two_State_Lineage(experimentTime, initCells, locBern, cGom, scaleGom, bern2, cG2, scaleG2, switchT, verbose=False):
     '''Creates a lineage at which the entire lineage changes state after a time point'''
-    
-    initCells = [lineage_num]
+
     LINEAGE = gpt(experimentTime, initCells, locBern, cGom, scaleGom, switchT, bern2, cG2, scaleG2)
-    
-    while len(LINEAGE) == 0:
+
+    while LINEAGE == False: #determines if lineage is empty, so can regenerate a new one
         LINEAGE = gpt(experimentTime, initCells, locBern, cGom, scaleGom, switchT, bern2, cG2, scaleG2)
-        
+
     X = remove_NaNs(LINEAGE)
     if verbose:
         print(len(X))
-    
-    return(X)
+
+    return X
