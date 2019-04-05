3cf098837a4bd1b6f2534b65a23003ea39beef17
Ali <alif@asmlab.org>
2019-04-04 18:46:21 -0700

    comment

--- lineage/figures/figure2.py
+++ lineage/figures/figure2.py
@@ -7,7 +7,7 @@ from .figureCommon import subplotLabel, getSetup
 def makeFigure():
     # Get list of axis objects
     ax, f = getSetup((7, 6), (3, 4))
-
+#udiuewdie
     subplotLabel(ax[0], 'A')
 
     f.tight_layout()
