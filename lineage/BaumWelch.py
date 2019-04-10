57287dbf2d99140ee9d883e621952332387f5d55
Nikan <nikan.namiri@gmail.com>
2019-04-09 19:26:03 -0700

    lint

--- lineage/BaumWelch.py
+++ lineage/BaumWelch.py
@@ -5,7 +5,7 @@ from .tHMM_utils import max_gen, get_gen, get_daughters, getAccuracy_BW
 from .DownwardRecursion import get_root_gammas, get_nonroot_gammas
 from .UpwardRecursion import get_leaf_Normalizing_Factors, get_leaf_betas, get_nonleaf_NF_and_betas, calculate_log_likelihood, beta_parent_child_func
 from .Lineage_utils import bernoulliParameterEstimatorAnalytical, gompertzAnalytical, exponentialAnalytical
-from ..Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
+from .Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
 
 def zeta_parent_child_func(node_parent_m_idx, node_child_n_idx, state_j, state_k, lineage, beta_array, MSD_array, gamma_array, T):
     '''calculates the zeta value that will be used to fill the transition matrix in baum welch'''
