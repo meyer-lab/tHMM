ef3e1bf42321e10cc00d8836be424d8fede4054d
Shakthi Visagan <shak360@gmail.com>
2019-04-22 17:44:53 -0700

    fixing some doc error regarding remove nans

--- lineage/figures/Breadth_Two_State_Lineage.py
+++ lineage/figures/Breadth_Two_State_Lineage.py
@@ -1,6 +1,6 @@
 '''Generates a lineage with breadth model ie cells all switch state at a single time point'''
 
-from lineage.Lineage_utils import remove_NaNs
+from lineage.Lineage_utils import remove_singleton_lineages
 from lineage.Lineage_utils import generatePopulationWithTime as gpt
 
 
@@ -12,7 +12,7 @@ def Breadth_Two_State_Lineage(experimentTime, initCells, locBern, cGom, scaleGom
     while not LINEAGE:  # determines if lineage is empty, so can regenerate a new one
         LINEAGE = gpt(experimentTime, initCells, locBern, cGom, scaleGom, switchT, bern2, cG2, scaleG2)
 
-    X = remove_NaNs(LINEAGE)
+    X = remove_singleton_lineages(LINEAGE)
     if verbose:
         print(len(X))
 
