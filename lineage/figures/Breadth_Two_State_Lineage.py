d6baadf7de816b10cd5a3caedf74e4fe2284fbb1
Nikan <nikan.namiri@gmail.com>
2019-05-27 11:06:14 -0700

    clean up breadth file

--- lineage/figures/Breadth_Two_State_Lineage.py
+++ lineage/figures/Breadth_Two_State_Lineage.py
@@ -7,21 +7,17 @@ from lineage.Lineage_utils import generatePopulationWithTime as gpt
 def Breadth_Two_State_Lineage(experimentTime, initCells, locBern, betaExp, switchT, bern2, betaExp2, FOM, verbose=False):
     '''Creates a lineage at which the entire lineage changes state after a time point'''
 
+    # Generate lineage and remove the cell unfinished and the cells that dont create a lineage
     LINEAGE = gpt(experimentTime, initCells, locBern, betaExp, switchT, bern2, betaExp2, FOM)
-
     LINEAGE = remove_unfinished_cells(LINEAGE)
     LINEAGE = remove_singleton_lineages(LINEAGE)
-        
     while not LINEAGE:  # determines if lineage is empty, so can regenerate a new one
         LINEAGE = gpt(experimentTime, initCells, locBern, betaExp, switchT, bern2, betaExp2, FOM)
         LINEAGE = remove_unfinished_cells(LINEAGE)
         LINEAGE = remove_singleton_lineages(LINEAGE)
-
-
-
     X = LINEAGE
-        
-        
+    
+    # Put cells in their respective true lineages based on their true states
     masterLineage = []
     sublineage2 = []
     for ii, cell in enumerate(X):
@@ -31,9 +27,4 @@ def Breadth_Two_State_Lineage(experimentTime, initCells, locBern, betaExp, switc
             sublineage2.append(cell)
         else:
             raise print('more than 2 true states error')
-            
-
-    if verbose:
-        print(len(masterLineage), len(sublineage2), len(X))
-        
     return X, masterLineage, sublineage2
