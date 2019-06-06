091bffabdd29c51c3aa169d6da317b4b881952d6
Nikan <nikan.namiri@gmail.com>
2019-06-05 23:22:17 -0700

    lower reps

--- lineage/figures/Fig_Gen.py
+++ lineage/figures/Fig_Gen.py
@@ -12,7 +12,7 @@ from .Matplot_gen import Matplot_gen
 from ..tHMM_utils import getAccuracy, getAIC
 from ..Lineage_utils import remove_singleton_lineages, remove_unfinished_cells
 
-def KL_per_lineage(T_MAS=500, T_2=100, reps=200, MASinitCells=[1], MASlocBern=[0.8], MASbeta=[80], initCells2=[1], locBern2=[0.99], beta2=[20], numStates=2, max_lin_length=200, min_lin_length=80, FOM='E', verbose=False):
+def KL_per_lineage(T_MAS=500, T_2=100, reps=2, MASinitCells=[1], MASlocBern=[0.8], MASbeta=[80], initCells2=[1], locBern2=[0.99], beta2=[20], numStates=2, max_lin_length=200, min_lin_length=80, FOM='E', verbose=False):
     """Run the KL divergence on emmission likelihoods."""
     # Make the master cells equal to the same thing
     MASlocBern_array, MASbeta_array= [], []
