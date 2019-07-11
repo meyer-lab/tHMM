ecb69b45063bd323b38eb3949a22e41711ff5d79
Shakthi Visagan <shak360@gmail.com>
2019-07-10 18:59:08 -0700

    stupid mistake regarding import

--- lineage/Depth_Two_State_Lineage.py
+++ lineage/Depth_Two_State_Lineage.py
@@ -3,7 +3,7 @@
 import numpy as np
 import logging
 from .Lineage_utils import remove_singleton_lineages, remove_unfinished_cells
-from .Lineage_utils import generatePopulationWithTime as gpt
+from .Lineage_utils import generatePopulationWithTime as gpt, select_population
 
 
 def Depth_Two_State_Lineage(T_MAS, MASinitCells, MASlocBern, T_2, initCells2, locBern2, FOM='E', betaExp=None, betaExp2=None):
