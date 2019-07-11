9b5312c1f06234b5cc11b6c8ca470113e0f86fb9
Farnazm <34812750+Farnazmdi@users.noreply.github.com>
2019-07-11 14:32:12 -0700

    Update Depth_Two_State_Lineage.py

--- lineage/Depth_Two_State_Lineage.py
+++ lineage/Depth_Two_State_Lineage.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import logging
-from .Lineage_utils import remove_singleton_lineages, select_population
+from .Lineage_utils import remove_singleton_lineages, select_population, remove_unfinished_cells
 from .Lineage_utils import generatePopulationWithTime as gpt
 
 
@@ -13,12 +13,12 @@ def Depth_Two_State_Lineage(T_MAS, MASinitCells, MASlocBern, T_2, initCells2, lo
     MASexperimentTime = T_MAS
     masterLineage = gpt(MASexperimentTime, MASinitCells, MASlocBern, FOM=FOM, betaExp=betaExp)
     masterLineage = remove_singleton_lineages(masterLineage)
-    masterLineage, _ = select_population(masterLineage, MASexperimentTime)
+    masterLineage = remove_unfinished_cells(masterLineage)
 
     while not masterLineage:
         masterLineage = gpt(MASexperimentTime, MASinitCells, MASlocBern, FOM=FOM, betaExp=betaExp)
         masterLineage = remove_singleton_lineages(masterLineage)
-        masterLineage, _ = select_population(masterLineage, MASexperimentTime)
+        masterLineage = remove_unfinished_cells(masterLineage)
     
     for cell in masterLineage:
         cell.true_state = 0
@@ -26,12 +26,12 @@ def Depth_Two_State_Lineage(T_MAS, MASinitCells, MASlocBern, T_2, initCells2, lo
     experimentTime2 = T_2
     sublineage2 = gpt(experimentTime2, initCells2, locBern2, FOM=FOM, betaExp=betaExp2)
     sublineage2 = remove_singleton_lineages(sublineage2)
-    sublineage2, _ = select_population(sublineage2, experimentTime2)
+    sublineage2 = remove_unfinished_cells(sublineage2)
 
     while not sublineage2:
         sublineage2 = gpt(experimentTime2, initCells2, locBern2, FOM=FOM, betaExp=betaExp2)
         sublineage2 = remove_singleton_lineages(sublineage2)
-        sublineage2, _ = select_population(sublineage2, experimentTime2)
+        sublineage2 = remove_unfinished_cells(sublineage2)
     
     cell_endT_holder = []
     for cell in masterLineage:
