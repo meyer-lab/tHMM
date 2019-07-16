eab8bfd4da0e2817288cb64f6d23021f5e025999
Shakthi Visagan <shak360@gmail.com>
2019-07-16 16:13:29 -0700

    Update Emissions.py

--- lineage/Emissions.py
+++ lineage/Emissions.py
@@ -11,19 +11,19 @@ class Cell:
         self.observation = observation #TODO
         
     def _divide(self, state, T):
-        """ Membber function that performs division of a cell. Equivalent to adding another timestep in a Markov process. """
-        parent_state = self.state
-        left_state, right_state = double(parent_state, T)
-        self.left = cell(state = left_state, left = None, right = None, parent = self)
+        """ Member function that performs division of a cell. Equivalent to adding another timestep in a Markov process. """
+        parent_state = self.state # get the state of the parent
+        left_state, right_state = double(parent_state, T) # roll a loaded die according to the row in the transtion matrix
+        self.left = cell(state = left_state, left = None, right = None, parent = self) # assign the outcoming states to new cells
         self.right = cell(state = right_state, left = None, right = None, parent = self)
         
         return self.left, self.right
 
 # second function
 def double(state, T):
-
+    """ Function that essentially rolls a loaded die given a state that determines the row of the transition matrix. """
     num_states = len(T[0])
-#     assertIn(state, range(num_state)), "the state you provided is not in the range"
+    assertIn(state, range(num_state)), "the state you provided is not in the range"
 
     state_decider = sp.multinomial.rvs(2, T[state])
     s = state_decider.tolist()
