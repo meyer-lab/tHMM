ea5a5df3cffd777e50b5ee28ae1feda72fdaa28e
Shakthi Visagan <shak360@gmail.com>
2019-07-17 14:15:10 -0700

    Update Emissions.py

--- lineage/Emissions.py
+++ lineage/Emissions.py
@@ -7,6 +7,7 @@ import numpy as np
 # Functions that return cells or lists of cells will be spaced with underscores.
 # Functions that are not to be used by a general user are prefixed with an underscore.
 # States of cells are 0-indexed and discrete (states start at 0 and are whole numbers).
+# Variables with the prefix num (i.e., num_states, num_cells) have an underscore following the 'num'.
 
 class CellVar:
     def __init__(self, state, left, right, parent, gen):
@@ -66,8 +67,8 @@ def _double(parent_state, T):
     """ Function that essentially rolls two of the same loaded dice given a state that determines the row of the transition matrix. The results of the roll of the loaded dice are two new states that are returned. """
     # Checking that the inputs are of the right shape
     assert T.shape[0] == T.shape[1], "Transition numpy array is not square. Ensure that your transition numpy array has the same number of rows and columns."
-    num_states = T.shape[0]
-    assert 0 <= parent_state <= num_states-1, "The parent state is a state outside of the range states being considered."
+    T_num_states = T.shape[0]
+    assert 0 <= parent_state <= T_num_states-1, "The parent state is a state outside of the range of states being considered."
     
     # Rolling two of the same loaded dice separate times and assigning where they landed to states
     left_state_results, right_state_results = sp.multinomial.rvs(n=1, p=T[parent_state,:], size=2) # first and second roll are left and right
@@ -76,56 +77,61 @@ def _double(parent_state, T):
 
     return left_state, right_state
 
+
 class LineageTree:
     def __init__(self, pi, T, E, desired_num_cells):
         self.pi = pi
+        pi_num_states = len(pi)
         self.T = T
+        T_shape = self.T.shape()
+        assert T_shape[0] == T_shape[1], "Transition numpy array is not square. Ensure that your transition numpy array has the same number of rows and columns."
+        T_num_states = self.T.shape[0]
         self.E = E
+        E_num_states = len[state for state in self.E.keys()]
+        assert pi_num_states == T_num_states == E_num_states
         self.desired_num_cells = desired_num_cells
-        self.lineage_list = generate(self.pi, self.T, self.desired_num_cells)
+        self.lineage_list = self._generate_lineage_list()
+        
+    def _generate_lineage_list(self):
+        """ Generates a single lineage tree given Markov variables. This only generates the hidden variables (i.e., the states). """
+        first_state_results = sp.multinomial.rvs(1, self.pi) # roll the dice and yield the state for the first cell
+        first_cell_state = first_state_results.index(1) 
+        first_cell = CellVar(state=first_cell_state, left=None, right=None, parent=None, gen=1) # create first cell
+        lineage_list = [first_cell]
 
-def generate(pi, T, desired_num_cells):
-    """ Generates a single lineage tree given Markov variables. This only generates the hidden variables (i.e., the states). """
-    first_state_results = sp.multinomial.rvs(1, pi) # roll the dice and yield the state for the first cell
-    first_cell_state = first_state_results.index(1) 
-    first_cell = CellVar(state=first_cell_state, left=None, right=None, parent=None, gen=1) # create first cell
-    lineage_list = [first_cell]
-    
-    for cell in lineage_list: # letting the first cell proliferate
-        if not cell.left: # if the cell has no daughters...
-            left_cell, right_cell = cell._divide(T) # make daughters by dividing and assigning states
-            lineage_list.append(left_cell) # add daughters to the list of cells
-            lineage_list.append(right_cell)
+        for cell in lineage_list: # letting the first cell proliferate
+            if not cell.left: # if the cell has no daughters...
+                left_cell, right_cell = cell._divide(self.T) # make daughters by dividing and assigning states
+                lineage_list.append(left_cell) # add daughters to the list of cells
+                lineage_list.append(right_cell)
 
-        if len(lineage_list) >= desired_num_cells:
-            break
-            
-    return lineage_list
+            if len(lineage_list) >= desired_num_cells:
+                break
 
+        return lineage_list
 
-def _get_state_count(state, lineage_list):
-    """ Counts the number of cells in a specific state and makes a list out of those numbers. Used for generating emissions for that specific state. """
-    cells_in_state = [] # a list holding cells in the same state
-    for cell in lineage_list: 
-        if cell.state == state: # if the cell is in the given state...
-            cells_in_state.append(cell) # append them to a list
+    def _get_state_count(self, state):
+        """ Counts the number of cells in a specific state and makes a list out of those numbers. Used for generating emissions for that specific state. """
+        cells_in_state = [] # a list holding cells in the same state
+        for cell in self.lineage_list: 
+            if cell.state == state: # if the cell is in the given state...
+                cells_in_state.append(cell) # append them to a list
 
-    num_cells_in_state = len(cells_in_state) # gets the number of cells in the list
-    
-    return num_cells_in_state, cells_in_state
+        num_cells_in_state = len(cells_in_state) # gets the number of cells in the list
 
+        return num_cells_in_state, cells_in_state
 
-def _generate_state_obs(emission_dict, state, lineage_list):
-    """ Gets the dictionary holding the inner dictionaries (the state dictionaries); the inner dictionaries are those where the key is the name of the distribution, and the value is an object of that distribution's class (which are instantiated with the parameter(s) defining that distribution.This functions make a list of tuples, holding the value of these emissions for each cell in a specific state. """
-    num_cells_in_state, cells_in_state = _state_count(state, lineage_list)
-    inner_state_dict = emission_dict['{}'.format(state)] 
+    def _generate_state_obs(self, state):
+        """ Gets the dictionary holding the inner dictionaries (the state dictionaries); the inner dictionaries are those where the key is the name of the distribution, and the value is an object of that distribution's class (which are instantiated with the parameter(s) defining that distribution.This functions make a list of tuples, holding the value of these emissions for each cell in a specific state. """
+        num_cells_in_state, cells_in_state = self._get_state_count(state)
+        inner_state_dict = emission_dict['{}'.format(state)] 
 
-    observation_list = []
-    for dist_object in inner_state_dict.values():
-        observation_list.append(dist_object.rvs(size=num_cells_in_state)) # counts is the number of cells for that state
+        observation_list = []
+        for dist_object in inner_state_dict.values():
+            observation_list.append(dist_object.rvs(size=num_cells_in_state)) # counts is the number of cells for that state
 
-        tuple_list = functools.reduce(( lambda x,y: list(zip(x,y))), observation_list) # makes tuples off of (bernoulli, dist_value)
-    return tuple_list
+            tuple_list = functools.reduce(( lambda x,y: list(zip(x,y))), observation_list) # makes tuples off of (bernoulli, dist_value)
+        return tuple_list
 
 
 def assign_emission(emission_dict, state, X):
