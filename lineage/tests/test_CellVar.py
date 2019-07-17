301bacdfc13c9c8a03ca3f4fb505f15634e38974
Shakthi Visagan <shak360@gmail.com>
2019-07-17 16:42:50 -0700

    Update and rename test_emission.py to test_CellVar.py

---
+++ lineage/tests/test_CellVar.py
@@ -0,0 +1,25 @@
+""" Unit test file. """
+import unittest
+
+from ..CellVar import CellVar as c
+
+class TestModel(unittest.TestCase):
+    
+def test_cell_divide(self):
+        """Make sure cells divide properly with proper parent/child member variables."""
+#        cell1 = c(startT=20)
+#        cell2, cell3 = cell1.divide(40)
+
+#        # cell divides at correct time & parent dies
+#        self.assertFalse(cell1.isUnfinished())
+#        self.assertTrue(cell1.tau == 20)
+#        self.assertTrue(cell2.startT == 40)
+#        self.assertTrue(cell2.isUnfinished())
+
+        # left and right children exist for cell1 with proper linking
+#        self.assertTrue(cell1.left is cell2)
+#        self.assertTrue(cell1.right is cell3)
+#        self.assertTrue(cell2.parent is cell1)
+#        self.assertTrue(cell3.parent is cell1)
+        
+    
