f7327c63388be54711df4c965a4564acb260f03a
Shakthi Visagan <shak360@gmail.com>
2019-07-16 17:15:48 -0700

    Update Emissions.py

--- lineage/Emissions.py
+++ lineage/Emissions.py
@@ -1,25 +1,63 @@
+""" This file contains the class for CellVar which holds the state and observation information in the hidden and observed trees respectively. """
 import scipy.stats as sp
 import numpy as np
 
-class Cell:
-    def __init__(self, state, left, right, parent):
+# Boolean functions are in camel-case.
+# Functions that return cells or lists of cells will be spaced with underscores.
+
+class CellVar:
+    def __init__(self, state, left, right, parent, gen):
         """ Instantiates the cell object. Contains memeber variables that identify daughter cells and parent cells. Also contains the state of the cell. """
         self.state = state
         self.left = left
         self.right = right
         self.parent = parent
-        self.observation = observation #TODO
+        self.gen = gen
         
-    def _divide(self, state, T):
+    def _divide(self, T):
         """ Member function that performs division of a cell. Equivalent to adding another timestep in a Markov process. """
-        parent_state = self.state # get the state of the parent
-        left_state, right_state = double(parent_state, T) # roll a loaded die according to the row in the transtion matrix
-        self.left = cell(state = left_state, left = None, right = None, parent = self) # assign the resulting states to new cells
-        self.right = cell(state = right_state, left = None, right = None, parent = self) # ensure that those cells are related
+        left_state, right_state = double(self.state, T) # roll a loaded die according to the row in the transtion matrix
+        self.left = CellVar(state=left_state, left=None, right=None, parent=self, gen=self.gen+1) # assign the resulting states to new cells
+        self.right = CellVar(state=right_state, left=None, right=None, parent=self, gen=self.gen+1) # ensure that those cells are related
         
         return self.left, self.right
-
-# second function
+    
+    def _isParent(self):
+        """ Boolean. Returns true if the cell has daughters. """
+        return self.left or self.right
+    
+    def _isChild(self):
+        """ Boolean. Returns true if this cell has a known parent. """
+        return self.parent.isParent()
+    
+    def _isRootParent(self):
+        """ Boolean. Returns true if this cell is the first cell in a lineage. """
+        bool_parent = False
+        if not self.parent and self.gen == 1:
+            bool_parent = True
+        return bool_parent
+    
+    def _isLeaf(self):
+        """ Boolean. Returns true when a cell is a leaf with no children. """
+        return self.left
+    
+    def _get_sister(self):
+        """ Member function that gets the sister of the current cell. """
+        cell_to_return = None
+        if self.parent.left is self:
+            cell_to_return = self.parent.right
+        elif self.parent.right is self:
+            cell_to_return = self.parent.left
+        return cell_to_return
+        
+    def _get_root_cell(self):
+        """ Get the first cell in the lineage to which this cell belongs. """
+        curr_cell = self
+        while curr_cell.gen > 1:
+            curr_cell = curr_cell.parent
+        assert _isRootParent(curr_cell)
+        return curr_cell
+        
 def _double(parent_state, T):
     """ Function that essentially rolls two of the same loaded dice given a state that determines the row of the transition matrix. The results of the roll of the loaded dice are two new states that are returned. """
     # Checking that the inputs are of the right shape
@@ -27,40 +65,27 @@ def _double(parent_state, T):
     num_states = T.shape[0]
     assert 0 <= parent_state <= num_states-1, "The parent state is a state outside of the range states being considered."
     
-    # Rolling two of the same loaded dice separate times
-    state_decider = sp.multinomial.rvs(n=1, p=T[parent_state,:], size=2)
-    s = state_decider.tolist()
+    # Rolling two of the same loaded dice separate times and assigning where they landed to states
+    left_state_results, right_state_results = sp.multinomial.rvs(n=1, p=T[parent_state,:], size=2) # first and second roll are left and right
+    left_state = left_state_results.index(1) # the result of the dice toss (where it landed) is the state
+    right_state = right_state_results.index(1)
 
-    if 2 in s:
-        same_index = s.index(2)
-        left_state = same_index
-        right_state = same_index
-        new_states = [left_state, right_state]
+    return left_state, right_state
 
-    elif 1 in s:
-        indexes = [i for i, x in enumerate(s) if x == 1]
-        left_state = indexes[0]
-        right_state = indexes[1]
-        new_states = [left_state, right_state]
-
-    return new_states
-
-
-# Main function
-def generate(T, Pi, num_cells):
-    state_dice = sp.multinomial.rvs(1, Pi)
-    s = state_dice.tolist()
-    first_cell_state = s.index(1) 
-    first_cell = cell(state = first_cell_state, left = None, right = None, parent = None)
-    X = [first_cell]
+def generate(T, pi, num_cells):
+    """ Generates a single lineage tree given Markov variables. This only generates the hidden variables (i.e., the states). """
+    first_state_results = sp.multinomial.rvs(1, pi) # roll the dice and yield the state for the first cell
+    first_cell_state = first_state_results.index(1) 
+    first_cell = CellVar(state=first_cell_state, left=None, right=None, parent=None, gen=1) # create first cell
+    lineage_list = [first_cell]
     
-    for cel in X:
-        if cel.left is None:
-            state = cel.state
-            cel_l, cel_r = cel.divide(state, T)
-            X.append(cel_l)
-            X.append(cel_r)
+    for cell in lineage_list: # letting the first cell proliferate
+        if not cell.left: # if the cell has no daughters...
+            left_cell, right_cell = cell._divide(T) # make daughters by dividing and assigning states
+            lineage_list.append(left_cell) # add daughters to the list of cells
+            lineage_list.append(right_cell)
 
-        if len(X) >= num_cells:
+        if len(lineage_list) >= num_cells:
             break
-    return X
+            
+    return lineage_list
