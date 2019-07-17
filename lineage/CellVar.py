13f4d02db4945339ba8e0ab33faeb0ca6093f140
Shakthi Visagan <shak360@gmail.com>
2019-07-17 16:44:38 -0700

    Update CellVar.py

--- lineage/CellVar.py
+++ lineage/CellVar.py
@@ -3,12 +3,13 @@ import scipy.stats as sp
 import numpy as np
 
 # temporary style guide:
-# Boolean functions are in camel-case.
+# Boolean functions are in camelCase.
 # Functions that return cells or lists of cells will be spaced with underscores.
 # Functions that are not to be used by a general user are prefixed with an underscore.
 # States of cells are 0-indexed and discrete (states start at 0 and are whole numbers).
 # Variables with the prefix num (i.e., num_states, num_cells) have an underscore following the 'num'.
 # Docstrings use """ and not '''.
+# Class names use camelCase.
 
 class CellVar:
     def __init__(self, state, left, right, parent, gen):
