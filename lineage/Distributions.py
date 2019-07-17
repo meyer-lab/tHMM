9613b5e0fe193b9c3b242c9e3ceda3c40d3071d6
Shakthi Visagan <shak360@gmail.com>
2019-07-17 16:50:53 -0700

    Update Distributions.py

--- lineage/Distributions.py
+++ lineage/Distributions.py
@@ -1,15 +1,22 @@
 ''' This file contains all the emision distributions as objects that could be used easily, and a user can add any new emissions to it. The idea is for the user to be able to define any emission or observation that follows a distribution and utilize it within the Markov model. The onus is on the user to define the emissions and distributions he or she wants to use, but we provide some distributions as examples. '''
 import scipy.stats as sp
 
-class bernoulli:
-    def __init__(self, bern_p):
+class observation:
+    def __init__(self, bern_p, expon_scale_beta, gamma_a, gamma_scale):
+        """ Initialization function should take in just in the parameters for the observations that comprise the multivariate random variable emission they expect their data to have. """
         self.bern_p = bern_p
-    
+        self.expon_scale_beta = expon_scale_beta
+        self.gamma_a = gamma_a
+        self.gamma_scale = gamma_scale
+        
     def rvs(self, size):
         return sp.bernoulli.rvs(p=self.bern_p, size=size)
+    # sp.gamma.rvs(a=self.gamma_a, scale=self.gamma_scale, size=size)
     
     def pdf(self, bern_obs):
         return sp.bernoulli.pmf(k=bern_obs, p=self.bern_p)
+    #sp.gamma.pdf(gamma_obs, a=self.gamma_a, scale=self.gamma_scale)
+#
     
     #def estimator
 
@@ -31,9 +38,8 @@ class gamma:
         self.gamma_scale = gamma_scale
         
     def rvs(self, size)
-        return sp.gamma.rvs(a=self.gamma_a, scale=self.gamma_scale, size=size)
+        return 
 
     def pdf(self, gamma_obs):
-        return sp.gamma.pdf(gamma_obs, a=self.gamma_a, scale=self.gamma_scale)
-
+        return 
     #def estimator
