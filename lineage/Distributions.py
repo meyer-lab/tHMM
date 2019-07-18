a73fdc4cccbe3f0b8e285c021be92606199f8426
Shakthi Visagan <shak360@gmail.com>
2019-07-17 18:02:05 -0700

    Update Distributions.py

--- lineage/Distributions.py
+++ lineage/Distributions.py
@@ -1,45 +1,32 @@
-''' This file contains all the emision distributions as objects that could be used easily, and a user can add any new emissions to it. The idea is for the user to be able to define any emission or observation that follows a distribution and utilize it within the Markov model. The onus is on the user to define the emissions and distributions he or she wants to use, but we provide some distributions as examples. '''
+""" This file is completely user defined"""
 import scipy.stats as sp
 
 class observation:
-    def __init__(self, bern_p, expon_scale_beta, gamma_a, gamma_scale):
+    def __init__(self, bern_p, expon_scale_beta, gamma_a, gamma_scale): # user has to identify what parameters to use for each state
         """ Initialization function should take in just in the parameters for the observations that comprise the multivariate random variable emission they expect their data to have. """
         self.bern_p = bern_p
         self.expon_scale_beta = expon_scale_beta
         self.gamma_a = gamma_a
         self.gamma_scale = gamma_scale
         
-    def rvs(self, size):
-        return sp.bernoulli.rvs(p=self.bern_p, size=size)
-    # sp.gamma.rvs(a=self.gamma_a, scale=self.gamma_scale, size=size)
+    def rvs(self, size): # user has to identify what the multivariate (or univariate if he or she so chooses) random variable looks like
+        """ User defined way of calculating a random variable given the parameters of the state stored in that observation's object. """
+        # {
+        bern_obs = sp.bernoulli.rvs(p=self.bern_p, size=size)
+        exp_obs = sp.expon.rvs(scale=self.expon_scale_beta, size=size)
+        gamma_obs = sp.gamma.rvs(a=self.gamma_a, scale=self.gamma_scale, size=size)
+        # } is user-defined in that they have to define and maintain the order of the multivariate random variables.
+        # These tuples of observations will go into the cells in the lineage tree.
+        return list(zip(bern_obs, exp_obs, gamma_obs))
     
-    def pdf(self, bern_obs):
-        return sp.bernoulli.pmf(k=bern_obs, p=self.bern_p)
-    #sp.gamma.pdf(gamma_obs, a=self.gamma_a, scale=self.gamma_scale)
-#
+    def pdf(self, tuple_of_obs): # user has to define how to calculate the likelihood
+        """ User defined way of calculating the likelihood of the observation stored in a cell. """
+        # In the case of a univariate observation, the user still has to define how the likelihood is calculated,
+        # but has the ability to just return the output of a known scipy.stats.<distribution>.<{pdf,pmf}> function.
+        # In the case of a multivariate observation, the user has to decide how the likelihood is calculated.
+        # In our example, we assume the observation's are uncorrelated across the dimensions (across the different
+        # distribution observations), so the likelihood of observing the multivariate observation is just the product of 
+        # the individual observation likelihoods.
+
     
-    #def estimator
-
-class expon:
-    def __init__(self, expon_scale_beta):
-        self.expon_scale_beta = expon_scale_beta
-        
-    def rvs(self, size):
-        return sp.expon.rvs(self.expon_scale_beta, size=size)
-    
-    def pdf(self, expon_obs):
-        return sp.expon.pdf(expon_obs, self.expon_scale_beta)
-
-    #def estimator
-
-class gamma:
-    def __init__(self, gamma_a, gamma_scale):
-        self.gamma_a = gamma_a
-        self.gamma_scale = gamma_scale
-        
-    def rvs(self, size)
-        return 
-
-    def pdf(self, gamma_obs):
-        return 
     #def estimator
