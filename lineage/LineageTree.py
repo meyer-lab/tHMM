6d2708369617b2a74e2064e9465b6b23bc5b288b
Shakthi Visagan <shak360@gmail.com>
2019-07-17 21:19:49 -0700

    Update LineageTree.py

--- lineage/LineageTree.py
+++ lineage/LineageTree.py
@@ -1,6 +1,7 @@
 """ This file contains the LineageTree class. """
 
 from .Lineage import CellVar
+from .Lineage import ObservationEmission
 
 import scipy.stats as sp
 import numpy as np
@@ -29,7 +30,7 @@ class LineageTree:
         self.desired_num_cells = desired_num_cells
         self.lineage_list = self._generate_lineage_list()
         for state in self.num_states:
-            self.E["{}".format(state)]["cells"] = self._assign_obs(state)
+            self.E["{}".format(state)].cells = self._assign_obs(state)
         
     def _generate_lineage_list(self):
         """ Generates a single lineage tree given Markov variables. This only generates the hidden variables (i.e., the states). """
@@ -52,36 +53,25 @@ class LineageTree:
     def _get_state_count(self, state):
         """ Counts the number of cells in a specific state and makes a list out of those numbers. Used for generating emissions for that specific state. """
         cells_in_state = [] # a list holding cells in the same state
+        indices_of_cells_in_state = []
         for cell in self.lineage_list: 
             if cell.state == state: # if the cell is in the given state...
                 cells_in_state.append(cell) # append them to a list
+                indices_of_cells_in_state.append(self.lineage_list)
 
         num_cells_in_state = len(cells_in_state) # gets the number of cells in the list
 
-        return num_cells_in_state, cells_in_state
-
-    def _generate_state_obs(self, state):
-        """ Gets the dictionary holding the inner dictionaries (the state dictionaries); the inner dictionaries are those where the key is the name of the distribution, and the value is an object of that distribution's class (which are instantiated with the parameter(s) defining that distribution.This functions make a list of tuples, holding the value of these emissions for each cell in a specific state. """
-        num_cells_in_state, _ = self._get_state_count(state)
-        inner_state_dict = self.E["{}".format(state)] 
-
-        dist_observation_lists = []
-        for dist_object in inner_state_dict.values():
-            dist_observation_lists.append(dist_object.rvs(size=num_cells_in_state)) # collect random variables
-            # Makes a list of tuples by zipping the list of different emissions into tuples. 
-            # For example, a list of 5 lists of emissions (one being Bernoulli, another being exponential, etc.) is reduced to
-            # a list of tuples, each tuple with 5 values.
-            dist_observation_tuples = functools.reduce((lambda growing_list, additional_list: list(zip(growing_list,additional_list))), dist_observation_lists)
-        return dist_observation_tuples
+        return num_cells_in_state, cells_in_state, indices_of_cells_in_state
 
     def _assign_obs(self, state):
-        """ Observation assignment for each state. """
-        dist_observation_tuples = self._generate_state_obs(state)
-        num_cells_in_state, cells_in_state = self._get_state_count(state)
-        assert len(cells_in_state) == len(dist_observation_tuples) == num_cells_in_state
+        """ Observation assignment give a state. """
+        num_cells_in_state, cells_in_state, _ = self._get_state_count(state)
+        list_of_tuples_of_obs = self.E["{}".format{state}]["emission_obj"].rvs(size=num_cells_in_state)
+        
+        assert len(cells_in_state) == len(list_of_tuples_of_obs) == num_cells_in_state
 
         for i, cell in enumerate(cells_in_state):
-            cell.obs = dist_observation_tuples[i]
+            cell.obs = list_of_tuples_of_obs[i]
 
         return cells_in_state
     
