9f1a48a1a57a212459f4cb1a3bd89fe00ef1b36b
Shakthi Visagan <shak360@gmail.com>
2019-07-19 16:23:31 -0700

    Update LineageTree.py

--- lineage/LineageTree.py
+++ lineage/LineageTree.py
@@ -31,58 +31,56 @@ class LineageTree:
         assert pi_num_states == T_num_states == E_num_states, "The number of states in your input Markov probability parameters are mistmatched. Please check that the dimensions and states match. "
         self.num_states = pi_num_states
         self.desired_num_cells = desired_num_cells
-        self.fullLineage_list = self._generate_lineage_list()
-        
-        # pruning
+        self.full_lin_list = self._generate_lineage_list()
+        for state in self.num_states:
+            self.E[state].full_lin_cells = self._assign_obs(state)
+         # pruning
         self.prune_boolean = prune_boolean # this is given by the user, true of they want the lineage to be pruned, false if they want the full binary tree
-        self.pruned_list = self._prune_lineage(self.fullLineage_list)
+        self.pruned_lin_list = self._prune_lineage()
 
         # Based on the user's decision, if they want the lineage to be pruned (prune_boolean == True), 
         # the lineage tree that is given to the tHMM, will be the pruned one.
         # If the user decides that they want the full binary tree (prune_boolean == False), 
-        # then the fullLineage_list will be passed to the output_lineage.
-        if prune_boolean is True:
-            self.output_lineage = self.pruned_list
+        # then the full_lin_list will be passed to the output_lineage.
+        if prune_boolean:
+            self.output_lineage = self.pruned_lin_list
         else:
-            self.output_lineage = self.fullLineage
+            self.output_lineage = self.full_lin_list
 
-        for state in self.num_states:
-            self.E[state].cells = self._assign_obs(state)
 
     def _generate_lineage_list(self):
         """ Generates a single lineage tree given Markov variables. This only generates the hidden variables (i.e., the states). """
         first_state_results = sp.multinomial.rvs(1, self.pi)  # roll the dice and yield the state for the first cell
         [first_cell_state] = np.where(first_state_results == 1)
         first_cell = CellVar(state=first_cell_state, left=None, right=None, parent=None, gen=1)  # create first cell
-        fullLineage_list = [first_cell]
+        self.full_lin_list = [first_cell]
 
-        for cell in fullLineage_list:  # letting the first cell proliferate
+        for cell in self.full_lin_list:  # letting the first cell proliferate
             if not cell.left:  # if the cell has no daughters...
                 left_cell, right_cell = cell._divide(self.T)  # make daughters by dividing and assigning states
-                fullLineage_list.append(left_cell)  # add daughters to the list of cells
-                fullLineage_list.append(right_cell)
+                self.full_lin_list.append(left_cell)  # add daughters to the list of cells
+                self.full_lin_list.append(right_cell)
 
-            if len(fullLineage_list) >= desired_num_cells:
+            if len(self.full_lin_list) >= desired_num_cells:
                 break
 
-        return fullLineage_list
+        return self.full_lin_list
 
     def _prune_lineage(self):
         """  """
-        self.pruned_list = self.fullLineage_list
-        for cell in to_be_pruned_tree:
-            _, residual_tree = get_subtrees(cell, fullLineage_list)
-            self.pruned_list = residual_tree
+        self.pruned_lin_list = self.full_lin_list
+        for cell in self.pruned_lin_list:
+            _, self.pruned_lin_list = get_subtrees(cell, self.pruned_lin_list)
         return self.pruned_list
 
     def _get_state_count(self, state):
         """ Counts the number of cells in a specific state and makes a list out of those numbers. Used for generating emissions for that specific state. """
         cells_in_state = []  # a list holding cells in the same state
         indices_of_cells_in_state = []
-        for cell in self.fullLineage_list:
+        for cell in self.full_lin_list:
             if cell.state == state:  # if the cell is in the given state...
                 cells_in_state.append(cell)  # append them to a list
-                indices_of_cells_in_state.append(self.fullLineage_list)
+                indices_of_cells_in_state.append(self.full_lin_list)
 
         num_cells_in_state = len(cells_in_state)  # gets the number of cells in the list
 
