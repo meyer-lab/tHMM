1fc4b699a18431e6c7f1f076e807e430204eed62
Shakthi Visagan <shak360@gmail.com>
2019-07-24 13:07:09 -0700

    more work on lienage tests

--- lineage/tests/test_LineageTree.py
+++ lineage/tests/test_LineageTree.py
@@ -2,32 +2,47 @@
 import unittest
 import numpy as np
 from ..LineageTree import LineageTree
+from ..StateDistribution import StateDistribution
 
 
 class TestModel(unittest.TestCase):
 
-    def test_generate_lineage_list(self):
+    def setUp(self):
+        # pi: the initial probability vector
+        pi = np.array([0.75, 0.25])
+
+        # T: transition probability matrix
+        T = np.array([[0.85, 0.15],
+                      [0.2, 0.8]])
         
-        # state0: resistant
-        # state1: susciptible
-        self.pi = np.array([0.85, 0.15]) # initial probability matrix
-        self.T = np.array([[0.95, 0.05],
-                      [0.3, 0.7]])
-        self.desired_num_cells = 2**8 - 1
+        # E: states are defined as StateDistribution objects
+        # State 0 parameters "Resistant"
+        state0 = 0
+        bern_p0 = 0.99
+        expon_scale_beta0 = 20
+        gamma_a0 = 5.0
+        gamma_scale0 = 1.0
 
-        full_lin_list = _generate_lineage_list(self)
-        self.assertTrue(len(full_lin_list) == len(self.desired_num_cells))
-        self.assertTrue(self.full_lin_list == full_lin_list)
+        # State 1 parameters "Susciptible"
+        state1 = 1
+        bern_p1 = 0.8
+        expon_scale_beta1 = 80
+        gamma_a1 = 10.0
+        gamma_scale1 = 2.0
 
-        for cell in full_lin_list:
-            self.assertTrue(cell is not None)
+        state_obj0 = StateDistribution(state0, bern_p0, expon_scale_beta0, gamma_a0, gamma_scale0)
+        state_obj1 = StateDistribution(state1, bern_p1, expon_scale_beta1, gamma_a1, gamma_scale1)
 
+        E = [state_obj0, state_obj1]
+                
+        self.lineage1_big_full = LineageTree(pi, T, E, desired_num_cells=2**9 - 1, prune_boolean=False)
+        self.lineage2_small_full = LineageTree(pi, T, E, desired_num_cells=2**3 - 1, prune_boolean=False)
+        self.lineage3_big_pruned = LineageTree(pi, T, E, desired_num_cells=2**9 - 1, prune_boolean=True)
+        self.lineage4_small_pruned = LineageTree(pi, T, E, desired_num_cells=2**3 - 1, prune_boolean=True)
             
     def test_prune_lineage(self):
-        pruned_list = _prune_lineage(self)
-        self.assertTrue(len(pruned_list) != 0)
+        pass
         
-
     def test_get_state_count(self):
         pass
 
