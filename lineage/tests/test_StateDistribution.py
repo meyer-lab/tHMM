40f6a54a631ea1246021cfa193cf14e3d1ef6b3b
farnazmdi <farnazmd95@gmail.com>
2019-07-26 16:34:14 -0700

    test for report time

--- lineage/tests/test_StateDistribution.py
+++ lineage/tests/test_StateDistribution.py
@@ -3,6 +3,7 @@ import unittest
 import numpy as np
 import scipy.stats as sp
 from ..StateDistribution import bernoulli_estimator, exponential_estimator, gamma_estimator
+from ..CellVar import CellVar as c
 
 
 class TestModel(unittest.TestCase):
@@ -26,5 +27,46 @@ class TestModel(unittest.TestCase):
         self.assertTrue(10 <= shape <= 15)
         self.assertTrue(2 <= scale <= 4)
 
+    def test_report_time(self):
+
+        T = np.array([[1.0, 0.0],
+                      [0.0, 1.0]])
+
+        pi = [0.0, 1.0]
+
+        state0 = 0
+        bern_p0 = 1.0
+        expon_scale_beta0 = 20
+        gamma_a0 = 5.0
+        gamma_scale0 = 1.0
+
+        # State 1 parameters "Susciptible"
+        state1 = 1
+        bern_p1 = 1.0
+        expon_scale_beta1 = 60
+        gamma_a1 = 10.0
+        gamma_scale1 = 2.0
+
+        state_obj0 = StateDistribution(state0, bern_p0, expon_scale_beta0, gamma_a0, gamma_scale0)
+        state_obj1 = StateDistribution(state1, bern_p1, expon_scale_beta1, gamma_a1, gamma_scale1)
+
+        E = [state_obj0, state_obj1]
+
+        desired_num_cells = 2**2 - 1 
+        prune_boolean = True # To get the full tree
+        lineage1 = LineageTree(pi, T, E, desired_num_cells, prune_boolean)
+
+        list_of_cells = [cells for cells in lineage1.lineage_output]
+        cell0_tau = list_of_cells[0].obs[1]
+        cell0_to_left_tau = list_of_cells[1].obs[1] + cell0_tau
+        cell0_to_right_tau = list_of_cells[2].obs[1] + cell0_tau
+
+        cell0_tau_report = report_time(list_of_cells[0])
+        self.assertTrue(cell0_tau == cell0_tau_report)
+
+        cell0_left_report = report_time(list_of_cells[1])
+        self.assertTrue(cell0_to_left_tau == cell0_left_report)
+
+        cell0_right_report = report_time(list_of_cells[2])
+        self.assertTrue(cell0_to_right_tau == cell0_right_report)
         
-    
