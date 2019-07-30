49b748e36a8085f6d9d2e48c807856f311b53240
Shakthi Visagan <shak360@gmail.com>
2019-07-30 10:35:09 -0700

    Update figure4.py

--- lineage/figures/figure4.py
+++ lineage/figures/figure4.py
@@ -10,10 +10,6 @@ def makeFigure():
     # Get list of axis objects
     ax, f = getSetup((12, 3), (1, 3))
 
-    number_of_cells_h1, accuracy_h1, bern_MAS_h1, bern_2_h1, MASlocBern, locBern2, MASbeta, beta2, betaExp_MAS_h1, betaExp_2_h1 = Lineage_Length()
-    Matplot_gen(ax[0:3], number_of_cells_h1, accuracy_h1, bern_MAS_h1, bern_2_h1, MASlocBern, locBern2, MASbeta, beta2,
-                betaExp_MAS_h1, betaExp_2_h1, xlabel='Cells per Lineage', FOM='E')  # Figure plots scale vs lineage length'''
-
     f.tight_layout()
 
     return f
