5e976960d122911aca7125f10650b8b15d564e4f
Shakthi Visagan <shak360@gmail.com>
2019-07-30 10:38:26 -0700

    Update figure6.py

--- lineage/figures/figure6.py
+++ lineage/figures/figure6.py
@@ -2,10 +2,6 @@
 This creates Figure 6.
 """
 from .figureCommon import getSetup
-import numpy as np
-from matplotlib import ticker
-from .Fig_Gen import KL_per_lineage
-from .Matplot_gen import moving_average
 
 
 def makeFigure():
