9544cfbb3f363e0168a6c4747348b1dce67b9cbb
Shakthi Visagan <shak360@gmail.com>
2019-07-30 23:55:06 -0700

    weird indent fix

--- lineage/tests/test_CellVar.py
+++ lineage/tests/test_CellVar.py
@@ -56,9 +56,10 @@ class TestModel(unittest.TestCase):
         self.assertTrue(cell._isParent())
 
     def test_isChild(self):
-         """ blah """
+        """ blah """
         T = np.array([[1.0, 0.0], 
                       [0.0, 1.0]])
+        
         parent_state = 1
         cell = c(state=parent_state, left=None, right=None, parent=None, gen=1)
         self.assertFalse(cell._isChild())
