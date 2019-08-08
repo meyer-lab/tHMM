9e9e3d9ed5fab2de4921c8a421a12e88b02c97e9
farnazmdi <farnazmd95@gmail.com>
2019-08-08 15:05:50 -0700

    tests for LineageTree added

--- lineage/tests/test_LineageTree.py
+++ lineage/tests/test_LineageTree.py
@@ -35,7 +35,38 @@ class TestModel(unittest.TestCase):
 
         E = [state_obj0, state_obj1]
 
-        self.lineage1_big_full = LineageTree(pi, T, E, desired_num_cells=2**4 - 1, prune_boolean=False)
-        self.lineage2_small_full = LineageTree(pi, T, E, desired_num_cells=2**3 - 1, prune_boolean=False)
-        self.lineage3_big_pruned = LineageTree(pi, T, E, desired_num_cells=2**9 - 1, prune_boolean=True)
-        self.lineage4_small_pruned = LineageTree(pi, T, E, desired_num_cells=2**3 - 1, prune_boolean=True)
+        self.lineage1_big = LineageTree(pi, T, E, desired_num_cells=2**9 - 1, prune_boolean=True)
+        self.lineage2_small = LineageTree(pi, T, E, desired_num_cells=2**5 - 1, prune_boolean=True)
+        self.lineage3_big_pruned = LineageTree(pi, T, E, desired_num_cells=2**9 - 1, prune_boolean=False)
+        self.lineage4_small_pruned = LineageTree(pi, T, E, desired_num_cells=2**3 - 1, prune_boolean=False)
+
+    def test_generate_lineage_list(self):
+        """ A unittest for generate_lineage_list. """
+        # checking the number of cells generated is equal to the desired number of cells given by the user.
+        self.assertTrue(len(self.lineage1_big.full_lin_list) == 2**9 -1)
+        self.assertTrue(len(self.lineage2_small.full_lin_list) == 2**5 -1)
+        self.assertTrue(self.lineage1_big.full_lin_list == self.lineage1_big.output_lineage), "The output lineage is wrong according to prune boolean"
+        self.assertTrue(self.lineage3_big_pruned.pruned_lin_list == self.lineage3_big_pruned.output_lineage), "The output lineage is wrong according to prune boolean"
+
+    def test_prune_lineage(self):
+        """ A unittest for prune_lineage. """
+        # checking the number of cells in the pruned version is smaller than the full version.
+        self.assertTrue(len(self.lineage1_big.full_lin_list) >= len(self.lineage1_big.pruned_lin_list))
+        self.assertTrue(len(self.lineage2_small.full_lin_list) >= len(self.lineage2_small.pruned_lin_list))
+
+        # checking all the cells in the pruned version should have all the bernoulli observations == 1 (dead cells have been removed.)
+        for cell in self.lineage1_big.pruned_lin_list:
+            self.assertFalse(cell.obs[0] == 0), "There are still cells that have died but not removed!"
+            if cell._isLeaf():
+                self.assertTrue(cell.left == None)
+                self.assertTrue(cell.right == None)
+
+        for cell in self.lineage2_small.pruned_lin_list:
+            self.assertFalse(cell.obs[0] == 0), "There are still cells that have died but not removed!"
+            if cell._isLeaf():
+                self.assertTrue(cell.left == None)
+                self.assertTrue(cell.right == None)
+            
+        
+        
+        
