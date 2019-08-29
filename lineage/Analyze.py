2a760fa850600fe64a516c8f90ec62f1a78f4de0
farnazmdi <farnazmd95@gmail.com>
2019-08-29 13:13:35 -0700

    accuracies and figure6

--- lineage/Analyze.py
+++ lineage/Analyze.py
@@ -1,5 +1,6 @@
 '''Calls the tHMM functions and outputs the parameters needed to generate the Figures'''
-
+import numpy as np
+import copy as cp
 from .BaumWelch import fit
 from .Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
 from .UpwardRecursion import get_leaf_Normalizing_Factors, get_leaf_betas, get_nonleaf_NF_and_betas, calculate_log_likelihood
@@ -40,12 +41,35 @@ def Analyze(X, numStates):
 #
 
 
-def accuracy(X, all_states):
-    for num, lineageObj in enumerate(X):
-        lin_estimated_states = all_states[num]
+def accuracy(tHMMobj, all_states):
+    accuracy = []
+    for num, lineageObj in enumerate(tHMMobj.X):
         lin_true_states = [cell.state for cell in lineageObj.output_lineage]
-        total = len(lin_estimated_states)
-        assert total == len(lin_true_states)
-        counter = [1. if a == b else 0. for (a, b) in zip(lin_estimated_states, lin_true_states)]
-        acc = sum(counter) / total
-    return max(acc, 1 - acc)
+
+        bern_diff = np.zeros((lineageObj.num_states))
+        gamma_a_diff = np.zeros((lineageObj.num_states))
+        gamma_scale_diff = np.zeros((lineageObj.num_states))
+        for state in range(lineageObj.num_states):
+            bern_diff[state] = abs(tHMMobj.estimate.E[state].bern_p - lineageObj.E[0].bern_p)
+            gamma_a_diff[state] = abs(tHMMobj.estimate.E[state].gamma_a - lineageObj.E[0].gamma_a)
+            gamma_scale_diff[state] = abs(tHMMobj.estimate.E[state].gamma_scale - lineageObj.E[0].gamma_scale)
+
+        bern_diff = bern_diff / sum(bern_diff)
+        gamma_a_diff = gamma_a_diff / sum(gamma_a_diff)
+        gamma_scale_diff = gamma_scale_diff / sum(gamma_scale_diff)
+
+        total_errs = bern_diff + gamma_a_diff + gamma_scale_diff
+        if total_errs[0] <= total_errs[1]:
+            new_all_states = all_states[num]
+        else:
+            print('SWITCHING!')
+            new_all_states = [not(x) for x in all_states[num]] 
+            tmp = cp.deepcopy(tHMMobj.estimate.E[1])
+            tHMMobj.estimate.E[1] = tHMMobj.estimate.E[0]
+            tHMMobj.estimate.E[0] = tmp
+
+        counter = [1 if a==b else 0 for (a,b) in zip(new_all_states,lin_true_states)]
+        acc = sum(counter)/len(lin_true_states)
+        accuracy.append(acc)
+
+    return accuracy
--- lineage/figures/figure6.py
+++ lineage/figures/figure6.py
@@ -18,10 +18,10 @@ rc('text', usetex=True)
 
 def makeFigure():
     """ Main figure generating function for Fig. 6 """
-    ax, f = getSetup((15, 5), (1, 3))
+    ax, f = getSetup((20, 10), (2, 4))
 
-    desired_num_cells, accuracies_unpruned, bern_unpruned, bern_p0, bern_p1, gamma_a_unpruned, gamma_a0, gamma_a1, gamma_scale_unpruned, gamma_scale0, gamma_scale1 = accuracy_increased_cells()
-    figure_maker(ax[0:3], desired_num_cells, accuracies_unpruned, bern_unpruned, bern_p0, bern_p1, gamma_a_unpruned, gamma_a0, gamma_a1, gamma_scale_unpruned, gamma_scale0, gamma_scale1)
+    desired_num_cells, accuracies_unpruned, bern_unpruned, bern_p0, bern_p1, gamma_a_unpruned, gamma_a0, gamma_a1, gamma_scale_unpruned, gamma_scale0, gamma_scale1, accuracies_pruned, bern_pruned, gamma_a_pruned, gamma_scale_pruned = accuracy_increased_cells()
+    figure_maker(ax, desired_num_cells, accuracies_unpruned, bern_unpruned, bern_p0, bern_p1, gamma_a_unpruned, gamma_a0, gamma_a1, gamma_scale_unpruned, gamma_scale0, gamma_scale1, accuracies_pruned, bern_pruned, gamma_a_pruned, gamma_scale_pruned)
 
     f.tight_layout()
     return f
@@ -55,41 +55,61 @@ def accuracy_increased_cells():
 
     E = [state_obj0, state_obj1]
     # the key part in this function
-    desired_num_cells = np.logspace(7, 10, num=50, base=2.0)
+    desired_num_cells = np.logspace(7, 10, num=10, base=2.0)
     desired_num_cells = [num_cell - 1 for num_cell in desired_num_cells]
 
     accuracies_unpruned = []
-    #accuracies_pruned = []
+    accuracies_pruned = []
     bern_unpruned = []
     gamma_a_unpruned = []
     gamma_scale_unpruned = []
+    bern_pruned = []
+    gamma_a_pruned = []
+    gamma_scale_pruned = []
+
 
     for num in desired_num_cells:
         print(num)
         lineage_unpruned = LineageTree(pi, T, E, num, prune_boolean=False)
+        lineage_pruned = cp.deepcopy(lineage_unpruned)
+        lineage_pruned.prune_boolean = True
 
         X1 = [lineage_unpruned]
+        X2 = [lineage_pruned]
         print("unpruned")
         deltas, state_ptrs, all_states, tHMMobj, NF, LL = Analyze(X1, 2)
-        acc1 = accuracy(X1, all_states)
+        deltas2, state_ptrs2, all_states2, tHMMobj2, NF2, LL2 = Analyze(X2, 2)
+        acc1 = accuracy(tHMMobj, all_states)[0]
+        acc2 = accuracy(tHMMobj2, all_states2)[0]
         accuracies_unpruned.append(100 * acc1)
+        accuracies_pruned.append(100 * acc2)
 
         bern_p_total = ()
         gamma_a_total = ()
         gamma_scale_total = ()
+        bern_p_total2 = ()
+        gamma_a_total2 = ()
+        gamma_scale_total2 = ()
         for state in range(tHMMobj.numStates):
             bern_p_total += (tHMMobj.estimate.E[state].bern_p,)
             gamma_a_total += (tHMMobj.estimate.E[state].gamma_a,)
             gamma_scale_total += (tHMMobj.estimate.E[state].gamma_scale,)
 
+            bern_p_total2 += (tHMMobj2.estimate.E[state].bern_p,)
+            gamma_a_total2 += (tHMMobj2.estimate.E[state].gamma_a,)
+            gamma_scale_total2 += (tHMMobj2.estimate.E[state].gamma_scale,)
+
         bern_unpruned.append(bern_p_total)
         gamma_a_unpruned.append(gamma_a_total)
         gamma_scale_unpruned.append(gamma_scale_total)
+        bern_pruned.append(bern_p_total2)
+        gamma_a_pruned.append(gamma_a_total2)
+        gamma_scale_pruned.append(gamma_scale_total2)
 
-    return desired_num_cells, accuracies_unpruned, bern_unpruned, bern_p0, bern_p1, gamma_a_unpruned, gamma_a0, gamma_a1, gamma_scale_unpruned, gamma_scale0, gamma_scale1
+    return desired_num_cells, accuracies_unpruned, bern_unpruned, bern_p0, bern_p1, gamma_a_unpruned, gamma_a0, gamma_a1, gamma_scale_unpruned, gamma_scale0, gamma_scale1, accuracies_pruned, bern_pruned, gamma_a_pruned, gamma_scale_pruned
 
 
-def figure_maker(ax, desired_num_cells, accuracies_unpruned, bern_unpruned, bern_p0, bern_p1, gamma_a_unpruned, gamma_a0, gamma_a1, gamma_scale_unpruned, gamma_scale0, gamma_scale1):
+def figure_maker(ax, desired_num_cells, accuracies_unpruned, bern_unpruned, bern_p0, bern_p1, gamma_a_unpruned, gamma_a0, gamma_a1, gamma_scale_unpruned, gamma_scale0, gamma_scale1, accuracies_pruned, bern_pruned, gamma_a_pruned, gamma_scale_pruned):
     x = desired_num_cells
     font = 11
     font2 = 10
@@ -126,3 +146,61 @@ def figure_maker(ax, desired_num_cells, accuracies_unpruned, bern_unpruned, bern
     ax[2].set_title('Gamma', fontsize=font)
     ax[2].tick_params(axis='both', which='major', labelsize=10, grid_alpha=0.25)
     ax[2].legend(loc='best', framealpha=0.3)
+
+    res = [[i for i, j in gamma_scale_unpruned], [j for i, j in gamma_scale_unpruned]]
+    ax[3].set_xlim((0, int(np.ceil(1.1 * max(x)))))
+    ax[3].set_xlabel('Number of Cells', fontsize=font2)
+    ax[3].scatter(x, res[0], c='b', marker="o", label='Susceptible Unpruned', alpha=0.5)
+    ax[3].scatter(x, res[1], c='r', marker="o", label='Resistant Unpruned', alpha=0.5)
+    ax[3].set_ylabel(r'Gamma scale $\alpha$', rotation=90, fontsize=font2)
+    ax[3].axhline(y=gamma_scale0, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='b', alpha=0.6)
+    ax[3].axhline(y=gamma_scale1, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='r', alpha=0.6)
+    ax[3].set_title('Gamma', fontsize=font)
+    ax[3].tick_params(axis='both', which='major', labelsize=10, grid_alpha=0.25)
+    ax[3].legend(loc='best', framealpha=0.3)
+    
+    ax[4].set_xlim((0, int(np.ceil(1.1 * max(x)))))
+    ax[4].set_xlabel('Number of Cells', fontsize=font2)
+    ax[4].set_ylim(0, 110)
+    ax[4].scatter(x, accuracies_pruned, c='k', marker="o", label='Accuracy', alpha=0.3)
+    ax[4].axhline(y=100, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='k', alpha=0.6)  # linestyle is dashdotdotted
+    ax[4].set_ylabel(r'Accuracy (\%)', rotation=90, fontsize=font2)
+    ax[4].get_yticks()
+    ax[4].tick_params(axis='both', which='major', labelsize=10, grid_alpha=0.25)
+    ax[4].set_title('State Assignment Accuracy', fontsize=font)
+
+    res = [[i for i, j in bern_pruned], [j for i, j in bern_pruned]]
+    ax[5].set_xlim((0, int(np.ceil(1.1 * max(x)))))
+    ax[5].set_xlabel('Number of Cells', fontsize=font2)
+    ax[5].scatter(x, res[0], c='b', marker="o", label='Susceptible Pruned', alpha=0.5)
+    ax[5].scatter(x, res[1], c='r', marker="o", label='Resistant Pruned', alpha=0.5)
+    ax[5].set_ylabel('Bern $p$', rotation=90, fontsize=font2)
+    ax[5].axhline(y=bern_p0, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='b', alpha=0.6)
+    ax[5].axhline(y=bern_p1, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='r', alpha=0.6)
+    ax[5].set_title('Bernoulli', fontsize=font)
+    ax[5].tick_params(axis='both', which='major', labelsize=10, grid_alpha=0.25)
+    ax[5].legend(loc='best', framealpha=0.3)
+
+    res = [[i for i, j in gamma_a_pruned], [j for i, j in gamma_a_pruned]]
+    ax[6].set_xlim((0, int(np.ceil(1.1 * max(x)))))
+    ax[6].set_xlabel('Number of Cells', fontsize=font2)
+    ax[6].scatter(x, res[0], c='b', marker="o", label='Susceptible Pruned', alpha=0.5)
+    ax[6].scatter(x, res[1], c='r', marker="o", label='Resistant Pruned', alpha=0.5)
+    ax[6].set_ylabel(r'Gamma a $\beta$', rotation=90, fontsize=font2)
+    ax[6].axhline(y=gamma_a0, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='b', alpha=0.6)
+    ax[6].axhline(y=gamma_a1, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='r', alpha=0.6)
+    ax[6].set_title('Gamma', fontsize=font)
+    ax[6].tick_params(axis='both', which='major', labelsize=10, grid_alpha=0.25)
+    ax[6].legend(loc='best', framealpha=0.3)
+
+    res = [[i for i, j in gamma_scale_pruned], [j for i, j in gamma_scale_pruned]]
+    ax[7].set_xlim((0, int(np.ceil(1.1 * max(x)))))
+    ax[7].set_xlabel('Number of Cells', fontsize=font2)
+    ax[7].scatter(x, res[0], c='b', marker="o", label='Susceptible Pruned', alpha=0.5)
+    ax[7].scatter(x, res[1], c='r', marker="o", label='Resistant Pruned', alpha=0.5)
+    ax[7].set_ylabel(r'Gamma scale $\alpha$', rotation=90, fontsize=font2)
+    ax[7].axhline(y=gamma_scale0, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='b', alpha=0.6)
+    ax[7].axhline(y=gamma_scale1, linestyle=(0, (3, 5, 1, 5, 1, 5)), linewidth=2, color='r', alpha=0.6)
+    ax[7].set_title('Gamma', fontsize=font)
+    ax[7].tick_params(axis='both', which='major', labelsize=10, grid_alpha=0.25)
+    ax[7].legend(loc='best', framealpha=0.3)
