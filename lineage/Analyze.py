ee7d1c69d0f47ff863e19c0180ca970c4b52984c
Shakthi Visagan <shak360@gmail.com>
2019-10-06 10:33:05 -0700

    removing old growth rate function

--- lineage/Analyze.py
+++ lineage/Analyze.py
@@ -109,19 +109,3 @@ def accuracyG(tHMMobj, all_states):
         acuracy_holder.append(100 * acc)
 
     return acuracy_holder
-
-
-def growth_rate(tHMMobj):
-    """ This function is to find the average growth rate for the total population of cells. """
-    exp_coef = np.zeros(shape=len(tHMMobj.X))
-    longest = np.zeros(shape=len(tHMMobj.X))
-
-    for lin_num, linObj in enumerate(tHMMobj.X):
-        longest[lin_num] = get_experiment_time(linObj)
-        print(longest)
-        y = float(len(linObj.output_lineage))  # number of cells up to the longest time
-        exp_coef[lin_num] = y ** (1 / longest[lin_num])  # exponential growth rate, assuming we start with 1 cell
-        print(exp_coef)
-
-    weighted_growthRate = np.sum([i * j for i, j in zip(longest, exp_coef)]) / sum(longest)
-    return weighted_growthRate
