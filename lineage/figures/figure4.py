2c232e93dea99040d2cc55a9e007dcbecacc6050
Shakthi Visagan <shak360@gmail.com>
2019-10-06 10:31:49 -0700

    figures cleared because they are on another branch

--- lineage/figures/figure4.py
+++ lineage/figures/figure4.py
@@ -1,13 +1,16 @@
 """
 This creates Figure 4.
 """
-from .figureCommon import getSetup
+from .figureCommon import subplotLabel, getSetup
 
 
 def makeFigure():
-    """ makes figure 4 """
-
+    """ makes figure 1 """
     # Get list of axis objects
-    _, f = getSetup((12, 3), (1, 3))
+    ax, f = getSetup((7, 6), (3, 4))
+
+    subplotLabel(ax[0], 'A')
+
+    f.tight_layout()
 
     return f
