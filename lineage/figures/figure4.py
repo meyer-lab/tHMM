e2c02ace277a3db3d7acb4a9cd80f6b9b1c7f25e
Shakthi Visagan <shak360@gmail.com>
2019-10-29 12:10:53 -0700

    fixing dumb error in figure 4 linting demon

--- lineage/figures/figure4.py
+++ lineage/figures/figure4.py
@@ -79,15 +79,7 @@ def accuracy_increased_cells():
         x.append(len(lineage.output_lineage))
 
         # Analyzing the lineages
-        _, _, all_states, tHMMobj, _, _ = Analyze(X1, 1)
-        
-        # Collecting the accuracies of the lineages
-        acc1 = accuracy(tHMMobj, all_states)[0] * 100
-        while acc1 < 50:
-            # Analyzing the lineages
-            _, _, all_states, tHMMobj, _, _ = Analyze(X1, 2)
-            # Collecting the accuracies of the lineages
-            acc1 = accuracy(tHMMobj, all_states)[0] * 100
+        _, _, _, tHMMobj, _, _ = Analyze(X1, 1)
 
         bern.append(tHMMobj.estimate.E[0].bern_p)
         gamma_a.append(tHMMobj.estimate.E[0].gamma_a)
