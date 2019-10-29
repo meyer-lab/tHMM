8cd558d7c7762e48c6848688fee19fd3b5ebf7c4
Shakthi Visagan <shak360@gmail.com>
2019-10-29 10:29:56 -0700

    mmore work on figure 4 linting

--- lineage/figures/figure4.py
+++ lineage/figures/figure4.py
@@ -79,13 +79,15 @@ def accuracy_increased_cells():
         x.append(len(lineage.output_lineage))
 
         # Analyzing the lineages
-        deltas, _, all_states, tHMMobj, _, _ = Analyze(X1, 1)
+        _, _, all_states, tHMMobj, _, _ = Analyze(X1, 1)
         
         # Collecting the accuracies of the lineages
         acc1 = accuracy(tHMMobj, all_states)[0] * 100
         while acc1 < 50:
             # Analyzing the lineages
-            deltas, _, all_states, tHMMobj, _, _ = Analyze(X1, 2)
+            _, _, all_states, tHMMobj, _, _ = Analyze(X1, 2)
+            # Collecting the accuracies of the lineages
+            acc1 = accuracy(tHMMobj, all_states)[0] * 100
 
         bern.append(tHMMobj.estimate.E[0].bern_p)
         gamma_a.append(tHMMobj.estimate.E[0].gamma_a)
--- lineage/figures/figure6.py
+++ lineage/figures/figure6.py
@@ -86,7 +86,6 @@ def accuracy_increased_cells():
         while acc1 < 50:
             # Analyzing the lineages
             deltas, _, all_states, tHMMobj, _, _ = Analyze(X1, 2)
-
             # Collecting the accuracies of the lineages
             acc1 = accuracy(tHMMobj, all_states)[0] * 100
 
