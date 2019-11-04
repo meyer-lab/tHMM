6bfdd9801e3e5d1be5c0607735603fa6f7374c05
Shakthi Visagan <shak360@gmail.com>
2019-11-03 22:27:13 -0800

    more work

--- lineage/figures/figure2.py
+++ lineage/figures/figure2.py
@@ -40,39 +40,70 @@ def makeFigure():
     E = [state_obj0, state_obj1]
 
     # creating a population
-    population = []
+    population_unpruned = []
+    population_pruned = []
     desired_experiment_time = 300
     for i in range(20):
-        population.append(LineageTree(pi, T, E, (2**12) - 1, desired_experiment_time=desired_experiment_time, prune_condition='both', prune_boolean=True))
+        population_unpruned.append(LineageTree(pi, T, E, (2**12) - 1, desired_experiment_time=desired_experiment_time, prune_condition='fate', prune_boolean=False))
+        population_pruned.append(LineageTree(pi, T, E, (2**12) - 1, desired_experiment_time=desired_experiment_time, prune_condition='both', prune_boolean=True))
 
-    hist = track_population_generation_histogram(population)
+    hist_unpruned = track_population_generation_histogram(population_unpruned)
+    hist_pruned = track_population_generation_histogram(population_pruned)
 
     # Get list of axis objects
-    ax, f = getSetup((16, 6), (1, 2))
+    ax, f = getSetup((16, 16), (2, 2))
+    
+    # unpruned
 
-    x_0 = [i for i in range(len(hist[0]))]
-    x_1 = [i for i in range(len(hist[1]))]
+    x_0_unp = [i for i in range(len(hist_unpruned[0]))]
+    x_1_unp = [i for i in range(len(hist_unpruned[1]))]
 
-    ax[0].set_xlim([-0.01, 13])
+    ax[0].set_xlim([-0.01, 12])
     ax[0].set_xlabel(r'Generation')
-    ax[0].bar(x_0, hist[0], color='#F9Cb9C', label='Resistant')
-    ax[0].bar(x_1, hist[1], bottom=hist[0], color='#A4C2F4', label='Susceptible')
-    ax[0].set_ylabel('Number of alive Cells')
-    ax[0].set_title('Pruned population growth')
+    ax[0].bar(x_0_unp, hist_unpruned[0], color='#F9Cb9C', label='Resistant')
+    ax[0].bar(x_1_unp, hist_unpruned[1], bottom=hist_unpruned[0], color='#A4C2F4', label='Susceptible')
+    ax[0].set_ylabel('Number of alive cells')
+    ax[0].set_title('Unpruned population growth')
     ax[0].grid(linestyle='--')
 
-    y_0 = [a / (a + b) for a, b in zip(hist[0], hist[1])]
-    y_1 = [b / (a + b) for a, b in zip(hist[0], hist[1])]
+    y_0_unp = [a/(a+b) for a, b in zip(hist_unpruned[0], hist_unpruned[1])]
+    y_1_unp = [b/(a+b) for a, b in zip(hist_unpruned[0], hist_unpruned[1])]
 
-    ax[1].set_xlim([-0.01, 13])
+    ax[1].set_xlim([-0.01, 12])
     ax[1].set_xlabel(r'Generation')
-    ax[1].plot(x_0, y_0, color='#F9Cb9C', label='Resistant')
-    ax[1].plot(x_1, y_1, color='#A4C2F4', label='Susceptible')
-    ax[1].set_ylabel('Proportion of alive Cells')
+    ax[1].plot(x_0_unp, y_0_unp, color='#F9Cb9C', label='Resistant')
+    ax[1].plot(x_1_unp, y_1_unp, color='#A4C2F4', label='Susceptible')
+    ax[1].set_ylabel('Proportion of alive cells')
     ax[1].set_ylim([-0.01, 1.01])
-    ax[1].set_title('Pruned population distribution')
+    ax[1].set_title('Unpruned population distribution')
     ax[1].grid(linestyle='--')
     ax[1].legend()
+    
+    # pruned
+    
+    x_0_p = [i for i in range(len(hist_pruned[0]))]
+    x_1_p = [i for i in range(len(hist_pruned[1]))]
+
+    ax[2].set_xlim([-0.01, 12])
+    ax[2].set_xlabel(r'Generation')
+    ax[2].bar(x_0_p, hist_pruned[0], color='#F9Cb9C', label='Resistant')
+    ax[2].bar(x_1_p, hist_pruned[1], bottom=hist_pruned[0], color='#A4C2F4', label='Susceptible')
+    ax[2].set_ylabel('Number of alive cells')
+    ax[2].set_title('Pruned population growth')
+    ax[2].grid(linestyle='--')
+
+    y_0_p = [a/(a+b) for a, b in zip(hist_pruned[0], hist_pruned[1])]
+    y_1_p = [b/(a+b) for a, b in zip(hist_pruned[0], hist_pruned[1])]
+
+    ax[3].set_xlim([-0.01, 12])
+    ax[3].set_xlabel(r'Generation')
+    ax[3].plot(x_0_p, y_0_p, color='#F9Cb9C', label='Resistant')
+    ax[3].plot(x_1_p, y_1_p, color='#A4C2F4', label='Susceptible')
+    ax[3].set_ylabel('Proportion of alive cells')
+    ax[3].set_ylim([-0.01, 1.01])
+    ax[3].set_title('Pruned population distribution')
+    ax[3].grid(linestyle='--')
+    ax[3].legend()
 
     f.tight_layout()
 
