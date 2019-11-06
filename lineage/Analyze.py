40a4b3f76196dc50292346404a376cc7fac6551c
farnazmdi <farnazmd95@gmail.com>
2019-11-06 11:43:11 -0800

    the KL function added to analyze

--- lineage/Analyze.py
+++ lineage/Analyze.py
@@ -1,6 +1,7 @@
 '''Calls the tHMM functions and outputs the parameters needed to generate the Figures'''
 import copy as cp
 import numpy as np
+import random
 from .BaumWelch import fit
 from .Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
 from .UpwardRecursion import get_leaf_Normalizing_Factors, get_leaf_betas, get_nonleaf_NF_and_betas, calculate_log_likelihood
@@ -231,60 +232,3 @@ def kl_divergence(p, q):
         KL(P||Q) = Integral[ P(x) log(P(x)/Q(x)) ] for continuous distributions,
         and summation instead of integral, for discrete distributions. """
     return np.sum(np.where(p != 0, p * np.log(p / q), 0))
-
-def KL_analyze():
-    """ Assuming we have 2-state model """
-    pi = np.array([0.6, 0.4], dtype="float")
-
-    # T: transition probability matrix
-    T = np.array([[0.65, 0.35],
-                  [0.35, 0.65]])
-
-    gamma_a0List = [5.0, 10.0, 15.0, 12.0]
-    gamma_scale0List = [2.0, 2.0, 2.0, 3.3]
-    gamma_a1List = [23.0, 20.0, 17.0, 12.0]
-    gamma_scale1List = [3.0, 3.0, 3.0, 3.3]
-
-    gammaKL = []
-    acc = []
-    for i in range(4):
-        state_obj0 = StateDistribution(state0, bern_p0, gamma_a0List[i], gamma_loc, gamma_scale0List[i])
-        state_obj1 = StateDistribution(state1, bern_p1, gamma_a1List[i], gamma_loc,  gamma_scale1List[i])
-
-        E = [state_obj0, state_obj1]
-        lineageObj = LineageTree(pi, T, E, desired_num_cells=2**11 -1, desired_experiment_time=100000, prune_condition='both', prune_boolean=False)
-        X = [lineageObj]
-        states = [cell.state for cell in lineageObj.output_lineage]
-        deltas, state_ptrs, all_states, tHMMobj, NF, LL = Analyze(X, 2)
-
-        # find the accuracy
-        temp = accuracy(tHMMobj, all_states)
-        acc.append(temp[0])
-
-        # find the KL divergence
-        state0obs=[]
-        state1obs=[]
-
-        for indx, cell in enumerate(lineageObj.output_lineage):
-            if all_states[0][indx] == 0:
-                state0obs.append(cell.obs[1])
-            elif all_states[0][indx] == 1:
-                state1obs.append(cell.obs[1])
-        p=scipy.stats.gamma.pdf(state0obs, a=gamma_a0List[i], loc=gamma_loc, scale=gamma_scale0List[i])
-        q=scipy.stats.gamma.pdf(state1obs, a=gamma_a1List[i], loc=gamma_loc, scale=gamma_scale1List[i])
-
-        size = min(p.shape[0], q.shape[0])
-        if size == 0:
-            raise ValueError('the number of cells predicted in one of the states is zero! ')
-        else:
-            pprime = random.sample(list(p), size)
-            qprime = random.sample(list(q), size)
-        gammaKL.append(kl_divergence(np.asarray(pprime), np.asarray(qprime)))
-
-    return KLgamma
-    
-        
-        
-        
-        
-    
