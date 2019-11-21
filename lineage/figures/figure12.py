baff2fbfa8b8d0f036b5086046f543f6c81bde30
farnazmdi <farnazmd95@gmail.com>
2019-11-20 16:34:03 -0800

    linting

--- lineage/figures/figure12.py
+++ lineage/figures/figure12.py
@@ -5,17 +5,16 @@ Purpose: Generates figure 12.
 Figure 12 is the KL divergence for different sets of parameters
 for a two state model. It plots the KL-divergence against accuracy.
 """
+from ..StateDistribution import StateDistribution
+from ..LineageTree import LineageTree
+from ..Analyze import accuracy, Analyze
+from .figureCommon import getSetup
 import numpy as np
 import random
 import scipy.stats as sp
 import pandas as pd
 import seaborn as sns
 sns.set(style="whitegrid", palette="pastel", color_codes=True)
-
-from .figureCommon import getSetup
-from ..Analyze import accuracy, Analyze
-from ..LineageTree import LineageTree
-from ..StateDistribution import StateDistribution
 
 
 def makeFigure():
@@ -30,6 +29,7 @@ def makeFigure():
     figure_maker(ax, accuracy, KL_gamma, dists)
 
     return f
+
 
 def KLdivergence():
     """ Assuming we have 2-state model """
@@ -47,9 +47,9 @@ def KLdivergence():
     bern_p0 = 0.99
     bern_p1 = 0.88
     a0 = np.linspace(5.0, 17.0, 10)
-    scale0 = 10*([2.0])
+    scale0 = 10 * ([2.0])
     a1 = np.linspace(30.0, 17.0, 10)
-    scale1 = 10*([3.0])
+    scale1 = 10 * ([3.0])
 
     gammaKL1 = []
     gammaKL_total = []
@@ -57,7 +57,7 @@ def KLdivergence():
     acc_total = []
     acc = []
 
-    assert len(a0) == len(scale0) == len(a1) == len(scale1),"the length of the parameters are not the same!"
+    assert len(a0) == len(scale0) == len(a1) == len(scale1), "the length of the parameters are not the same!"
 
     for i in range(a0.shape[0]):
         state_obj0 = StateDistribution(state0, bern_p0, a0[i], gamma_loc, scale0[i])
@@ -65,7 +65,7 @@ def KLdivergence():
 
         E = [state_obj0, state_obj1]
         lineage = LineageTree(pi, T, E, (2**12) - 1,
-                              desired_experiment_time=600, 
+                              desired_experiment_time=600,
                               prune_condition='both',
                               prune_boolean=True)
         while len(lineage.output_lineage) < 16:
@@ -78,8 +78,8 @@ def KLdivergence():
         _, obs0 = list(zip(*lineage.lineage_stats[0].full_lin_cells_obs))
         _, obs1 = list(zip(*lineage.lineage_stats[1].full_lin_cells_obs))
 
-        p=sp.gamma.pdf(obs0, a=a0[i], loc=gamma_loc, scale=scale0[i])
-        q=sp.gamma.pdf(obs1, a=a0[i], loc=gamma_loc, scale=scale1[i])
+        p = sp.gamma.pdf(obs0, a=a0[i], loc=gamma_loc, scale=scale0[i])
+        q = sp.gamma.pdf(obs1, a=a0[i], loc=gamma_loc, scale=scale1[i])
 
         size = min(p.shape[0], q.shape[0])
         if size == 0:
@@ -92,7 +92,7 @@ def KLdivergence():
 
         X = [lineage]
         states = [cell.state for cell in lineage.output_lineage]
-        num_iter=5 # for every KL value, it runs the model 5 times
+        num_iter = 5  # for every KL value, it runs the model 5 times
         # to get the accuracy and returns the average accuracy for 5 iterations.
         for j in range(num_iter):
             _, _, all_states, tHMMobj, _, _ = Analyze(X, 2)
@@ -104,7 +104,7 @@ def KLdivergence():
     gammaKL_total.append(gammaKL1)
 
     for j in range(10):
-        tmp = np.sum(acc1[j:num_iter*(j+1)])/len(acc1[j:num_iter*(j+1)])
+        tmp = np.sum(acc1[j:num_iter * (j + 1)]) / len(acc1[j:num_iter * (j + 1)])
         acc.append(tmp)
     return acc, gammaKL_total[0]
 
@@ -112,57 +112,57 @@ def KLdivergence():
 def distributionPlot():
     """ Here we plot the distributions used in the KL divergence plot
     to show how far the distributions are for each point.
-    For 10 distributions, 500 random variables are generated 
+    For 10 distributions, 500 random variables are generated
     and to be used by the violinplot they are concatenated in
-    "lifetime" column of the DataFrame, and to separate them, 
+    "lifetime" column of the DataFrame, and to separate them,
     there is another column named "distributions". """
     gamma_loc = 0
     bern_p0 = 0.99
     bern_p1 = 0.88
     a0 = np.linspace(5.0, 17.0, 10)
-    scale0 = 10*([2.0])
+    scale0 = 10 * ([2.0])
     a1 = np.linspace(30.0, 17.0, 10)
-    scale1 = 10*([3.0])
+    scale1 = 10 * ([3.0])
 
-    dist1 = np.concatenate((sp.gamma.rvs(a=a0[9], loc=gamma_loc, scale=scale0[9], 
-                         size=500), sp.gamma.rvs(a=a1[9], loc=gamma_loc,
-                         scale=scale1[9], 
-                         size=500), sp.gamma.rvs(a=a0[8], 
-                         loc=gamma_loc, scale=scale0[8], 
-                         size=500), sp.gamma.rvs(a=a1[8], 
-                         loc=gamma_loc, scale=scale1[8], 
-                         size=500), sp.gamma.rvs(a=a0[7],
-                         loc=gamma_loc, scale=scale0[7], size=500), 
-                         sp.gamma.rvs(a=a1[7], loc=gamma_loc,
-                         scale=scale1[7], size=500), sp.gamma.rvs(a=a0[6],
-                         loc=gamma_loc, scale=scale0[6], size=500),
-                         sp.gamma.rvs(a=a1[6], loc=gamma_loc,
-                         scale=scale1[6], size=500),sp.gamma.rvs(a=a0[5],
-                         loc=gamma_loc, scale=scale0[5], size=500),
-                         sp.gamma.rvs(a=a1[5], loc=gamma_loc,
-                         scale=scale1[5], size=500),
-                         sp.gamma.rvs(a=a0[4], loc=gamma_loc,
-                         scale=scale0[4], size=500), sp.gamma.rvs(a=a1[4],
-                         loc=gamma_loc, scale=scale1[4], size=500),
-                         sp.gamma.rvs(a=a0[3], loc=gamma_loc, scale=scale0[3],
-                         size=500), sp.gamma.rvs(a=a1[3], loc=gamma_loc,
-                         scale=scale1[3], size=500), sp.gamma.rvs(a=a0[2],
-                         loc=gamma_loc, scale=scale0[2], size=500), 
-                         sp.gamma.rvs(a=a1[2], loc=gamma_loc, scale=scale1[2],
-                         size=500), sp.gamma.rvs(a=a0[1], loc=gamma_loc,
-                         scale=scale0[1], size=500), sp.gamma.rvs(a=a1[1],
-                         loc=gamma_loc, scale=scale1[1], size=500),
-                         sp.gamma.rvs(a=a0[0], loc=gamma_loc, scale=scale0[0],
-                         size=500), sp.gamma.rvs(a=a1[0], loc=gamma_loc,
-                         scale=scale1[0], size=500)))
-    
-    dists = pd.DataFrame(columns = ['lifetime [hr]', 'distributions', 'hues'])
-    dists['lifetime [hr]']=dist1
-    dists['distributions']=1000*(['d1']) + 1000*(['d2']) + 1000*(['d3']) + 1000*(['d4']) + \
-    1000*(['d5']) + 1000*(['d6']) + 1000*(['d7']) + 1000*(['d8']) + 1000*(['d9']) + 1000*(['d10'])
-    dists['hues'] = 500*[1] + 500*[2] + 500*[1] + 500*[2] + 500*[1] + 500*[2] + 500*[1] + 500*[2] \
-    + 500*[1] + 500*[2] + 500*[1] + 500*[2] + 500*[1] + 500*[2] + 500*[1] + 500*[2] + 500*[1] + 500*[2] \
-    + 500*[1] + 500*[2]
+    dist1 = np.concatenate((sp.gamma.rvs(a=a0[9], loc=gamma_loc, scale=scale0[9],
+                                         size=500), sp.gamma.rvs(a=a1[9], loc=gamma_loc,
+                                                                 scale=scale1[9],
+                                                                 size=500), sp.gamma.rvs(a=a0[8],
+                                                                                         loc=gamma_loc, scale=scale0[8],
+                                                                                         size=500), sp.gamma.rvs(a=a1[8],
+                                                                                                                 loc=gamma_loc, scale=scale1[8],
+                                                                                                                 size=500), sp.gamma.rvs(a=a0[7],
+                                                                                                                                         loc=gamma_loc, scale=scale0[7], size=500),
+                            sp.gamma.rvs(a=a1[7], loc=gamma_loc,
+                                         scale=scale1[7], size=500), sp.gamma.rvs(a=a0[6],
+                                                                                  loc=gamma_loc, scale=scale0[6], size=500),
+                            sp.gamma.rvs(a=a1[6], loc=gamma_loc,
+                                         scale=scale1[6], size=500), sp.gamma.rvs(a=a0[5],
+                                                                                  loc=gamma_loc, scale=scale0[5], size=500),
+                            sp.gamma.rvs(a=a1[5], loc=gamma_loc,
+                                         scale=scale1[5], size=500),
+                            sp.gamma.rvs(a=a0[4], loc=gamma_loc,
+                                         scale=scale0[4], size=500), sp.gamma.rvs(a=a1[4],
+                                                                                  loc=gamma_loc, scale=scale1[4], size=500),
+                            sp.gamma.rvs(a=a0[3], loc=gamma_loc, scale=scale0[3],
+                                         size=500), sp.gamma.rvs(a=a1[3], loc=gamma_loc,
+                                                                 scale=scale1[3], size=500), sp.gamma.rvs(a=a0[2],
+                                                                                                          loc=gamma_loc, scale=scale0[2], size=500),
+                            sp.gamma.rvs(a=a1[2], loc=gamma_loc, scale=scale1[2],
+                                         size=500), sp.gamma.rvs(a=a0[1], loc=gamma_loc,
+                                                                 scale=scale0[1], size=500), sp.gamma.rvs(a=a1[1],
+                                                                                                          loc=gamma_loc, scale=scale1[1], size=500),
+                            sp.gamma.rvs(a=a0[0], loc=gamma_loc, scale=scale0[0],
+                                         size=500), sp.gamma.rvs(a=a1[0], loc=gamma_loc,
+                                                                 scale=scale1[0], size=500)))
+
+    dists = pd.DataFrame(columns=['lifetime [hr]', 'distributions', 'hues'])
+    dists['lifetime [hr]'] = dist1
+    dists['distributions'] = 1000 * (['d1']) + 1000 * (['d2']) + 1000 * (['d3']) + 1000 * (['d4']) + \
+        1000 * (['d5']) + 1000 * (['d6']) + 1000 * (['d7']) + 1000 * (['d8']) + 1000 * (['d9']) + 1000 * (['d10'])
+    dists['hues'] = 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] \
+        + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] \
+        + 500 * [1] + 500 * [2]
     return dists
 
 
@@ -171,7 +171,7 @@ def figure_maker(ax, accuracy, KL_gamma, dists):
     i = 0
     ax[i].set_xlabel('KL divergence')
     ax[i].set_ylim(0, 110)
-    ax[i].set_xlim(0, 1.07*max(KL_gamma))
+    ax[i].set_xlim(0, 1.07 * max(KL_gamma))
     ax[i].scatter(KL_gamma, accuracy, c='k', marker="o", edgecolors='k', alpha=0.25)
     ax[i].set_ylabel(r'Accuracy [\%]')
     ax[i].axhline(y=100, linestyle='--', linewidth=2, color='k', alpha=1)
@@ -181,6 +181,6 @@ def figure_maker(ax, accuracy, KL_gamma, dists):
     i += 1
 
     sns.violinplot(x="distributions", y="lifetime [hr]",
-               inner="quart", palette="muted", 
-               split=True, hue="hues", data=dists, ax=ax[i])
+                   inner="quart", palette="muted",
+                   split=True, hue="hues", data=dists, ax=ax[i])
     sns.despine(left=True, ax=ax[i])
