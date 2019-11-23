62b395bc0256e9cd4cc880bb131b0e6eb7320d1b
farnazmdi <farnazmd95@gmail.com>
2019-11-22 16:30:27 -0800

    warnings remoooved

--- lineage/figures/figure12.py
+++ lineage/figures/figure12.py
@@ -6,8 +6,8 @@ Figure 12 is the KL divergence for different sets of parameters
 for a two state model. It plots the KL-divergence against accuracy.
 """
 import itertools
-import numpy as np
 import random
+import numpy as np
 import scipy.stats as sp
 import pandas as pd
 import seaborn as sns
@@ -26,9 +26,9 @@ def makeFigure():
 
     # Get list of axis objects
     ax, f = getSetup((20, 6), (1, 2))
-    accuracy, KL_gamma = KLdivergence()
+    accuracyy, KL_gamma = KLdivergence()
     dists = distributionPlot()
-    figure_maker(ax, accuracy, KL_gamma, dists)
+    figure_maker(ax, accuracyy, KL_gamma, dists)
 
     return f
 
@@ -56,12 +56,11 @@ def KLdivergence():
     gammaKL1 = []
     gammaKL_total = []
     acc1 = []
-    acc_total = []
     acc = []
 
     assert len(a0) == len(scale0) == len(a1) == len(scale1)
-
-    for i in range(a0.shape[0]):
+    a0Range = a0.shape[0]
+    for i in range(a0Range):
         state_obj0 = StateDistribution(state0,
                                        bern_p0,
                                        a0[i],
@@ -92,16 +91,13 @@ def KLdivergence():
         q = sp.gamma.pdf(obs1, a=a0[i], loc=gamma_loc, scale=scale1[i])
 
         size = min(p.shape[0], q.shape[0])
-        if size == 0:
-            raise ValueError('# of cells in one of the states is zero!')
-        else:
-            pprime = random.sample(list(p), size)
-            qprime = random.sample(list(q), size)
+        assert size > 0, "pdf array for KL is empty"
+        pprime = random.sample(list(p), size)
+        qprime = random.sample(list(q), size)
             # find the KL divergence
         gammaKL1.append(sp.entropy(np.asarray(pprime), np.asarray(qprime)))
 
         X = [lineage]
-        states = [cell.state for cell in lineage.output_lineage]
         num_iter = 5  # for every KL value, it runs the model 5 times
         # accuracy and returns the avg accuracy for 5 iters
         for j in range(num_iter):
@@ -128,8 +124,6 @@ def distributionPlot():
     "lifetime" column of the DataFrame, and to separate them,
     there is another column named "distributions". """
     gamma_loc = 0
-    bern_p0 = 0.99
-    bern_p1 = 0.88
     a0 = np.linspace(5.0, 17.0, 10)
     scale0 = 2.0
     a1 = np.linspace(30.0, 17.0, 10)
@@ -137,12 +131,12 @@ def distributionPlot():
     total = []
     for i in range(10):
         a = list(sp.gamma.rvs(a=a0[9-i], loc=gamma_loc,
-                                          scale=scale0,
-                                          size=500))
+                              scale=scale0,
+                              size=500))
         total.append(a)
         b = list(sp.gamma.rvs(a=a1[9-i], loc=gamma_loc,
-                                      scale=scale1,
-                                      size=500))
+                              scale=scale1,
+                              size=500))
         total.append(b)
 
     TOdf = list(itertools.chain.from_iterable(total))
@@ -161,13 +155,15 @@ def distributionPlot():
     return dists
 
 
-def figure_maker(ax, accuracy, KL_gamma, dists):
+def figure_maker(ax, accuracyyy, KL_gamma, dists):
+    """ makes the figure showing
+    distributions get farther """
 
     i = 0
     ax[i].set_xlabel('KL divergence')
     ax[i].set_ylim(0, 110)
     ax[i].set_xlim(0, 1.07 * max(KL_gamma))
-    ax[i].scatter(KL_gamma, accuracy, c='k', marker="o", edgecolors='k', alpha=0.25)
+    ax[i].scatter(KL_gamma, accuracyyy, c='k', marker="o", edgecolors='k', alpha=0.25)
     ax[i].set_ylabel(r'Accuracy [\%]')
     ax[i].axhline(y=100, linestyle='--', linewidth=2, color='k', alpha=1)
     ax[i].set_title('KL divergence for two state model')
