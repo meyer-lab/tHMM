2b80dc84abe013a71eca6191ce6372e525e93487
farnazmdi <farnazmd95@gmail.com>
2019-12-10 12:37:10 -0800

    linting

--- lineage/figures/figure10.py
+++ lineage/figures/figure10.py
@@ -2,8 +2,6 @@
 File: figure10.py
 Purpose: Generates figure 10.
 """
-import numpy as np
-import matplotlib.pyplot as plt
 
 from .figureCommon import getSetup
 
@@ -13,6 +11,6 @@ def makeFigure():
     Makes figure 10.
     """
 
-    ax, f = getSetup((21, 6), (1, 3))
+    _, f = getSetup((21, 6), (1, 3))
 
     return f
--- lineage/figures/figure3.py
+++ lineage/figures/figure3.py
@@ -2,8 +2,6 @@
 File: figure3.py
 Purpose: Generates figure 3.
 """
-import numpy as np
-import matplotlib.pyplot as plt
 
 from .figureCommon import getSetup
 
@@ -13,6 +11,6 @@ def makeFigure():
     Makes figure 3.
     """
 
-    ax, f = getSetup((21, 6), (1, 3))
+    _, f = getSetup((21, 6), (1, 3))
 
     return f
