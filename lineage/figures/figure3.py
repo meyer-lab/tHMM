987adee7f7170c179dd3ad5a5b759981d13afc6e
farnazmdi <farnazmd95@gmail.com>
2020-01-15 21:23:22 -0800

    removing empty figures

--- lineage/figures/figure3.py
+++ lineage/figures/figure3.py
@@ -1,16 +1,225 @@
 """
-File: figure3.py
-Purpose: Generates figure 3.
+File: figure8.py
+Purpose: Generates figure 7 and 8.
+Figure 7 is the accuracy and transition matrix parameter estimation
+for a single pruned lineage with heterogeneity (two true states).
+Figure 8 is the parameter estimation for a single pruned lineage with heterogeneity (two true states).
 """
+import numpy as np
+from matplotlib import gridspec, pyplot as plt
 
-from .figureCommon import getSetup
+from .figureCommon import moving_average
+from ..Analyze import accuracy, Analyze
+from ..LineageTree import LineageTree
+from ..StateDistribution import StateDistribution
+
+
+def getSetup(figsize):
+    """Setup figures."""
+
+    plt.rc('font', **{'family': 'sans-serif', 'size': 25})
+    # for Palatino and other serif fonts use:
+    # rc('font',**{'family':'serif','serif':['Palatino']})
+    plt.rc('text', usetex=True)
+    plt.rc('xtick', **{'labelsize': 'medium'})
+    plt.rc('ytick', **{'labelsize': 'medium'})
+
+    # Setup plotting space
+    f = plt.figure(figsize=figsize)
+
+    # Make grid
+    gs1 = gridspec.GridSpec(2, 6, figure=f)
+
+    # Get list of axis objects
+    ax = [f.add_subplot(gs1[0, 0:2]), f.add_subplot(gs1[0, 2:4]),
+          f.add_subplot(gs1[0, 4:6]), f.add_subplot(gs1[-1, 1:3]),
+          f.add_subplot(gs1[-1, 3:5])]
+
+    return (ax, f)
 
 
 def makeFigure():
     """
-    Makes figure 3.
+    Makes figures 7 & 8.
     """
 
-    _, f = getSetup((21, 6), (1, 3))
+    # Get list of axis objects
+    ax, f = getSetup((21, 12))
+#     f.subplot2grid(shape, loc, rowspan=1, colspan=1)
+    x, accuracies, tr, pi, bern_pruned, bern_p0, bern_p1, gamma_a_pruned, gamma_a0, gamma_a1, gamma_scale_pruned, gamma_scale0, gamma_scale1 = accuracy_increased_cells()
+    figure_maker(ax, x, accuracies, tr, pi, bern_pruned, bern_p0, bern_p1, gamma_a_pruned, gamma_a0, gamma_a1, gamma_scale_pruned, gamma_scale0, gamma_scale1)
+    f.tight_layout()
 
     return f
+
+
+def accuracy_increased_cells():
+    """
+    Calculates accuracy and transition rate estimation over an increasing number of cells in a lineage for an pruned two-state model.
+    """
+
+    # pi: the initial probability vector
+    piiii = np.array([0.6, 0.4], dtype="float")
+
+    # T: transition probability matrix
+    T = np.array([[0.75, 0.25],
+                  [0.15, 0.85]], dtype="float")
+
+    # State 0 parameters "Resistant"
+    state0 = 0
+    bern_p0 = 0.99
+    gamma_a0 = 20
+    gamma_loc = 0
+    gamma_scale0 = 5
+
+    # State 1 parameters "Susceptible"
+    state1 = 1
+    bern_p1 = 0.88
+    gamma_a1 = 10
+    gamma_scale1 = 1
+
+    state_obj0 = StateDistribution(state0, bern_p0, gamma_a0, gamma_loc, gamma_scale0)
+    state_obj1 = StateDistribution(state1, bern_p1, gamma_a1, gamma_loc, gamma_scale1)
+    E = [state_obj0, state_obj1]
+
+    x = []
+    accuracies = []
+    tr = []
+    pi = []
+    bern_pruned = []
+    gamma_a_pruned = []
+    gamma_scale_pruned = []
+
+    times = np.linspace(100, 1000, 25)
+
+    for experiment_time in times:
+        # Creating an unpruned and pruned lineage
+        lineage = LineageTree(piiii, T, E, (2**12) - 1, experiment_time, prune_condition='both', prune_boolean=True)
+        while len(lineage.output_lineage) < 16:
+            del lineage
+            lineage = LineageTree(piiii, T, E, (2**12) - 1, experiment_time, prune_condition='both', prune_boolean=True)
+
+        # Setting then into a list or a population of lineages and collecting the length of each lineage
+        X1 = [lineage]
+        x.append(len(lineage.output_lineage))
+
+        # Analyzing the lineages
+        deltas, _, all_states, tHMMobj, _, _ = Analyze(X1, 2)
+
+        # Collecting the accuracies of the lineages
+        acc1 = accuracy(tHMMobj, all_states)[0] * 100
+        while acc1 < 50:
+            # Analyzing the lineages
+            deltas, _, all_states, tHMMobj, _, _ = Analyze(X1, 2)
+
+            # Collecting the accuracies of the lineages
+            acc1 = accuracy(tHMMobj, all_states)[0] * 100
+        accuracies.append(acc1)
+
+        # Transition and Pi estimates
+        transition_mat = tHMMobj.estimate.T  # unpruned
+
+        temp1 = T - transition_mat
+        tr.append(np.linalg.norm(temp1))
+
+        pi_mat = tHMMobj.estimate.pi
+        t1 = piiii - pi_mat
+        pi.append(np.linalg.norm(t1))
+
+        # Collecting the parameter estimations
+        bern_p_total = ()
+        gamma_a_total = ()
+        gamma_scale_total = ()
+
+        for state in range(tHMMobj.numStates):
+            bern_p_total += (tHMMobj.estimate.E[state].bern_p,)
+            gamma_a_total += (tHMMobj.estimate.E[state].gamma_a,)
+            gamma_scale_total += (tHMMobj.estimate.E[state].gamma_scale,)
+
+        bern_pruned.append(bern_p_total)
+        gamma_a_pruned.append(gamma_a_total)
+        gamma_scale_pruned.append(gamma_scale_total)
+
+    return x, accuracies, tr, pi, bern_pruned, bern_p0, bern_p1, gamma_a_pruned, gamma_a0, gamma_a1, gamma_scale_pruned, gamma_scale0, gamma_scale1
+
+
+def figure_maker(ax, x, accuracies, tr, pi, bern_pruned, bern_p0, bern_p1, gamma_a_pruned, gamma_a0, gamma_a1, gamma_scale_pruned, gamma_scale0, gamma_scale1):
+    """
+    Makes figure 8.
+    """
+    i = 0
+    res = [[i for i, j in bern_pruned], [j for i, j in bern_pruned]]
+    ax[i].set_xlim((16, int(np.ceil(4 * max(x)))))
+    ax[i].set_xlabel('Number of Cells')
+    ax[i].scatter(x, res[0], c='#F9Cb9C', edgecolors='k', marker="o", alpha=0.5)
+    ax[i].scatter(x, res[1], c='#A4C2F4', edgecolors='k', marker="o", alpha=0.5)
+    ax[i].set_ylabel('Bernoulli $p$')
+    ax[i].set_ylim([0.85, 1.01])
+    ax[i].axhline(y=bern_p0, linestyle='--', linewidth=2, label='Resistant', color='#F9Cb9C', alpha=1)
+    ax[i].axhline(y=bern_p1, linestyle='--', linewidth=2, label='Susceptible', color='#A4C2F4', alpha=1)
+    ax[i].set_title(r'Bernoulli $p$')
+    ax[i].grid(linestyle='--')
+    ax[i].set_xscale('log', basex=2)
+    ax[i].tick_params(axis='both', which='major', grid_alpha=0.25)
+
+    i += 1
+    res = [[i for i, j in gamma_a_pruned], [j for i, j in gamma_a_pruned]]
+    ax[i].set_xlim((16, int(np.ceil(4 * max(x)))))
+    ax[i].set_xlabel('Number of Cells')
+    ax[i].scatter(x, res[0], c='#F9Cb9C', edgecolors='k', marker="o", alpha=0.5)
+    ax[i].scatter(x, res[1], c='#A4C2F4', edgecolors='k', marker="o", alpha=0.5)
+    ax[i].set_ylabel(r'Gamma $k$')
+    ax[i].set_ylim([5, 25])
+    ax[i].axhline(y=gamma_a0, linestyle='--', linewidth=2, label='Resistant', color='#F9Cb9C', alpha=1)
+    ax[i].axhline(y=gamma_a1, linestyle='--', linewidth=2, label='Susceptible', color='#A4C2F4', alpha=1)
+    ax[i].set_title(r'Gamma $k$')
+    ax[i].grid(linestyle='--')
+    ax[i].set_xscale('log', basex=2)
+    ax[i].tick_params(axis='both', which='major', grid_alpha=0.25)
+
+    i += 1
+    res = [[i for i, j in gamma_scale_pruned], [j for i, j in gamma_scale_pruned]]
+    ax[i].set_xlim((16, int(np.ceil(4 * max(x)))))
+    ax[i].set_xlabel('Number of Cells')
+    ax[i].scatter(x, res[0], c='#F9Cb9C', edgecolors='k', marker="o", alpha=0.5)
+    ax[i].scatter(x, res[1], c='#A4C2F4', edgecolors='k', marker="o", alpha=0.5)
+    ax[i].set_ylabel(r'Gamma $\theta$')
+    ax[i].set_ylim([0, 7])
+    ax[i].axhline(y=gamma_scale0, linestyle='--', linewidth=2, label='Resistant', color='#F9Cb9C', alpha=1)
+    ax[i].axhline(y=gamma_scale1, linestyle='--', linewidth=2, label='Susceptible', color='#A4C2F4', alpha=1)
+    ax[i].set_title(r'Gamma $\theta$')
+    ax[i].grid(linestyle='--')
+    ax[i].set_xscale('log', basex=2)
+    ax[i].tick_params(axis='both', which='major', grid_alpha=0.25)
+    ax[i].legend()
+
+    x_vs_acc = np.column_stack((x, accuracies))
+    sorted_x_vs_acc = x_vs_acc[np.argsort(x_vs_acc[:, 0])]
+
+    x_vs_tr = np.column_stack((x, tr))
+    sorted_x_vs_tr = x_vs_tr[np.argsort(x_vs_tr[:, 0])]
+
+    i += 1
+    ax[i].set_xlim((16, int(np.ceil(4 * max(x)))))
+    ax[i].set_xlabel('Number of Cells')
+    ax[i].set_ylim(0, 110)
+    ax[i].scatter(x, accuracies, c='k', marker="o", label='Accuracy', edgecolors='k', alpha=0.25)
+    ax[i].plot(sorted_x_vs_acc[:, 0][49:], moving_average(sorted_x_vs_acc[:, 1]), c='k', label='Moving Average')
+    ax[i].set_xscale('log', basex=2)
+    ax[i].set_ylabel(r'Accuracy [\%]')
+    ax[i].axhline(y=100, linestyle='--', linewidth=2, color='k', alpha=1)
+    ax[i].set_title('State Assignment Accuracy')
+    ax[i].grid(linestyle='--')
+    ax[i].tick_params(axis='both', which='major', grid_alpha=0.25)
+
+    i += 1
+    ax[i].set_xlim((16, int(np.ceil(4 * max(x)))))
+    ax[i].set_xlabel('Number of Cells')
+    ax[i].scatter(x, tr, c='k', marker="o", edgecolors='k', alpha=0.25)
+    ax[i].plot(sorted_x_vs_tr[:, 0][49:], moving_average(sorted_x_vs_tr[:, 1]), c='k', label='Moving Average')
+    ax[i].set_xscale('log', basex=2)
+    ax[i].set_ylabel(r'$||T-T_{est}||_{F}$')
+    ax[i].axhline(y=0, linestyle='--', linewidth=2, color='k', alpha=1)
+    ax[i].set_title('Transition Matrix Estimation')
+    ax[i].grid(linestyle='--')
+    ax[i].tick_params(axis='both', which='major', grid_alpha=0.25)
--- lineage/figures/figure6.py
+++ lineage/figures/figure6.py
@@ -1,17 +1,177 @@
 """
-File: figure6.py
-Purpose: Generates figure 6.
+File: figure12.py
+Purpose: Generates figure 12.
 
+Figure 12 is the KL divergence for different sets of parameters
+for a two state model. It plots the KL-divergence against accuracy.
 """
-
+import itertools
+import random
+import numpy as np
+import scipy.stats as sp
+import pandas as pd
+import seaborn as sns
+from ..StateDistribution import StateDistribution
+from ..LineageTree import LineageTree
+from ..Analyze import accuracy, Analyze
 from .figureCommon import getSetup
+
+sns.set(style="whitegrid", palette="pastel", color_codes=True)
 
 
 def makeFigure():
     """
-    Makes figure 6.
+    Makes figure 12.
     """
 
-    _, f = getSetup((21, 6), (1, 3))
+    # Get list of axis objects
+    ax, f = getSetup((20, 6), (1, 2))
+    accuracyy, KL_gamma = KLdivergence()
+    dists = distributionPlot()
+    figure_maker(ax, accuracyy, KL_gamma, dists)
 
     return f
+
+
+def KLdivergence():
+    """ Assuming we have 2-state model """
+
+    # pi: the initial probability vector
+    pi = np.array([0.5, 0.5], dtype="float")
+
+    # T: transition probability matrix
+    T = np.array([[0.50, 0.50],
+                  [0.50, 0.50]])
+
+    state0 = 0
+    state1 = 1
+    gamma_loc = 0
+    bern_p0 = 0.99
+    bern_p1 = 0.88
+    a0 = np.linspace(5.0, 17.0, 10)
+    scale0 = 10 * ([2.0])
+    a1 = np.linspace(30.0, 17.0, 10)
+    scale1 = 10 * ([3.0])
+
+    gammaKL1 = []
+    gammaKL_total = []
+    acc1 = []
+    acc = []
+
+    assert len(a0) == len(scale0) == len(a1) == len(scale1)
+    for i in range(len(a0)):
+        state_obj0 = StateDistribution(state0,
+                                       bern_p0,
+                                       a0[i],
+                                       gamma_loc,
+                                       scale0[i])
+        state_obj1 = StateDistribution(state1,
+                                       bern_p1,
+                                       a1[i],
+                                       gamma_loc,
+                                       scale1[i])
+
+        E = [state_obj0, state_obj1]
+        lineage = LineageTree(pi, T, E, (2**12) - 1,
+                              desired_experiment_time=600,
+                              prune_condition='both',
+                              prune_boolean=True)
+        while len(lineage.output_lineage) < 16:
+            del lineage
+            lineage = LineageTree(pi, T, E, (2**12) - 1,
+                                  desired_experiment_time=600,
+                                  prune_condition='both',
+                                  prune_boolean=True)
+
+        _, obs0 = list(zip(*lineage.lineage_stats[0].full_lin_cells_obs))
+        _, obs1 = list(zip(*lineage.lineage_stats[1].full_lin_cells_obs))
+
+        p = sp.gamma.pdf(obs0, a=a0[i], loc=gamma_loc, scale=scale0[i])
+        q = sp.gamma.pdf(obs1, a=a0[i], loc=gamma_loc, scale=scale1[i])
+
+        size = min(p.shape[0], q.shape[0])
+        assert size > 0, "pdf array for KL is empty"
+        pprime = random.sample(list(p), size)
+        qprime = random.sample(list(q), size)
+        # find the KL divergence
+        gammaKL1.append(sp.entropy(np.asarray(pprime), np.asarray(qprime)))
+
+        X = [lineage]
+        num_iter = 2  # for every KL value, it runs the model 2 times
+        # accuracy and returns the avg accuracy for 2 iters
+        for j in range(num_iter):
+            _, _, all_states, tHMMobj, _, _ = Analyze(X, 2)
+
+            # find the accuracy
+            temp = accuracy(tHMMobj, all_states)[0] * 100
+
+            acc1.append(temp)
+    gammaKL_total.append(gammaKL1)
+
+    for j in range(10):
+        tmp = np.sum(acc1[j:num_iter * (j + 1)]) \
+            / len(acc1[j:num_iter * (j + 1)])
+        acc.append(tmp)
+    return acc, gammaKL_total[0]
+
+
+def distributionPlot():
+    """ Here we plot the distributions used in the KL divergence plot
+    to show how far the distributions are for each point.
+    For 10 distributions, 500 random variables are generated
+    and to be used by the violinplot they are concatenated in
+    "lifetime" column of the DataFrame, and to separate them,
+    there is another column named "distributions". """
+    gamma_loc = 0
+    a0 = np.linspace(5.0, 17.0, 10)
+    scale0 = 2.0
+    a1 = np.linspace(30.0, 17.0, 10)
+    scale1 = 3.0
+    total = []
+    for i in range(10):
+        a = list(sp.gamma.rvs(a=a0[9 - i], loc=gamma_loc,
+                              scale=scale0,
+                              size=500))
+        total.append(a)
+        b = list(sp.gamma.rvs(a=a1[9 - i], loc=gamma_loc,
+                              scale=scale1,
+                              size=500))
+        total.append(b)
+
+    TOdf = list(itertools.chain.from_iterable(total))
+
+    dists = pd.DataFrame(columns=['distributions', 'hues'])
+    dists['distributions'] = 1000 * (['d1']) + 1000 * (['d2']) + \
+        1000 * (['d3']) + 1000 * (['d4']) + 1000 * (['d5']) + \
+        1000 * (['d6']) + 1000 * (['d7']) + 1000 * (['d8']) + \
+        1000 * (['d9']) + 1000 * (['d10'])
+    dists['hues'] = 500 * [1] + 500 * [2] + 500 * [1] + \
+        500 * [2] + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] \
+        + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] + \
+        500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] + 500 * [1] + 500 * [2] \
+        + 500 * [1] + 500 * [2]
+    dists['lifetime [hr]'] = TOdf
+    return dists
+
+
+def figure_maker(ax, accuracyyy, KL_gamma, dists):
+    """ makes the figure showing
+    distributions get farther """
+
+    i = 0
+    ax[i].set_xlabel('KL divergence')
+    ax[i].set_ylim(0, 110)
+    ax[i].set_xlim(0, 1.07 * max(KL_gamma))
+    ax[i].scatter(KL_gamma, accuracyyy,
+                  c='k', marker="o", edgecolors='k', alpha=0.25)
+    ax[i].set_ylabel(r'Accuracy [\%]')
+    ax[i].axhline(y=100, linestyle='--', linewidth=2, color='k', alpha=1)
+    ax[i].set_title('KL divergence for two state model')
+    ax[i].grid(linestyle='--')
+
+    i += 1
+
+    sns.violinplot(x="distributions", y="lifetime [hr]",
+                   inner="quart", palette="muted",
+                   split=True, hue="hues", data=dists, ax=ax[i])
+    sns.despine(left=True, ax=ax[i])
