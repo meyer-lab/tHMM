66d6618397510db8098e5692b5f2a273d982e704
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-01 09:31:07 -0800

    Reduce diff (#239)
    * More vectorized calculation.
    
    * Remove verbose error.
    
    * Fix.
    
    * Another fix.
    
    * Fix?
    
    * Minor cleanup.
    
    * Simplify
    
    * Update UpwardRecursion.py
    
    * Update UpwardRecursion.py
    
    * Try this.
    
    * Fix.

--- lineage/DownwardRecursion.py
+++ lineage/DownwardRecursion.py
@@ -23,9 +23,8 @@ def get_nonroot_gammas(tHMMobj, gammas, betas):
     '''get the gammas for all other nodes using recursion from the root nodes'''
     for num, lineageObj in enumerate(tHMMobj.X):  # for each lineage in our Population
         lineage = lineageObj.output_lineage
-        MSD_array = tHMMobj.MSD[num]  # getting the MSD of the respective lineage
         T = tHMMobj.estimate.T
-        beta_array = betas[num]  # instantiating N by K array
+        coeffs = betas[num] / tHMMobj.MSD[num]
 
         for level in lineageObj.output_list_of_gens[1:]:
             for cell in level:
@@ -33,20 +32,16 @@ def get_nonroot_gammas(tHMMobj, gammas, betas):
 
                 for daughter_idx in cell._get_daughters():
                     child_idx = lineage.index(daughter_idx)
-                    coeffs = beta_array[child_idx, :] / MSD_array[child_idx, :]
+
+                    beta_parent = beta_parent_child_func(beta_array=betas[num],
+                                                         T=T,
+                                                         MSD_array=tHMMobj.MSD[num],
+                                                         node_child_n_idx=child_idx)
 
                     for child_state_k in range(tHMMobj.numStates):
-                        sum_holder = 0.0
+                        sum_holder = np.sum(T[:, child_state_k] * gammas[num][parent_idx, :] / beta_parent)
 
-                        for parent_state_j in range(tHMMobj.numStates):
-                            beta_parent = beta_parent_child_func(beta_array=beta_array,
-                                                                 T=T,
-                                                                 MSD_array=MSD_array,
-                                                                 state_j=parent_state_j,
-                                                                 node_child_n_idx=child_idx)
-                            sum_holder += T[parent_state_j, child_state_k] * gammas[num][parent_idx, parent_state_j] / beta_parent
-
-                        gammas[num][child_idx, child_state_k] = coeffs[child_state_k] * sum_holder
+                        gammas[num][child_idx, child_state_k] = coeffs[child_idx, child_state_k] * sum_holder
                         assert np.all(gammas[num][0, :] == betas[num][0, :])
 
     for _, gg in enumerate(gammas):
