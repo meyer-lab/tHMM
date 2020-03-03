e4ad778e050cd7d334a1b4b4f4e92085b70cd7a1
Shakthi Visagan <shak360@gmail.com>
2020-03-02 16:54:22 -0800

    adding timer to fig creation

--- genFigures.py
+++ genFigures.py
@@ -3,6 +3,7 @@
 import sys
 import matplotlib
 import numpy as np
+import time
 matplotlib.use('AGG')
 
 fdir = './output/'
@@ -14,7 +15,9 @@ if __name__ == '__main__':
     nameOut = 'figure' + sys.argv[1]
 
     exec('from lineage.figures import ' + nameOut)
+    start_time = time.time()
     ff = eval(nameOut + '.makeFigure()')
+    print("--- %s seconds ---" % (time.time() - start_time))
     ff.savefig(fdir + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
 
     print(nameOut + ' is done.')
