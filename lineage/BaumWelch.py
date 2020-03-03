d12281167f11cffcf2edac12f750a3bd7a0927d8
Shakthi Visagan <shak360@gmail.com>
2020-03-02 16:26:21 -0800

    more wokr

--- lineage/BaumWelch.py
+++ lineage/BaumWelch.py
@@ -1,5 +1,6 @@
 ''' Re-calculates the tHMM parameters of pi, T, and emissions using Baum Welch. '''
 import numpy as np
+from numba import njit
 
 from .DownwardRecursion import get_root_gammas, get_nonroot_gammas
 from .UpwardRecursion import get_leaf_Normalizing_Factors, get_leaf_betas, get_nonleaf_NF_and_betas, calculate_log_likelihood, beta_parent_child_func
@@ -40,7 +41,7 @@ def get_all_gammas(lineageObj, gamma_arr):
     return holder
 
 
-@jit(forceobj=True)
+@njit
 def get_all_zetas(lineageObj, beta_array, MSD_array, gamma_array, T):
     '''sum of the list of all the zeta parent child for all the parent cells for a given state transition pair'''
     assert MSD_array.shape[1] == gamma_array.shape[1] == beta_array.shape[1], "Number of states in tHMM object mismatched!"
