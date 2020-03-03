87b9bb09fd07108883390788f88ee95561d8a22a
Shakthi Visagan <shak360@gmail.com>
2020-03-03 11:31:40 -0800

    Update genFigures.py

--- genFigures.py
+++ genFigures.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     exec('from lineage.figures import ' + nameOut)
     start_time = time.time()
     ff = eval(nameOut + '.makeFigure()')
-    print("--- %s seconds ---" % (time.time() - start_time))
+    print("--- {} {} seconds ---".format(nameOut, time.time() - start_time))
     ff.savefig(fdir + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
 
     print(nameOut + ' is done.')
