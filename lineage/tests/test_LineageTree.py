a4f02587c8b587af00a4c9e078a67505689a7251
Aaron Meyer <aaron@asmlab.org>
2020-03-04 07:13:18 -0800

    Testing fixes.

--- lineage/tests/test_LineageTree.py
+++ lineage/tests/test_LineageTree.py
@@ -34,8 +34,8 @@ class TestModel(unittest.TestCase):
         gamma_a1 = 10
         gamma_scale1 = 1
 
-        state_obj0 = StateDistribution(self.state0, bern_p0, gamma_a0, gamma_scale0)
-        state_obj1 = StateDistribution(self.state1, bern_p1, gamma_a1, gamma_scale1)
+        state_obj0 = StateDistribution(bern_p0, gamma_a0, gamma_scale0)
+        state_obj1 = StateDistribution(bern_p1, gamma_a1, gamma_scale1)
 
         self.E = [state_obj0, state_obj1]
 
