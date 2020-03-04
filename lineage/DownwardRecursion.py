d68d84dcfbc609b588aae6dc11ef17cd7b6a6884
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-04 09:08:04 -0800

    Update DownwardRecursion.py

--- lineage/DownwardRecursion.py
+++ lineage/DownwardRecursion.py
@@ -25,7 +25,9 @@ def get_nonroot_gammas(tHMMobj, gammas, betas):
 
     for num, lineageObj in enumerate(tHMMobj.X):  # for each lineage in our Population
         lineage = lineageObj.output_lineage
-        coeffs = betas[num] / (tHMMobj.MSD[num] + np.finfo(np.float).eps)
+        
+        with np.errstate(divide='ignore', invalid='ignore'):
+            coeffs = betas[num] / tHMMobj.MSD[num]
 
         for level in lineageObj.output_list_of_gens[1:]:
             for cell in level:
