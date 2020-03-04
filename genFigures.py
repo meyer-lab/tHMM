5dceb94003ef0f7ac7369b8f2c478232fd8b7a64
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-04 09:43:35 -0800

    Small fixes (#247)
    * Fixes.
    
    * More edits.
    
    * Edits.
    
    * Edit.
    
    * More cleaning
    
    * More cleanup
    
    * Fewer figures.
    
    * More cleanup.
    
    * Fixes.
    
    * Bump.
    
    * Try this.
    
    * Fix.
    
    * Fix.
    
    * Revert.
    
    * Testing fixes.
    
    * Fix.
    
    * Fix.
    
    * Fixes.
    
    * Tweak.
    
    * Fixes.
    
    * Update DownwardRecursion.py

--- genFigures.py
+++ genFigures.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+import time
 import matplotlib
 import numpy as np
 matplotlib.use('AGG')
@@ -11,10 +12,11 @@ fdir = './output/'
 np.random.seed(1)
 
 if __name__ == '__main__':
+    start = time.time()
     nameOut = 'figure' + sys.argv[1]
 
     exec('from lineage.figures import ' + nameOut)
     ff = eval(nameOut + '.makeFigure()')
     ff.savefig(fdir + nameOut + '.svg', dpi=ff.dpi, bbox_inches='tight', pad_inches=0)
 
-    print(nameOut + ' is done.')
+    print(f'{nameOut} is done after {time.time() - start} seconds.')
