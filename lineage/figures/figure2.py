ae52eb961bf345a03200b2889d9ef68251fa1a8e
Aaron Meyer <aaron@asmlab.org>
2020-03-04 07:23:02 -0800

    Tweak.

--- lineage/figures/figure2.py
+++ lineage/figures/figure2.py
@@ -23,19 +23,8 @@ def makeFigure():
     T = np.array([[0.85, 0.15],
                   [0.15, 0.85]], dtype="float")
 
-    # State 0 parameters "Resistant"
-    bern_p0 = 0.99
-    gamma_a0 = 20
-    gamma_scale0 = 5
-
-    # State 1 parameters "Susceptible"
-    bern_p1 = 0.88
-    gamma_a1 = 10
-    gamma_scale1 = 1
-
-    state_obj0 = StateDistribution(bern_p0, gamma_a0, gamma_scale0)
-    state_obj1 = StateDistribution(bern_p1, gamma_a1, gamma_scale1)
-    E = [state_obj0, state_obj1]
+    # bern, gamma_a, gamma_scale
+    E = [StateDistribution(0.99, 20, 5), StateDistribution(0.88, 10, 1)]
 
     # creating a population
     population_pruned = []
