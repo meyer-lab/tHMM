95c5e3f38d33704ec77fe1811dbb7223df9df077
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-08 20:58:03 -0700

    Upgrade packages. (#252)
    Co-authored-by: Aaron Meyer <aaron@asmlab.org>

--- lineage/figures/figureCommon.py
+++ lineage/figures/figureCommon.py
@@ -6,19 +6,21 @@ the figure creation files.
 from string import ascii_lowercase
 import numpy as np
 from matplotlib import gridspec, pyplot as plt
+import seaborn as sns
 
 
 def getSetup(figsize, gridd):
-    """Setup figures."""
+    """ Establish figure set-up with subplots. """
+    sns.set(style="whitegrid", font_scale=0.7, color_codes=True, palette="colorblind", rc={"grid.linestyle": "dotted", "axes.linewidth": 0.6})
 
-    # Setup plotting space
-    f = plt.figure(figsize=figsize)
-
-    # Make grid
-    gs1 = gridspec.GridSpec(*gridd)
+    # Setup plotting space and grid
+    f = plt.figure(figsize=figsize, constrained_layout=True)
+    gs1 = gridspec.GridSpec(*gridd, figure=f)
 
     # Get list of axis objects
-    ax = [f.add_subplot(gs1[x]) for x in range(gridd[0] * gridd[1])]
+    ax = list()
+    for x in range(gridd[0] * gridd[1]):
+        ax.append(f.add_subplot(gs1[x]))
 
     return (ax, f)
 
