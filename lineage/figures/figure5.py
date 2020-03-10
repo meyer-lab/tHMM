ce0586f3bb82bfeb69361874184edad784c8c59e
Aaron Meyer <aaron@asmlab.org>
2020-03-10 07:48:48 -0700

    Simplify file.

--- lineage/figures/figure5.py
+++ lineage/figures/figure5.py
@@ -26,24 +26,13 @@ def makeFigure():
     return f
 
 
-def AIC_increased_cells1():
-    """
-    Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a one-state model.
-    """
-
+def run_AIC(T, E, num_to_evaluate):
     # pi: the initial probability vector
-    pi = np.ones(2) / 2.0
+    # make an even starting p
+    pi = np.ones(T.shape[0]) / T.shape[0]
 
-    # T: transition probability matrix
-    T = np.ones((2, 2)) / 2.0
-
-    # bern, gamma_a, gamma_scale
-    state_obj0 = StateDistribution(0.99, 20, 5)
-    state_obj1 = StateDistribution(0.99, 20, 5)
-    E = [state_obj0, state_obj1]
-
+    # States to evaluate with the model
     desired_num_states = [1, 2, 3]
-    num_to_evaluate = 10
 
     list_of_populations = []
     for idx in range(num_to_evaluate):
@@ -65,13 +54,28 @@ def AIC_increased_cells1():
     return desired_num_states, AIC_holder
 
 
+def AIC_increased_cells1():
+    """
+    Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a one-state model.
+    """
+
+    # T: transition probability matrix
+    T = np.ones((2, 2)) / 2.0
+
+    # bern, gamma_a, gamma_scale
+    state_obj0 = StateDistribution(0.99, 20, 5)
+    state_obj1 = StateDistribution(0.99, 20, 5)
+    E = [state_obj0, state_obj1]
+
+    num_to_evaluate = 10
+
+    return run_AIC(T, E, num_to_evaluate)
+
+
 def AIC_increased_cells2():
     """
     Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a two-state model.
     """
-
-    # pi: the initial probability vector
-    pi = np.ones(2) / 2.0
 
     # T: transition probability matrix
     T = np.ones((2, 2)) / 2.0
@@ -81,36 +85,15 @@ def AIC_increased_cells2():
     state_obj1 = StateDistribution(0.88, 10, 1)
     E = [state_obj0, state_obj1]
 
-    desired_num_states = [1, 2, 3]
     num_to_evaluate = 10
 
-    list_of_populations = []
-    for idx in range(num_to_evaluate):
-            # Creating an unpruned and pruned lineage
-        list_of_populations.append([LineageTree(pi, T, E, (2**8) - 1, 1000000000, prune_condition='fate', prune_boolean=False)])
-
-    AIC_holder = []
-    for num_states_to_evaluate in desired_num_states:
-        tmp_AIC_holder_by_state = []
-        # Analyze the lineages in the list of populations
-        output = run_Analyze_over(list_of_populations, num_states_to_evaluate)
-        # Collecting the results of analyzing the lineages
-        for idx, (tHMMobj, _, LL) in enumerate(output):
-            AIC, _ = getAIC(tHMMobj, LL)
-            tmp_AIC_holder_by_state.append(AIC)
-
-        AIC_holder.append(tmp_AIC_holder_by_state)
-
-    return desired_num_states, AIC_holder
+    return run_AIC(T, E, num_to_evaluate)
 
 
 def AIC_increased_cells3():
     """
     Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a three-state model.
     """
-
-    # pi: the initial probability vector
-    pi = np.ones(3) / 3.0
 
     # T: transition probability matrix
     T = np.ones((3, 3)) / 3.0
@@ -124,27 +107,9 @@ def AIC_increased_cells3():
 
     E = [state_obj0, state_obj1, state_obj2]
 
-    desired_num_states = [1, 2, 3]
     num_to_evaluate = 10
 
-    list_of_populations = []
-    for idx in range(num_to_evaluate):
-            # Creating an unpruned and pruned lineage
-        list_of_populations.append([LineageTree(pi, T, E, (2**8) - 1, 1000000000, prune_condition='fate', prune_boolean=False)])
-
-    AIC_holder = []
-    for num_states_to_evaluate in desired_num_states:
-        tmp_AIC_holder_by_state = []
-        # Analyze the lineages in the list of populations
-        output = run_Analyze_over(list_of_populations, num_states_to_evaluate)
-        # Collecting the results of analyzing the lineages
-        for idx, (tHMMobj, _, LL) in enumerate(output):
-            AIC, _ = getAIC(tHMMobj, LL)
-            tmp_AIC_holder_by_state.append(AIC)
-
-        AIC_holder.append(tmp_AIC_holder_by_state)
-
-    return desired_num_states, AIC_holder
+    return run_AIC(T, E, num_to_evaluate)
 
 
 def figure_maker(ax, i, desired_num_states, AIC_holder):
