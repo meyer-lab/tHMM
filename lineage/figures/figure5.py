27741ff753c37c26221f7efaa087dd61d709fc72
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-10 08:12:15 -0700

    Change transitions (#255)
    * Try this.
    
    * AIC figure is fixed.
    
    Co-authored-by: Aaron Meyer <aaron@asmlab.org>

--- lineage/figures/figure5.py
+++ lineage/figures/figure5.py
@@ -17,7 +17,7 @@ def makeFigure():
     """
     Makes figure 11.
     """
-    ax, f = getSetup((7, 6), (1, 3))
+    ax, f = getSetup((7, 3), (1, 3))
 
     figure_maker(ax[0], *AIC_increased_cells1())
     figure_maker(ax[1], *AIC_increased_cells2())
@@ -26,7 +26,11 @@ def makeFigure():
     return f
 
 
-def run_AIC(T, E, num_to_evaluate=10):
+def run_AIC(Trate, E, num_to_evaluate=10):
+    # Normalize the transition matrix
+    T = Trate + np.eye(len(E))
+    T = T / np.sum(T, axis=0)[np.newaxis, :]
+
     # pi: the initial probability vector
     # make an even starting p
     pi = np.ones(T.shape[0]) / T.shape[0]
@@ -37,19 +41,15 @@ def run_AIC(T, E, num_to_evaluate=10):
     list_of_populations = []
     for idx in range(num_to_evaluate):
         # Creating an unpruned and pruned lineage
-        list_of_populations.append([LineageTree(pi, T, E, (2**8) - 1, 1000000000, prune_condition='fate', prune_boolean=False)])
+        list_of_populations.append([LineageTree(pi, T, E, (2**8) - 1, 1E9, prune_condition='fate', prune_boolean=False)])
 
-    AIC_holder = []
-    for num_states_to_evaluate in desired_num_states:
-        tmp_AIC_holder_by_state = []
+    AIC_holder = np.empty((len(desired_num_states), num_to_evaluate))
+    for ii, num_states_to_evaluate in enumerate(desired_num_states):
         # Analyze the lineages in the list of populations
         output = run_Analyze_over(list_of_populations, num_states_to_evaluate)
         # Collecting the results of analyzing the lineages
         for idx, (tHMMobj, _, LL) in enumerate(output):
-            AIC, _ = getAIC(tHMMobj, LL)
-            tmp_AIC_holder_by_state.append(AIC)
-
-        AIC_holder.append(tmp_AIC_holder_by_state)
+            AIC_holder[ii, idx] = getAIC(tHMMobj, LL)[0]
 
     return desired_num_states, AIC_holder
 
@@ -59,14 +59,11 @@ def AIC_increased_cells1():
     Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a one-state model.
     """
 
-    # T: transition probability matrix
-    T = np.ones((2, 2)) / 2.0
-
     # bern, gamma_a, gamma_scale
     E = [StateDistribution(0.99, 20, 5),
          StateDistribution(0.99, 20, 5)]
 
-    return run_AIC(T, E)
+    return run_AIC(0.01, E)
 
 
 def AIC_increased_cells2():
@@ -74,14 +71,11 @@ def AIC_increased_cells2():
     Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a two-state model.
     """
 
-    # T: transition probability matrix
-    T = np.ones((2, 2)) / 2.0
-
     # bern, gamma_a, gamma_scale
     E = [StateDistribution(0.99, 20, 5),
          StateDistribution(0.88, 10, 1)]
 
-    return run_AIC(T, E)
+    return run_AIC(0.01, E)
 
 
 def AIC_increased_cells3():
@@ -89,25 +83,22 @@ def AIC_increased_cells3():
     Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a three-state model.
     """
 
-    # T: transition probability matrix
-    T = np.ones((3, 3)) / 3.0
-
     # E: states are defined as StateDistribution objects
     # bern, gamma_a, gamma_scale
     E = [StateDistribution(0.7, 5.0, 1.0),
          StateDistribution(0.85, 10.0, 2.0),
          StateDistribution(0.99, 15.0, 3.0)]
 
-    return run_AIC(T, E)
+    return run_AIC(0.01, E)
 
 
 def figure_maker(ax, desired_num_states, AIC_holder):
     """
     Makes figure 11.
     """
-    ax.set_xlim((0, int(np.ceil(1.1 * max(desired_num_states)))))
+    AIC_holder = AIC_holder - np.min(AIC_holder, axis=0)[np.newaxis, :]
     ax.set_xlabel('Number of States')
-    ax.plot(desired_num_states, np.array(AIC_holder), 'k', alpha=0.5)
-    ax.set_ylabel(r'AIC')
+    ax.plot(desired_num_states, AIC_holder, 'k', alpha=0.5)
+    ax.set_ylabel('Normalized AIC')
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.set_title('State Assignment AIC')
