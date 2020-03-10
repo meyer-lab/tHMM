a4b5c8abd725116f37e66387312f8b113c2785b5
Aaron Meyer <aaron@asmlab.org>
2020-03-10 08:52:19 -0700

    Ready.

--- lineage/figures/figure5.py
+++ lineage/figures/figure5.py
@@ -19,11 +19,22 @@ def makeFigure():
     """
     ax, f = getSetup((7, 3), (1, 3))
 
-    figure_maker(ax[0], *AIC_increased_cells1())
-    figure_maker(ax[1], *AIC_increased_cells2())
-    figure_maker(ax[2], *AIC_increased_cells3())
+    # bern, gamma_a, gamma_scale
+    Sone = StateDistribution(0.99, 20, 5)
+    Stwo = StateDistribution(0.88, 10, 1)
+    Eone = [Sone, Sone]
+    Etwo = [Sone, Stwo]
+    Ethree = [Sone, Stwo, StateDistribution(0.40, 30, 1)]
+
+    figure_maker(ax[0], run_AIC(0.02, Eone))
+    figure_maker(ax[1], run_AIC(0.02, Etwo))
+    figure_maker(ax[2], run_AIC(0.02, Ethree))
 
     return f
+
+
+# States to evaluate with the model
+desired_num_states = np.arange(1, 6)
 
 
 def run_AIC(Trate, E, num_to_evaluate=10):
@@ -34,9 +45,6 @@ def run_AIC(Trate, E, num_to_evaluate=10):
     # pi: the initial probability vector
     # make an even starting p
     pi = np.ones(T.shape[0]) / T.shape[0]
-
-    # States to evaluate with the model
-    desired_num_states = [1, 2, 3]
 
     list_of_populations = []
     for idx in range(num_to_evaluate):
@@ -51,48 +59,10 @@ def run_AIC(Trate, E, num_to_evaluate=10):
         for idx, (tHMMobj, _, LL) in enumerate(output):
             AIC_holder[ii, idx] = getAIC(tHMMobj, LL)[0]
 
-    return desired_num_states, AIC_holder
+    return AIC_holder
 
 
-def AIC_increased_cells1():
-    """
-    Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a one-state model.
-    """
-
-    # bern, gamma_a, gamma_scale
-    E = [StateDistribution(0.99, 20, 5),
-         StateDistribution(0.99, 20, 5)]
-
-    return run_AIC(0.01, E)
-
-
-def AIC_increased_cells2():
-    """
-    Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a two-state model.
-    """
-
-    # bern, gamma_a, gamma_scale
-    E = [StateDistribution(0.99, 20, 5),
-         StateDistribution(0.88, 10, 1)]
-
-    return run_AIC(0.01, E)
-
-
-def AIC_increased_cells3():
-    """
-    Calculates accuracy and parameter estimation by increasing the number of cells in a lineage for a three-state model.
-    """
-
-    # E: states are defined as StateDistribution objects
-    # bern, gamma_a, gamma_scale
-    E = [StateDistribution(0.7, 5.0, 1.0),
-         StateDistribution(0.85, 10.0, 2.0),
-         StateDistribution(0.99, 15.0, 3.0)]
-
-    return run_AIC(0.01, E)
-
-
-def figure_maker(ax, desired_num_states, AIC_holder):
+def figure_maker(ax, AIC_holder):
     """
     Makes figure 11.
     """
@@ -100,6 +70,6 @@ def figure_maker(ax, desired_num_states, AIC_holder):
     ax.set_xlabel('Number of States')
     ax.plot(desired_num_states, AIC_holder, 'k', alpha=0.5)
     ax.set_ylabel('Normalized AIC')
-    ax.set_ylim(top=50)
+    ax.set_ylim(0.0, 50.0)
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.set_title('State Assignment AIC')
