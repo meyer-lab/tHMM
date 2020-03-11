ce506c059e396528640ef2cfed456611744a2ebc
Shakthi Visagan <shak360@gmail.com>
2020-03-10 18:40:08 -0700

    add figure 5

--- lineage/figures/figure5.py
+++ lineage/figures/figure5.py
@@ -1,14 +1,49 @@
 """
-This creates Figure 1.
+File: figure5.py
+Purpose: Generates figure 5.
 """
-from .figureCommon import subplotLabel, getSetup
+import numpy as np
+
+from .figureCommon import getSetup, subplotLabel
+from .figure4 import figure_maker, E, piiii, figFourCommon
+from ..LineageTree import LineageTree
 
 
 def makeFigure():
-    """ makes figure 1 """
+    """
+    Makes figures 3.
+    """
+
     # Get list of axis objects
-    ax, f = getSetup((7, 6), (3, 4))
+    ax, f = getSetup((7, 6), (2, 3))
+
+    figure_maker(ax, *accuracy_increased_cells())
 
     subplotLabel(ax)
 
     return f
+
+
+def accuracy_increased_cells():
+    """
+    Calculates accuracy and transition rate estimation over an increasing number of cells in a lineage for an pruned two-state model.
+    """
+
+    # Creating a list of populations to analyze over
+    list_of_populations = []
+    
+    # Create a list of transition matrices to transition over called list_of_Ts
+    
+    # Iterate through the transition matrices
+    for T in list_of_Ts:
+        # Creating an unpruned and pruned lineage
+        lineage = LineageTree(piiii, T, E, (2**12) - 1, experiment_time, prune_condition='both', prune_boolean=True)
+
+        while len(lineage.output_lineage) < 16:
+            del lineage
+            lineage = LineageTree(piiii, T, E, (2**12) - 1, experiment_time, prune_condition='both', prune_boolean=True)
+
+        # Adding populations into a holder for analysing
+        list_of_populations.append([lineage])
+
+    return figFourCommon(list_of_populations)
