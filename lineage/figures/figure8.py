29b426f2214ffd3754b1e8d5eeb54a3d2febd94e
Shakthi Visagan <shak360@gmail.com>
2020-03-23 16:42:23 -0700

    more work

--- lineage/figures/figure8.py
+++ lineage/figures/figure8.py
@@ -17,7 +17,7 @@ from .figureCommon import (
     max_desired_num_cells,
     max_experiment_time,
     num_data_points,
-    state0,
+    state1,
 )
 from ..LineageTree import LineageTree
 from ..StateDistribution import StateDistribution
@@ -48,7 +48,7 @@ def accuracy():
     """
 
     # Creating a list of populations to analyze over
-    list_of_Es = [[state0, StateDistribution(0.99, a, 5)] for a in np.logspace(1, 1.1, num_data_points, base=20)]
+    list_of_Es = [[StateDistribution(0.88, a, 1), state1] for a in np.logspace(1, 1.5, num_data_points, base=10)]
     list_of_populations = []
     for E in list_of_Es:
         population = []
