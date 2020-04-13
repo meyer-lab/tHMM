9f691ef629fc15059d7c0d113eff666f52ea3614
Shakthi Visagan <shak360@gmail.com>
2020-04-13 15:46:07 -0700

    more work

--- lineage/figures/figure2.py
+++ lineage/figures/figure2.py
@@ -47,6 +47,9 @@ def accuracy():
     # Creating a list of populations to analyze over
     cells = np.linspace(16, 128, num_data_points)
     list_of_populations = []
+    list_of_fpi = []
+    list_of_fT = []
+    list_of_fE = []
     for cell_num in cells:
         population = []
 
@@ -54,5 +57,8 @@ def accuracy():
 
         # Adding populations into a holder for analysing
         list_of_populations.append(population)
+        list_of_fpi.append(pi)
+        list_of_fT.append(T)
+        list_of_fE.append(E)
 
-    return commonAnalyze(list_of_populations, fpi=pi, fT=T)
+    return commonAnalyze(list_of_populations, list_of_fpi=list_of_fpi, list_of_fT=list_of_fT, list_of_fE=list_of_fE)
