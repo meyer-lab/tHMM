d8ce333d41b820330226f5c84aa84252804a2302
jclagarde <jclagarde01@gmail.com>
2020-04-30 17:46:42 -0700

    changes

--- lineage/figures/figure12.py
+++ lineage/figures/figure12.py
@@ -17,6 +17,7 @@ from .figureCommon import (
     max_desired_num_cells,
     lineage_good_to_analyze,
     num_data_points,
+    min_desired_num_cells,
 )
 from ..LineageTree import LineageTree
 
@@ -46,7 +47,7 @@ def accuracy():
     """
 
     # Creating a list of populations to analyze over
-    cells = np.linspace(16, 128, num_data_points)
+    cells = np.linspace(min_desired_num_cells, max_desired_num_cells, num_data_points)
     list_of_populations = []
     list_of_fpi = []
     list_of_fT = []
--- lineage/figures/figure3.py
+++ lineage/figures/figure3.py
@@ -60,7 +60,7 @@ def accuracy():
 
             good2go = False
             while not good2go:
-                tmp_lineage = LineageTree(pi, T, E, 16)
+                tmp_lineage = LineageTree(pi, T, E, min_desired_num_cells)
                 good2go = lineage_good_to_analyze(tmp_lineage)
 
             population.append(tmp_lineage)
