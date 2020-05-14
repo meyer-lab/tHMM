d7a66f289090932c6c74d9c1bac32e5ce12cbeff
Shakthi Visagan <shak360@gmail.com>
2020-05-14 16:08:53 -0700

    more work

--- lineage/tests/test_BaumWelch.py
+++ lineage/tests/test_BaumWelch.py
@@ -2,10 +2,9 @@
 import unittest
 import numpy as np
 from ..states.StateDistribution import StateDistribution
-from ..UpwardRecursion import get_leaf_Normalizing_Factors, get_leaf_betas, get_nonleaf_NF_and_betas, calculate_log_likelihood
-from ..BaumWelch import fit
 from ..LineageTree import LineageTree
 from ..tHMM import tHMM
+from ..Analyze import Analyze
 
 
 class TestBW(unittest.TestCase):
