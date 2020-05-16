4b671bce33efb7b2438bbaa5d50437c39fd99674
Aaron Meyer <git@ameyer.me>
2020-05-16 10:39:44 -0700

    More cleanup.

--- lineage/tests/test_BaumWelch.py
+++ lineage/tests/test_BaumWelch.py
@@ -1,30 +1,17 @@
 """ Unit test file. """
 import unittest
 import numpy as np
-from ..states.StateDistribution import StateDistribution
 from ..UpwardRecursion import calculate_log_likelihood
 from ..BaumWelch import fit, calculateQuantities
 from ..LineageTree import LineageTree
 from ..tHMM import tHMM
+from lineage.figures.figureCommon import pi, T, E
 
 
 class TestBW(unittest.TestCase):
     """ Unit tests for Baum-Welch methods. """
     def commonTest(self, **kwargs):
         """ This tests that one step of Baum-Welch increases the likelihood of the fit. """
-        # State 0 parameters "Resistant"
-        state_obj0 = StateDistribution(0.99, 20, 5)
-        # State 1 parameters "Susceptible"
-        state_obj1 = StateDistribution(0.8, 10, 1)
-
-        E = [state_obj0, state_obj1]
-
-        # pi: the initial probability vector
-        pi = np.array([0.6, 0.4], dtype="float")
-
-        # T: transition probability matrix
-        T = np.array([[0.85, 0.15], [0.15, 0.85]], dtype="float")
-
         # Using an unpruned lineage to avoid unforseen issues
         X = LineageTree(pi, T, E, desired_num_cells=(2 ** 11) - 1, **kwargs)
         tHMMobj = tHMM([X], num_states=2)  # build the tHMM class with X
@@ -56,4 +43,4 @@ class TestBW(unittest.TestCase):
 
     def test_step3(self):
         """ This tests that one step of Baum-Welch increases the likelihood of the fit. """
-        self.commonTest(censor_condition=3, desired_experimental_time=500)
+        self.commonTest(censor_condition=3, desired_experimental_time=1000)
