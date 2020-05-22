58b069e7480aa1573fdc4d1b42b7306599a2a6e5
farnazmdi <farnazmd95@gmail.com>
2020-05-21 19:54:51 -0700

    changed xtype

--- lineage/figures/figure16.py
+++ lineage/figures/figure16.py
@@ -182,4 +182,4 @@ def accuracy():
         list_of_fT.append(T)
         list_of_fE.append(E)
 
-    return commonAnalyze(list_of_populations, xtype="prop", list_of_fpi=list_of_fpi)
+    return commonAnalyze(list_of_populations, xtype="length", list_of_fpi=list_of_fpi)
