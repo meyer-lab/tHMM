a0e046c7b899884788ecb9801623c10bbfbfa768
jclagarde <jclagarde01@gmail.com>
2020-05-27 21:47:25 -0700

    changed statedistribution1

--- lineage/states/StateDistribution1.py
+++ lineage/states/StateDistribution1.py
@@ -73,7 +73,7 @@ class StateDistribution:
         """
         Method to print out a state distribution object.
         """
-        return "State object w/ parameters: {}.".format(self.params)
+        return f"State object w/ parameters: {self.params}."
 
 
 # Because parameter estimation requires that estimators be written or imported,
