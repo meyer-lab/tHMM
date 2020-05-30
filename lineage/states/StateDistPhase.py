d60b72862198165e859e96904b35898777780c99
farnazmdi <farnazmd95@gmail.com>
2020-05-30 15:10:49 -0700

    issue addressed

--- lineage/states/StateDistPhase.py
+++ lineage/states/StateDistPhase.py
@@ -7,20 +7,21 @@ from .stateCommon import bern_pdf, bernoulli_estimator, gamma_pdf, gamma_estimat
 class StateDistribution2:
     """ For G1 and G2 separated as observations. """
 
-    def __init__(self, bern_p=0.9, gamma_a1=7.0, gamma_scale1=3, gamma_a2=14.0, gamma_scale2=6):  # user has to identify what parameters to use for each state
+    def __init__(self, bern_p1=0.9, bern_p2=0.75, gamma_a1=7.0, gamma_scale1=3, gamma_a2=14.0, gamma_scale2=6):  # user has to identify what parameters to use for each state
         """ Initialization function should take in just in the parameters for the observations that comprise the multivariate random variable emission they expect their data to have. """
-        self.params = [bern_p, gamma_a1, gamma_scale1, gamma_a2, gamma_scale2]
+        self.params = [bern_p1, bern_p2, gamma_a1, gamma_scale1, gamma_a2, gamma_scale2]
 
     def rvs(self, size):  # user has to identify what the multivariate (or univariate if he or she so chooses) random variable looks like
         """ User-defined way of calculating a random variable given the parameters of the state stored in that observation's object. """
         # {
-        bern_obs = sp.bernoulli.rvs(p=self.params[0], size=size)  # bernoulli observations
-        gamma_obsG1 = sp.gamma.rvs(a=self.params[1], scale=self.params[2], size=size)  # gamma observations
-        gamma_obsG2 = sp.gamma.rvs(a=self.params[3], scale=self.params[4], size=size)
+        bern_obsG1 = sp.bernoulli.rvs(p=self.params[0], size=size)  # bernoulli observations
+        bern_obsG2 = sp.bernoulli.rvs(p=self.params[1], size=size) 
+        gamma_obsG1 = sp.gamma.rvs(a=self.params[2], scale=self.params[3], size=size)  # gamma observations
+        gamma_obsG2 = sp.gamma.rvs(a=self.params[4], scale=self.params[5], size=size)
         time_censor = [1] * (len(gamma_obsG1) + len(gamma_obsG2))
         # } is user-defined in that they have to define and maintain the order of the multivariate random variables.
         # These tuples of observations will go into the cells in the lineage tree.
-        return bern_obs, gamma_obsG1, gamma_obsG2, time_censor
+        return bern_obsG1, bern_obsG2, gamma_obsG1, gamma_obsG2, time_censor
 
     def pdf(self, tuple_of_obs):  # user has to define how to calculate the likelihood
         """ User-defined way of calculating the likelihood of the observation stored in a cell. """
@@ -30,12 +31,15 @@ class StateDistribution2:
         # In our example, we assume the observation's are uncorrelated across the dimensions (across the different
         # distribution observations), so the likelihood of observing the multivariate observation is just the product of
         # the individual observation likelihoods.
-        bern_ll = bern_pdf(tuple_of_obs[0], self.params[0])
+        bern_llG1 = bern_pdf(tuple_of_obs[0], self.params[0])
+        bern_llG2 = bern_pdf(tuple_of_obs[1], self.params[1])
+        gamma_llG1 = gamma_pdf(tuple_of_obs[2], self.params[2], self.params[3])
+        gamma_llG2 = gamma_pdf(tuple_of_obs[3], self.params[4], self.params[5])
 
-        gamma_llG1 = gamma_pdf(tuple_of_obs[1], self.params[1], self.params[2])
-        gamma_llG2 = gamma_pdf(tuple_of_obs[2], self.params[3], self.params[4])
-
-        return bern_ll * gamma_llG1 * gamma_llG2
+        if tuple_of_obs[0] == 0:
+            return bern_llG2 * gamma_llG1 * gamma_llG2
+        else:
+            return bern_llG1 * bern_llG2 * gamma_llG1 * gamma_llG2
 
     def estimator(self, list_of_tuples_of_obs, gammas):
         """ User-defined way of estimating the parameters given a list of the tuples of observations from a group of cells. """
@@ -44,14 +48,16 @@ class StateDistribution2:
 
         # getting the observations as individual lists
         # {
-        bern_obs = list(unzipped_list_of_tuples_of_obs[0])
-        gamma_obsG1 = list(unzipped_list_of_tuples_of_obs[1])
-        gamma_obsG2 = list(unzipped_list_of_tuples_of_obs[2])
-        gamma_censor_obs = list(unzipped_list_of_tuples_of_obs[3])
+        bern_obsG1 = list(unzipped_list_of_tuples_of_obs[0])
+        bern_obsG2 = list(unzipped_list_of_tuples_of_obs[1])
+        gamma_obsG1 = list(unzipped_list_of_tuples_of_obs[2])
+        gamma_obsG2 = list(unzipped_list_of_tuples_of_obs[3])
+        gamma_censor_obs = list(unzipped_list_of_tuples_of_obs[4])
 
-        self.params[0] = bernoulli_estimator(bern_obs, gammas)
-        self.params[1], self.params[2] = gamma_estimator(gamma_obsG1, gamma_censor_obs, gammas)
-        self.params[3], self.params[4] = gamma_estimator(gamma_obsG2, gamma_censor_obs, gammas)
+        self.params[0] = bernoulli_estimator(bern_obsG1, gammas)
+        self.params[1] = bernoulli_estimator(bern_obsG2, gammas)
+        self.params[2], self.params[3] = gamma_estimator(gamma_obsG1, gamma_censor_obs, gammas)
+        self.params[4], self.params[5] = gamma_estimator(gamma_obsG2, gamma_censor_obs, gammas)
         # } requires the user's attention.
         # Note that we return an instance of the state distribution class, but now instantiated with the parameters
         # from estimation. This is then stored in the original state distribution object which then gets updated
