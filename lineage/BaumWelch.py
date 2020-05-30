005c290533bf5e1afeebd9f061f1aeaccf3adf6c
Aaron Meyer <git@ameyer.me>
2020-05-30 14:13:28 -0700

    More iter.

--- lineage/BaumWelch.py
+++ lineage/BaumWelch.py
@@ -71,7 +71,7 @@ def calculateQuantities(tHMMobj):
     return NF, betas, gammas, LL
 
 
-def fit(tHMMobj, tolerance=np.spacing(1), max_iter=200):
+def fit(tHMMobj, tolerance=np.spacing(1), max_iter=300):
     """Runs the tHMM function through Baum Welch fitting"""
     num_states = tHMMobj.num_states
 
