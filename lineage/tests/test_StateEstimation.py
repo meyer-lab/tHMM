ce67ca1ac693f118d91d8afdbf6362b4bffcc160
Shakthi Visagan <shak360@gmail.com>
2020-06-05 18:11:11 -0700

    whitespace linting

--- lineage/tests/test_StateEstimation.py
+++ lineage/tests/test_StateEstimation.py
@@ -45,7 +45,7 @@ class TestModel(unittest.TestCase):
         self.solver_expon_censored = tHMM([self.lineage_expon_censored], 1)  # evaluating for one state
         self.solver_expon_censored.fit()
         self.expon_state_censored_estimate = self.solver_expon_censored.estimate.E[0]
-        
+
     def estimationEvaluationGamma(self):
         """
         Evaluates the performance of fitting and the underlying estimator
@@ -54,7 +54,7 @@ class TestModel(unittest.TestCase):
         """
         self.assertGreater(1., abs(self.gamma_state_estimate.gamma_a-self.E_gamma.gamma_a))
         self.assertGreater(1., abs(self.gamma_state_estimate.gamma_scale-self.E_gamma.gamma_scale))
-        
+
     def estimationEvaluationExpon(self):
         """
         Evaluates the performance of fitting and the underlying estimator
@@ -62,16 +62,16 @@ class TestModel(unittest.TestCase):
         Exponential uncensored.
         """
         self.assertGreater(1., abs(self.expon_state_estimate.exp_beta - self.E_expon.exp_beta))
-        
+
     def estimationEvaluationGammaCensored(self):
         """
         Evaluates the performance of fitting and the underlying estimator
         by comparing the parameter estimates to their true values.
         Gamma censored.
         """
-        self.assertGreater(1., abs(self.gamma_state_censored_estimate.gamma_a - self.E_gamma.gamma_a))  
+        self.assertGreater(1., abs(self.gamma_state_censored_estimate.gamma_a - self.E_gamma.gamma_a))
         self.assertGreater(1., abs(self.gamma_state_censored_estimate.gamma_scale - self.E_gamma.gamma_scale))
-        
+
     def estimationEvaluationExponCensored(self):
         """
         Evaluates the performance of fitting and the underlying estimator
