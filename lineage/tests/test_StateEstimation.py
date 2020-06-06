a1403cb57bbc575d89b8a335058c9af3df50818b
Shakthi Visagan <shak360@gmail.com>
2020-06-05 17:46:41 -0700

    more work

--- lineage/tests/test_StateEstimation.py
+++ lineage/tests/test_StateEstimation.py
@@ -35,13 +35,13 @@ class TestModel(unittest.TestCase):
         self.expon_state_estimate = self.solver_expon.estimate.E[0]
 
         self.lineage_gamma_censored = LineageTree(self.pi, self.T, self.E_gamma, 2**9, censor_condition=3, desired_experiment_time=30)
-        assert(not all([cell.obs[2] for cell in self.lineage_gamma_censored.output_lineage]))  # ensures that at least some cells are censored
+        assert not all([cell.obs[2] for cell in self.lineage_gamma_censored.output_lineage])  # ensures that at least some cells are censored
         self.solver_gamma_censored = tHMM([self.lineage_gamma_censored], 1)  # evaluating for one state
         self.solver_gamma_censored.fit()
         self.gamma_state_censored_estimate = self.solver_gamma_censored.estimate.E[0]
 
         self.lineage_expon_censored = LineageTree(self.pi, self.T, self.E_expon, 2**9, censor_condition=3, desired_experiment_time=30)
-        assert(not all([cell.obs[2] for cell in self.lineage_expon_censored.output_lineage]))  # ensures that at least some cells are censored
+        assert not all([cell.obs[2] for cell in self.lineage_expon_censored.output_lineage])  # ensures that at least some cells are censored
         self.solver_expon_censored = tHMM([self.lineage_expon_censored], 1)  # evaluating for one state
         self.solver_expon_censored.fit()
         self.expon_state_censored_estimate = self.solver_expon_censored.estimate.E[0]
