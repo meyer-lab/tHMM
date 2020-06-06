6f4bf22818a975c443096a3fa0c640593ee9aaad
Shakthi Visagan <shak360@gmail.com>
2020-06-05 17:53:10 -0700

    more work

--- lineage/tests/test_StateEstimation.py
+++ lineage/tests/test_StateEstimation.py
@@ -52,8 +52,8 @@ class TestModel(unittest.TestCase):
         by comparing the parameter estimates to their true values.
         Gamma uncensored.
         """
-        self.assertGreater(abs(self.gamma_state_estimate.gamma_a-self.E_gamma.gamma_a) < 1)
-        self.assertGreater(abs(self.gamma_state_estimate.gamma_scale-self.E_gamma.gamma_scale) < 1)
+        self.assertGreater(1., abs(self.gamma_state_estimate.gamma_a-self.E_gamma.gamma_a))
+        self.assertGreater(1., abs(self.gamma_state_estimate.gamma_scale-self.E_gamma.gamma_scale))
         
     def estimationEvaluationExpon(self):
         """
@@ -61,7 +61,7 @@ class TestModel(unittest.TestCase):
         by comparing the parameter estimates to their true values.
         Exponential uncensored.
         """
-        self.assertGreater(abs(self.expon_state_estimate.exp_beta - self.E_expon.exp_beta) < 1)
+        self.assertGreater(1., abs(self.expon_state_estimate.exp_beta - self.E_expon.exp_beta))
         
     def estimationEvaluationGammaCensored(self):
         """
@@ -69,8 +69,8 @@ class TestModel(unittest.TestCase):
         by comparing the parameter estimates to their true values.
         Gamma censored.
         """
-        self.assertGreater(abs(self.gamma_state_censored_estimate.gamma_a - self.E_gamma.gamma_a) < 1)  
-        self.assertGreater(abs(self.gamma_state_censored_estimate.gamma_scale - self.E_gamma.gamma_scale) < 1)
+        self.assertGreater(1., abs(self.gamma_state_censored_estimate.gamma_a - self.E_gamma.gamma_a))  
+        self.assertGreater(1., abs(self.gamma_state_censored_estimate.gamma_scale - self.E_gamma.gamma_scale))
         
     def estimationEvaluationExponCensored(self):
         """
@@ -78,4 +78,4 @@ class TestModel(unittest.TestCase):
         by comparing the parameter estimates to their true values.
         Exponential censored.
         """
-        self.assertGreater(abs(self.expon_state_censored_estimate.exp_beta-self.E_expon.exp_beta) < 1)
+        self.assertGreater(1., abs(self.expon_state_censored_estimate.exp_beta-self.E_expon.exp_beta))
