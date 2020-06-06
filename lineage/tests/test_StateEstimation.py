685267b6e1355b7c36a2f8ce6a92ebb99c85e446
Shakthi Visagan <shak360@gmail.com>
2020-06-05 17:13:52 -0700

    more work

--- lineage/tests/test_StateEstimation.py
+++ lineage/tests/test_StateEstimation.py
@@ -14,24 +14,40 @@ class TestModel(unittest.TestCase):
     def setUp(self):
         self.pi = np.array([1])
         self.T = np.array([[1]])
-        self.E_gamma = [gamma_state()]
-        self.E_expon = [expon_state()]
+        self.E_gamma = [gamma_state(bern_p=1., gamma_a=7, gamma_scale=4.5)]
+        self.E_expon = [expon_state(bern_p=1., exp_beta=7.0)]
+        # setting the bern_p to 1. ensures that all cells live and censoring is only
+        # due to living past the experiment time
 
         self.lineage_gamma = LineageTree(self.pi, self.T, self.E_gamma, 2**9)
-        self.solver_gamma = tHMM([self.lineage_gamma], 1) # evaluating for one state
+        self.solver_gamma = tHMM([self.lineage_gamma], 1)  # evaluating for one state
         self.solver_gamma.fit()
 
         self.lineage_expon = LineageTree(self.pi, self.T, self.E_expon, 2**9)
-        self.solver_expon = tHMM([self.lineage_expon], 1) # evaluating for one state
+        self.solver_expon = tHMM([self.lineage_expon], 1)  # evaluating for one state
         self.solver_expon.fit()
 
         self.lineage_gamma_censored = LineageTree(self.pi, self.T, self.E_gamma, 2**9, censor_condition=3, desired_experiment_time=30)
-        self.solver_gamma_censored = tHMM([self.lineage_gamma_censored], 1) # evaluating for one state
+        self.solver_gamma_censored = tHMM([self.lineage_gamma_censored], 1)  # evaluating for one state
         self.solver_gamma_censored.fit()
 
         self.lineage_expon_censored = LineageTree(self.pi, self.T, self.E_expon, 2**9, censor_condition=3, desired_experiment_time=30)
-        self.solver_expon_censored = tHMM([self.lineage_expon_censored], 1) # evaluating for one state
+        self.solver_expon_censored = tHMM([self.lineage_expon_censored], 1)  # evaluating for one state
         self.solver_expon_censored.fit()
         
         
-    def eva
+    def estimationEvaluation(self):
+        gamma_state_estimate = self.solver_gamma.estimate.E[0]
+        expon_state_estimate = self.solver_expon.estimate.E[0]
+        gamma_state_censored_estimate = self.solver_gamma_censored.estimate.E[0]
+        expon_state_censored_estimate = self.solver_expon_censored.estimate.E[0]
+
+        self.assertTrue(abs(gamma_state_estimate.gamma_a - self.E_gamma.gamma_a) < 1)
+        self.assertTrue(abs(gamma_state_estimate.gamma_scale - self.E_gamma.gamma_scale) < 1)
+        
+        self.assertTrue(abs(expon_state_estimate.exp_beta - self.E_expon.exp_beta) < 1)
+
+        self.assertTrue(abs(gamma_state_censored_estimate.gamma_a - self.E_gamma.gamma_a) < 1)  
+        self.assertTrue(abs(gamma_state_censored_estimate.gamma_scale - self.E_gamma.gamma_scale) < 1)
+        
+        self.assertTrue(abs(expon_state_censored_estimate.exp_beta - self.E_expon.exp_beta) < 1)
