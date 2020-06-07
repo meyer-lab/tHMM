917930c098bf186134c0eb4c31feaf19627c54a8
Shakthi Visagan <shak360@gmail.com>
2020-06-07 14:18:30 -0700

    Update test_StateEstimation.py

--- lineage/tests/test_StateEstimation.py
+++ lineage/tests/test_StateEstimation.py
@@ -25,19 +25,19 @@ class TestModel(unittest.TestCase):
         # Setting the bern_p to 1. ensures that all cells live and censoring is only
         # due to living past the experiment time
 
-        self.lineage_gamma = LineageTree(self.pi, self.T, self.E_gamma, 2**9)
+        self.lineage_gamma = LineageTree(self.pi, self.T, self.E_gamma, 2**11)
         self.solver_gamma = tHMM([self.lineage_gamma], 1)  # evaluating for one state
         self.solver_gamma.fit()
         self.gamma_state_estimate = self.solver_gamma.estimate.E[0]
 
-        self.lineage_expon = LineageTree(self.pi, self.T, self.E_expon, 2**9)
+        self.lineage_expon = LineageTree(self.pi, self.T, self.E_expon, 2**11)
         self.solver_expon = tHMM([self.lineage_expon], 1)  # evaluating for one state
         self.solver_expon.fit()
         self.expon_state_estimate = self.solver_expon.estimate.E[0]
 
         good2go = False
         while not good2go:
-            lineage_gamma_censored = LineageTree(self.pi, self.T, self.E_gamma, 2**9, censor_condition=3, desired_experiment_time=100)
+            lineage_gamma_censored = LineageTree(self.pi, self.T, self.E_gamma, 2**11, censor_condition=3, desired_experiment_time=100)
             good2go = lineage_good_to_analyze(lineage_gamma_censored)
         self.lineage_gamma_censored = lineage_gamma_censored
         assert not all([cell.obs[2] == 1 for cell in self.lineage_gamma_censored.output_lineage])  # ensures that at least some cells are censored
@@ -47,7 +47,7 @@ class TestModel(unittest.TestCase):
 
         good2go = False
         while not good2go:
-            lineage_expon_censored = LineageTree(self.pi, self.T, self.E_expon, 2**9, censor_condition=3, desired_experiment_time=100)
+            lineage_expon_censored = LineageTree(self.pi, self.T, self.E_expon, 2**11, censor_condition=3, desired_experiment_time=100)
             good2go = lineage_good_to_analyze(lineage_expon_censored)
         self.lineage_expon_censored = lineage_expon_censored
         assert not all([cell.obs[2] == 1 for cell in self.lineage_expon_censored.output_lineage])  # ensures that at least some cells are censored
