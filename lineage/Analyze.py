bd4c2c6058c37a896931baf68dba7a0ed283c16f
Shakthi Visagan <shak360@gmail.com>
2020-06-09 14:24:17 -0700

    more work

--- lineage/Analyze.py
+++ lineage/Analyze.py
@@ -141,9 +141,9 @@ def Results(tHMMobj, pred_states_by_lineage, LL):
         for state_assignment in pred_states_by_lineage:
             temp_pred_states_by_lineage.append([switcher[state] for state in state_assignment])
         new_pred_states_by_lineage_holder.append(temp_pred_states_by_lineage)
-        switcher_LL_holder.append(np.sum(tHMMobj.log_score(temp_pred_states_by_lineage, E=tHMMobj.X[0].E)))
+        switcher_LL_holder.append(np.sum(tHMMobj.log_score(temp_pred_states_by_lineage, pi=tHMMobj.X[0].pi, T=tHMMobj.X[0].T, E=tHMMobj.X[0].E)))
         print(switcher)
-        print(tHMMobj.log_score(temp_pred_states_by_lineage, pi=tHMMobj.X[0].pi, T=tHMMobj.X[0].T, E=tHMMobj.X[0].E))
+        print(switcher_LL_holder[-1])
     max_idx = switcher_LL_holder.index(max(switcher_LL_holder))
 
     # Create switcher map based on the minimal likelihood of different permutations of state
