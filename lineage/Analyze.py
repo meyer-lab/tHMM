10c8737c51616588acf4e7fb142ef0bb11d52cb8
Shakthi Visagan <shak360@gmail.com>
2020-06-09 14:15:59 -0700

    more work

--- lineage/Analyze.py
+++ lineage/Analyze.py
@@ -141,7 +141,7 @@ def Results(tHMMobj, pred_states_by_lineage, LL):
         for state_assignment in pred_states_by_lineage:
             temp_pred_states_by_lineage.append([switcher[state] for state in state_assignment])
         new_pred_states_by_lineage_holder.append(temp_pred_states_by_lineage)
-        switcher_LL_holder.append(tHMMobj.log_score(temp_pred_states_by_lineage, E=tHMMobj.X[0].E))
+        switcher_LL_holder.append(np.sum(tHMMobj.log_score(temp_pred_states_by_lineage, E=tHMMobj.X[0].E)))
         print(switcher)
         print(tHMMobj.log_score(temp_pred_states_by_lineage, pi=tHMMobj.X[0].pi, T=tHMMobj.X[0].T, E=tHMMobj.X[0].E))
     max_idx = switcher_LL_holder.index(max(switcher_LL_holder))
