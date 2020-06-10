5cbd7136418b6b61e6215e7c18246ee5010ecd96
Shakthi Visagan <shak360@gmail.com>
2020-06-09 17:31:19 -0700

    more work

--- lineage/Analyze.py
+++ lineage/Analyze.py
@@ -103,7 +103,7 @@ def Results(tHMMobj, pred_states_by_lineage, LL):
     results_dict["LL"] = LL
     results_dict["total_number_of_cells"] = sum([len(lineage) for lineage in tHMMobj.X])
 
-    true_states_by_lineage = np.array([np.array([cell.state for cell in lineage.output_lineage] for lineage in tHMMobj.X]))
+    true_states_by_lineage = np.array([np.array([cell.state for cell in lineage.output_lineage] for lineage in tHMMobj.X])))
     ravel_true_states_by_lineage = np.ravel(true_states_by_lineage)
 
     # 1. Decide how to switch states based on the state assignment that yields the maximum likelihood
