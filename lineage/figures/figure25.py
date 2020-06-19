e719ba04a0bb9ad2cb0fc2da287f959b00e7a022
Shakthi Visagan <shak360@gmail.com>
2020-06-19 16:14:47 -0700

    more work

--- lineage/figures/figure25.py
+++ lineage/figures/figure25.py
@@ -129,10 +129,10 @@ def figureMaker2(ax, E, total_cellnum, dataframe, dataParams):
     sns.stripplot(x="cell number", y='Bern. G1 p', hue='state', data=dataParams, dodge=False, jitter=True, ax=ax[i], marker='o', linewidth=0.5, edgecolor="white", palette=sns.xkcd_palette(['blue', 'green']))
     for tick, text in zip(ax[i].get_xticks(), ax[i].get_xticklabels()):
         # plot horizontal lines across the column, centered on the tick
-        ax.plot([tick-0.5, tick+0.5], [paramTrues[:, 0, 0][0], paramTrues[:, 0, 0][0]], color='blue')
-        ax.plot([tick-0.5, tick+0.5], [paramTrues[:, 1, 0][0], paramTrues[:, 1, 0][0]], color='green')
-        ax.plot([tick-0.5, tick+0.5], [paramTrues[:, 0, 1][0], paramTrues[:, 0, 1][0]], color='orange')
-        ax.plot([tick-0.5, tick+0.5], [paramTrues[:, 1, 1][0], paramTrues[1, 1, 1][0]], color='red')
+        ax[i].plot([tick-0.5, tick+0.5], [paramTrues[:, 0, 0][0], paramTrues[:, 0, 0][0]], color='blue')
+        ax[i].plot([tick-0.5, tick+0.5], [paramTrues[:, 1, 0][0], paramTrues[:, 1, 0][0]], color='green')
+        ax[i].plot([tick-0.5, tick+0.5], [paramTrues[:, 0, 1][0], paramTrues[:, 0, 1][0]], color='orange')
+        ax[i].plot([tick-0.5, tick+0.5], [paramTrues[:, 1, 1][0], paramTrues[1, 1, 1][0]], color='red')
     sns.stripplot(x="cell number", y='Bern. G2 p', hue='state', data=dataParams, dodge=False, jitter=True, ax=ax[i], marker='^', linewidth=0.5, edgecolor="white", palette=sns.xkcd_palette(['orange', 'red']))
     ax[i].grid(linestyle="--")
     ax[i].set_ylabel("bernoulli parameters")
