6a4876bd2a1e1b3224df882bf796e1ec1796226d
Shakthi Visagan <shak360@gmail.com>
2020-06-22 16:43:34 -0700

    more work

--- lineage/LineageInputOutput.py
+++ lineage/LineageInputOutput.py
@@ -5,6 +5,4 @@ import pandas as pd
 
 def import_Heiser(path=r"~/Projects/CAPSTONE/lineage/data/heiser_data/LT_AU003_A3_4_Lapatinib_V2.xlsx"):
     excel_file = pd.read_excel(path)
-    data = excel_file.to_numpy()
-    assert data[0][16] == "Lineage Size"
-    
+    data = excel_file.to_numpy()    
