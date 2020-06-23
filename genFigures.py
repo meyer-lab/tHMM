bf6ef7acc503bbdc0f3f77e3c053226301bf6451
Shakthi Visagan <shak360@gmail.com>
2020-06-22 17:28:13 -0700

    more work

--- genFigures.py
+++ genFigures.py
@@ -7,6 +7,7 @@ import numpy as np
 matplotlib.use('AGG')
 
 fdir = './output/'
+cartoon_dir = r"./lineage/figures/cartoons"
 
 # TODO: Remove this one day.
 np.random.seed(1)
@@ -24,41 +25,41 @@ if __name__ == '__main__':
     if sys.argv[1] == '2':
         # Overlay Figure 2 cartoon
         overlayCartoon(fdir + 'figure2.svg',
-                       './lineage/data/figures/figure2.svg', 260, 250, scalee=0.2) 
+                       f'{cartoon_dir}/figure2.svg', 260, 250, scalee=0.2) 
     
     if sys.argv[1] == '3':
         # Overlay Figure 3 cartoon
         overlayCartoon(fdir + 'figure3.svg',
-                       './lineage/data/figures/figure3.svg', 270, 250, scalee=0.18)
+                       f'{cartoon_dir}/figure3.svg', 270, 250, scalee=0.18)
     
     if sys.argv[1] == '4':
         # Overlay Figure 4 cartoon
         overlayCartoon(fdir + 'figure4.svg',
-                       './lineage/data/figures/figure4.svg', 270, 250, scalee=0.18) 
+                       f'{cartoon_dir}/figure4.svg', 270, 250, scalee=0.18) 
         
     if sys.argv[1] == '5':
         # Overlay Figure 5 cartoon
         overlayCartoon(fdir + 'figure5.svg',
-                       './lineage/data/figures/figure5.svg', 270, 250, scalee=0.18)
+                       f'{cartoon_dir}/figure5.svg', 270, 250, scalee=0.18)
         
     if sys.argv[1] == '6':
         # Overlay Figure 6 cartoon
         overlayCartoon(fdir + 'figure6.svg',
-                       './lineage/data/figures/figure6.svg', 270, 250, scalee=0.19)
+                       f'{cartoon_dir}/figure6.svg', 270, 250, scalee=0.19)
     
     if sys.argv[1] == '7':
         # Overlay Figure 7 cartoon
         overlayCartoon(fdir + 'figure7.svg',
-                       './lineage/data/figures/figure7.svg', 270, 250, scalee=0.18)
+                       f'{cartoon_dir}/figure7.svg', 270, 250, scalee=0.18)
     
     if sys.argv[1] == '8':
         # Overlay Figure 8 cartoon
         overlayCartoon(fdir + 'figure8.svg',
-                       './lineage/data/figures/figure8.svg', 270, 250, scalee=0.18)
+                       f'{cartoon_dir}/figure8.svg', 270, 250, scalee=0.18)
     
     if sys.argv[1] == '9':
         # Overlay Figure 9 cartoon
         overlayCartoon(fdir + 'figure9.svg',
-                       './lineage/data/figures/figure9.svg', 270, 250, scalee=0.18)
+                       f'{cartoon_dir}/figure9.svg', 270, 250, scalee=0.18)
     
   
