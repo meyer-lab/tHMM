608e5a166a053b1f2e2c479cfc39105736dec147
farnazmdi <farnazmd95@gmail.com>
2020-06-25 21:52:49 -0700

    should be done

--- lineage/figures/figure4.py
+++ lineage/figures/figure4.py
@@ -9,9 +9,10 @@ from .figureCommon import (
     subplotLabel,
     commonAnalyze,
     pi,
-    T,
     E2,
-    lineage_good_to_analyze
+    lineage_good_to_analyze,
+    max_desired_num_cells,
+    num_data_points
 )
 from ..LineageTree import LineageTree
 from ..states.StateDistPhase import StateDistribution
@@ -41,27 +42,19 @@ def accuracy():
     """
 
     # Creating a list of populations to analyze over
-    list_of_Ts = [np.array([[i, 1.0 - i], [i, 1.0 - i]]) for i in np.linspace(0.01, 0.5, 50)]
+    list_of_Ts = [np.array([[i, 1.0 - i], [i, 1.0 - i]]) for i in np.linspace(0.1, 0.9, num_data_points)]
     list_of_populations = []
     list_of_fpi = []
     list_of_fT = []
     list_of_fE = []
-    list_cell_num_st1 = []
     for T in list_of_Ts:
         population = []
 
         good2go = False
         while not good2go:
-            tmp_lineage = LineageTree(pi, T, E2, 2**9-1)
+            tmp_lineage = LineageTree(pi, T, E2, max_desired_num_cells)
             good2go = lineage_good_to_analyze(tmp_lineage)
 
-        # how many cells are in state 1
-        cell_num_st1 = []
-        for cell in tmp_lineage.full_lineage:
-            if cell.state == 0:
-                cell_num_st1.append(1.0)
-
-        list_cell_num_st1.append(len(cell_num_st1))
         population.append(tmp_lineage)
 
         # Adding populations into a holder for analysing
@@ -70,8 +63,7 @@ def accuracy():
         list_of_fT.append(T)
         list_of_fE.append(E2)
 
-    cell_nums, _, _, _, _, _, accuracy = commonAnalyze(list_of_populations, list_of_fT=list_of_fT)
-    percentageS1 = [(a/b)*100 for a, b in zip(list_cell_num_st1, cell_nums)]
+    percentageS1, _, _, _, _, _, accuracy = commonAnalyze(list_of_populations, xtype="prop", list_of_fpi=list_of_fpi)
 
     dataframe = pd.DataFrame(columns=['% in S1', 'state acc.'])
     maxx = len(percentageS1)
@@ -87,9 +79,9 @@ def accuracy():
             newperc[indx] = np.round(np.mean(percentageS1[int(maxx * 3 / 4):int(maxx)]), 2)
     dataframe['state acc.'] = accuracy
     dataframe['% in S1'] = newperc
-    return percentageS1, accuracy
+    return dataframe, accuracy
 
-def figureMaker2(ax, percentageS1, accuracy):
+def figureMaker2(ax, dataframe, accuracy):
     """
     This makes figure 4.
     """
@@ -97,12 +89,10 @@ def figureMaker2(ax, percentageS1, accuracy):
     i = 0
     ax[i].axis('off')
     i += 1
-    # state accuracy
-    ax[i].scatter(percentageS1, accuracy)
-#     sns.boxplot(x="% in S1", y="state acc.", data=dataframe, ax=ax[i], palette="deep")
+    # state assignment accuracy
+    sns.boxplot(x="% in S1", y="state acc.", data=dataframe, ax=ax[i], palette="deep")
     ax[i].set_title("state assignemnt accuracy")
     ax[i].set_ylabel("accuracy (%)")
-    ax[i].text(1.0, 80, "total 511 cells")
     ax[i].grid(linestyle="--")
     ax[i].set_ylim(bottom=10.0, top=105.0)
     ax[i].tick_params(axis="both", which="major", grid_alpha=0.25)
