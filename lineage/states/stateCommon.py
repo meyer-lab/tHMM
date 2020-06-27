dcbca66372a39c333e01d097c5470a6738649408
Shakthi Visagan <shak360@gmail.com>
2020-06-27 12:09:44 -0700

    more work

--- lineage/states/stateCommon.py
+++ lineage/states/stateCommon.py
@@ -25,7 +25,7 @@ def bernoulli_estimator(bern_obs, gammas):
     """
     Add up all the 1s and divide by the total length (finding the average).
     """
-    gammas2use = [g in b,g in zip(bern_obs,gammas) if not math.isnan(b)]
+    gammas2use = [g for b,g in zip(bern_obs,gammas) if not math.isnan(b)]
     return sum(np.nan_to_num(gammas * bern_obs)) / sum(gammas2use)
 
 
@@ -44,8 +44,9 @@ def gamma_estimator(gamma_obs, time_censor_obs, gammas):
     This is a weighted, closed-form estimator for two parameters
     of the Gamma distribution.
     """
-    gammaCor = sum(gammas * gamma_obs) / sum(gammas)
-    s = np.log(gammaCor) - sum(gammas * np.log(gamma_obs)) / sum(gammas)
+    gammas2use = [g for b,g in zip(gamma_obs,gammas) if not math.isnan(b)]
+    gammaCor = sum(np.nan_to_num(gammas * gamma_obs)) / sum(gammas2use)
+    s = np.log(gammaCor) - sum(np.nan_to_num(gammas * np.log(gamma_obs))) / sum(gammas2use)
 
     def f(k): return np.log(k) - sc.polygamma(0, k) - s
 
