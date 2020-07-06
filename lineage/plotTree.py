9e95c9502a91a1c5f37aec58e76fff182088f510
farnazmdi <farnazmd95@gmail.com>
2020-07-06 16:11:43 -0700

    fixed?

--- lineage/plotTree.py
+++ lineage/plotTree.py
@@ -8,9 +8,9 @@ def fullRecursive(cell, a):
     a should be: a = [Clade(lineage1.full_lineage[0].obs[2]+lineage1.full_lineage[0].obs[3])]
     """
     if cell.isLeaf():
-        return Clade(branch_length=(cell.time))
+        return Clade(branch_length=cell.time)
     else:
-        return Clade(branch_length=(cell.time), clades=[fullRecursive(cell.left, a), fullRecursive(cell.right, a)])
+        return Clade(branch_length=cell.time, clades=[fullRecursive(cell.left, a), fullRecursive(cell.right, a)])
 
 def CensoredRecursive(cell, a):
     """ To plot the lineage while censored (from G1 or G2).
@@ -18,16 +18,18 @@ def CensoredRecursive(cell, a):
     If cell died in G2, the lifetime of the cell until dies is shown in blue.
     If none of the above, the cell continues to divide and is shown in black.
     a should be: a = [Clade(lineage1.full_lineage[0].obs[2]+lineage1.full_lineage[0].obs[3])]
+    If you are interested, you can take a look at the source code for creating Clades manually:
+    https://github.com/biopython/biopython/blob/fce4b11b4b8e414f1bf093a76e04a3260d782905/Bio/Phylo/BaseTree.py#L801
     """
     if cell.isLeaf():
         return Clade(branch_length=(cell.time))
     else:
         if cell.obs[0] == 0:
-            return Clade(branch_length=(cell.obs[2]), color="red") # dead in G1
+            return Clade(branch_length=cell.time, color="red") # dead in G1
         elif cell.obs[1] == 0:
-            return Clade(branch_length=(cell.obs[2]+cell.obs[3]), color="blue") # dead in G2
+            return Clade(branch_length=cell.time, color="blue") # dead in G2
         else:
-            return Clade(branch_length=(cell.time),
+            return Clade(branch_length=cell.time,
                          clades=[CensoredRecursive(cell.left, a), CensoredRecursive(cell.right, a)])
 
 def plotLineage(lineage, path):
