0ee0472b8b99c9fc8c185d0c7f311af9f398cfc7
lkargi <luka.karginov@gmail.com>
2020-07-05 20:32:20 -0500

    testing new run analyze aic

--- lineage/figures/figureS10.py
+++ lineage/figures/figureS10.py
@@ -17,7 +17,7 @@ def makeFigure():
     """
     Makes figure 10.
     """
-    ax, f = getSetup((7, 3), (1, 4))
+    ax, f = getSetup((7, 3), (1, 3))
 
     # bern, gamma_a, gamma_scale
     Sone = StateDistribution(0.99, 20, 5)
@@ -37,7 +37,7 @@ def makeFigure():
 desired_num_states = np.arange(1, 6)
 
 
-def run_AIC(relative_state_change, E, num_to_evaluate=10):
+def run_AIC(relative_state_change, E, num_lineages_to_evaluate=10):
     """
     Run's AIC for known lineages with known pi,
     and T values and stores the output for
@@ -48,29 +48,57 @@ def run_AIC(relative_state_change, E, num_to_evaluate=10):
     T = (np.eye(len(E)) + relative_state_change)
     T = T/np.sum(T, axis=1)[:,np.newaxis]
 
-    lineages = []
-    for _ in range(num_to_evaluate):
-        lineages.append([LineageTree.init_from_parameters(pi, T, E, 2**7-1)])
-    
-    ls = []
-    for lineage in lineages:
-        ls.append(lineage[0])
-    AICs = np.empty((len(lineages), num_states_shown)) 
-    for state in range(num_states_shown):
-        tHMM, _, LL = Analyze(ls, state+1)
-        AIC, _ = tHMM.get_AIC(LL)
-        for lineage in range(len(lineages)):
-            AICs[lineage][state] = AIC[lineage]
+    lineages = [LineageTree.init_from_parameters(pi, T, E, 2**7-1) for _ in range(num_lineages_to_evaluate)]
+    state_list = np.arange(1,6)
+
+   # AICs = np.empty((len(lineages), num_states_shown)) 
+   # for state in range(num_states_shown):
+   #     tHMM, _, LL = Analyze(lineages, state+1)
+   #     AIC, _ = tHMM.get_AIC(LL)
+   #     for lineage in range(len(lineages)):
+   #         AICs[lineage][state] = AIC[lineage]
    
-   # AICs = np.empty((len(lineages), num_states_shown))
-   # for states in range(0,num_states_shown):
-       # output = run_Analyze_over(lineages, states+1)
-       # for lineageNo in range(len(lineages)):  
-           # AIC, _ = output[lineageNo][0].get_AIC(output[lineageNo][2])
-           # AICs[lineageNo][states]= AIC[0]
+    AICs = np.empty((len(lineages), num_states_shown))
+    output = run_Analyze_AIC(lineages, state_list)
+    for idx, states in enumerate(state_list):
+        AIC, _ = output[idx][0].get_AIC(output[idx][2])
+        for lineageNo in range(len(lineages)):
+            AICs[lineageNo][idx]= AIC[lineageNo]
     
     return AICs.T
 
+
+def run_Analyze_AIC(population, state_list, **kwargs):
+    """
+    A function that can be parallelized to speed up figure creation.
+
+    This function is the outermost for-loop we will end up using
+    when analyzing heterogenous populations or lineages.
+
+    Analyze is the bottleneck in the figure creation process. The
+    rest of the code involved in figure creation deals with collecting
+    and computing certain statistics, most of which can be done in an
+    additional for loop over the results from Analyze.
+
+    :param population: A list of populations that contain lineages.
+    :type: list
+    :param state_list: An integer number of states to identify (a hyper-parameter of our model)
+    :type state_list: Int
+    """
+    list_of_fpi = kwargs.get("list_of_fpi", [None] * len(population))
+    list_of_fT = kwargs.get("list_of_fT", [None] * len(population))
+    list_of_fE = kwargs.get("list_of_fE", [None] * len(population))
+    output = []
+    exe = ProcessPoolExecutor()
+
+    prom_holder = []
+    for idx, num_states in enumerate(states_list):
+        prom_holder.append(exe.submit(Analyze, population, num_states, fpi=list_of_fpi[idx], fT=list_of_fT[idx], fE=list_of_fE[idx]))
+
+    for _, prom in enumerate(prom_holder):
+        output.append(prom.result())
+
+    return output
 
 def figure_maker(ax, AIC_holder):
     """
