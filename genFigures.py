f190c3e9c7914aa4eeb4f4db505153d97c46551f
jclagarde <jclagarde01@gmail.com>
2020-07-16 15:55:51 -0700

    overlayed figure 1

--- genFigures.py
+++ genFigures.py
@@ -111,4 +111,4 @@ if __name__ == '__main__':
     if sys.argv[1] == '1':
         #Overlay Figure 1 cartoon
         overlayCartoon(fdir + 'figure1.svg',
-                        f'{cartoon_dir}/figure1.svg', 0, 0, scalee=0.85)
+                        f'{cartoon_dir}/figure1.svg', 0, 0, scalee=0.77)
