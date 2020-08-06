5ed4a60fa84b65fcef6d38e835a7e86f6476453a
farnazmdi <farnazmd95@gmail.com>
2020-08-05 18:17:59 -0700

    fig2 edits

--- genFigures.py
+++ genFigures.py
@@ -128,7 +128,7 @@ if __name__ == '__main__':
     if sys.argv[1] == '2':
         # Overlay Figure 2 cartoon
         overlayCartoon(fdir + 'figure2.svg',
-                       f'{cartoon_dir}/figure2.svg', 0, 0, scalee=6.0)
+                       f'{cartoon_dir}/figure2.svg', 0, 0, scalee=0.098)
 
     if sys.argv[1] == '3':
         # Overlay Figure 3 cartoon
