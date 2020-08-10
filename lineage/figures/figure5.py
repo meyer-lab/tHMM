fb14877e676c78ca4e5cc85d97e488a0d0dd505f
farnazmdi <farnazmd95@gmail.com>
2020-08-10 16:14:49 -0700

    fig5 edits

--- lineage/figures/figure5.py
+++ lineage/figures/figure5.py
@@ -17,7 +17,7 @@ from .figureCommon import (
     scatter_kws_list,
 )
 from ..LineageTree import LineageTree
-from ..states.StateDistributionGamma import StateDistribution
+from ..states.StateDistributionGaPhs import StateDistribution
 
 
 def makeFigure():
@@ -26,7 +26,7 @@ def makeFigure():
     """
 
     # Get list of axis objects
-    ax, f = getSetup((6.9, 5), (2, 2))
+    ax, f = getSetup((7, 5), (2, 2))
     number_of_columns = 25
     figureMaker5(ax, *accuracy(number_of_columns))
 
@@ -38,7 +38,7 @@ def makeFigure():
 def accuracy(number_of_columns):
     """ A Helper function to create more random copies of a population. """
     # Creating a list of populations to analyze over
-    list_of_Es = [[StateDistribution(0.99, 12, a), StateDistribution(0.99, 12, 1)] for a in np.linspace(1, 5, num_data_points)]
+    list_of_Es = [[StateDistribution(0.99, 0.9, 12, a, 4, 5), StateDistribution(0.99, 0.8, 12, 1.5, 8, 5)] for a in np.linspace(1.5, 4, num_data_points)]
     list_of_populations = []
     list_of_fpi = []
     list_of_fT = []
@@ -46,13 +46,12 @@ def accuracy(number_of_columns):
     for E in list_of_Es:
         population = []
 
-        for _ in range(3):
-            good2go = False
-            while not good2go:
-                tmp_lineage = LineageTree.init_from_parameters(pi, T, E, 0.3*max_desired_num_cells)
-                good2go = lineage_good_to_analyze(tmp_lineage)
+        good2go = False
+        while not good2go:
+            tmp_lineage = LineageTree.init_from_parameters(pi, T, E, max_desired_num_cells)
+            good2go = lineage_good_to_analyze(tmp_lineage)
 
-            population.append(tmp_lineage)
+        population.append(tmp_lineage)
 
         # Adding populations into a holder for analysing
         list_of_populations.append(population)
@@ -61,11 +60,14 @@ def accuracy(number_of_columns):
         list_of_fE.append(E)
 
     wass, _, accuracy_after_switching, _, _, paramTrues = commonAnalyze(list_of_populations, xtype="wass", list_of_fpi=list_of_fpi, parallel=True)
+    for indx, a in enumerate(accuracy_after_switching):
+        if a <= 60:
+            print(list_of_populations[indx])
 
-    distribution_df = pd.DataFrame(columns=["Distribution type", "lifetime", "State"])
+    distribution_df = pd.DataFrame(columns=["Distribution type", "G1 lifetime", "State"])
     lineages = [list_of_populations[int(num_data_points * i / 4.)][0] for i in range(4)]
     len_lineages = [len(lineage) for lineage in lineages]
-    distribution_df["lifetime"] = [cell.obs[1] for lineage in lineages for cell in lineage.output_lineage]
+    distribution_df["G1 lifetime"] = [(cell.obs[1]+cell.obs[2]) for lineage in lineages for cell in lineage.output_lineage]
     distribution_df["State"] = ["State 1" if cell.state == 0 else "State 2" for lineage in lineages for cell in lineage.output_lineage]
     distribution_df["Distribution type"] = len_lineages[0] * ["Same"] +\
         len_lineages[1] * ["Similar"] +\
@@ -76,9 +78,7 @@ def accuracy(number_of_columns):
     wasser_df = pd.DataFrame(columns=["Wasserstein distance", "State Assignment Accuracy"])
     wasser_df["Wasserstein distance"] = wass
     wasser_df["State Assignment Accuracy"] = accuracy_after_switching
-
     return distribution_df, wasser_df
-
 
 def figureMaker5(ax, distribution_df, wasser_df):
     """
@@ -92,11 +92,11 @@ def figureMaker5(ax, distribution_df, wasser_df):
     ax[i].axis('off')
 
     i += 1
-    sns.violinplot(x="Distribution type", y="lifetime", hue="State", split=True, data=distribution_df, ax=ax[i])
+    sns.violinplot(x="Distribution type", y="G1 lifetime", hue="State", split=True, data=distribution_df, ax=ax[i])
 
     i += 1
     # state accuracy
     sns.regplot(x="Wasserstein distance", y="State Assignment Accuracy", data=wasser_df, ax=ax[i], lowess=True, marker='+', scatter_kws=scatter_kws_list[0])
     ax[i].set_title("State Assignment Accuracy")
     ax[i].set_ylabel("Accuracy [%]")
-    ax[i].set_ylim(bottom=50.0, top=101)
+    ax[i].set_ylim(bottom=10.0, top=101)
