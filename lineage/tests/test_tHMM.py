61bd99a0046015c8ce7d64862b2be8f479af61ab
Farnaz Mohammadi <farnazmd95@gmail.com>
2020-08-12 09:33:45 -0700

    figures 5, 6, S07 (#496)
    * figures 5,6,S07
    
    * fix test tHMM that keeps failing
    
    * decrease threshold
    
    * some more edits
    
    * fig5 edits
    
    * viterbi test fix
    
    * linting

--- lineage/tests/test_tHMM.py
+++ lineage/tests/test_tHMM.py
@@ -9,7 +9,7 @@ from ..UpwardRecursion import (
 from ..LineageTree import LineageTree
 from ..tHMM import tHMM
 from ..states.StateDistributionGaPhs import StateDistribution as StateDistPhase
-from ..figures.figureCommon import pi, T, E
+from ..figures.figureCommon import pi, T, E, lineage_good_to_analyze
 from ..Analyze import Analyze, Results
 
 
@@ -109,7 +109,14 @@ class TestModel(unittest.TestCase):
         Really defined states should get an accuracy >95%.
         Lineages used should be large and distinct.
         """
-        X = [LineageTree.init_from_parameters(self.pi, self.T, self.E, (2**10))]
+        X = []
+        for _ in range(10):
+            good2go = False
+            while not good2go:
+                tmp_lineage = LineageTree.init_from_parameters(self.pi, self.T, self.E, 2 ** 11 - 1)
+                good2go = lineage_good_to_analyze(tmp_lineage)
+         
+        X.append(tmp_lineage)
         tree_obj, predicted_states, LL = Analyze(X, 3)
         results_dict = Results(tree_obj, predicted_states, LL)
         accuracy = results_dict["balanced_accuracy_score"]
