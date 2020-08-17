d8f49e99acbc0255acebdaf8c09ab1fd5b2c69ca
jclagarde <jclagarde01@gmail.com>
2020-08-16 18:09:22 -0700

    more work

--- lineage/figures/figureS12.py
+++ lineage/figures/figureS12.py
@@ -49,6 +49,12 @@ def accuracy():
     the experiment time.
     """
 
+    pi2 = np.array([0.60, 0.40])
+    T2 = np.array([[0.75, 0.25], [0.1, 0.90]])
+    E2 = [StateDistPhase(0.99, 0.9, 20, 5, 10, 3), StateDistPhase(0.88, 0.75, 10, 2, 15, 4)]
+    
+    X_2 = [LineageTree.init_from_parameters(pi2, T2, E2, desired_num_cells=(2 ** 11) - 1)]
+    
     pi = np.array([0.55, 0.35, 0.10])
     T = np.array([[0.75, 0.20, 0.05], [0.1, 0.85, 0.05], [0.1, 0.1, 0.8]])
     E = [StateDistPhase(0.99, 0.9, 20, 5, 10, 3), StateDistPhase(0.88, 0.75, 10, 2, 15, 4), StateDistPhase(0.77, 0.85, 15, 7, 20, 5)]
@@ -60,13 +66,19 @@ def accuracy():
     for _ in range(10):
         good2go = False
         while not good2go:
+            tmp_lineage2 = LineageTree.init_from_parameters(pi2, T2, E2, 2 ** 11 - 1)
+            good2go = lineage_good_to_analyze(tmp_lineage2)
+            
+    for _ in range(10):
+        good2go = False
+        while not good2go:
             tmp_lineage = LineageTree.init_from_parameters(pi, T, E, 2 ** 11 - 1)
             good2go = lineage_good_to_analyze(tmp_lineage)
 
-    #X2.append(tmp_lineage)
-    #tree_obj, predicted_states, LL = Analyze(X, 2)
-    #results_dict = Results(tree_obj, predicted_states, LL)
-    #accuracy = results_dict["accuracy_after_switching"]
+    X2.append(tmp_lineage2)
+    tree_obj, predicted_states, LL = Analyze(X_2, 2)
+    results_dict = Results(tree_obj, predicted_states, LL)
+    accuracy = results_dict["accuracy_after_switching"]
 
         
     X3.append(tmp_lineage)
