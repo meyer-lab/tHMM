93dbf7ef0461a00953a72bee60c1996862301863
Farnaz Mohammadi <farnazmd95@gmail.com>
2020-08-18 10:12:42 -0700

    Update figureS12.py

--- lineage/figures/figureS12.py
+++ lineage/figures/figureS12.py
@@ -12,7 +12,6 @@ from .figureCommon import (
     getSetup,
     subplotLabel,
     commonAnalyze,
-    figureMaker,
     pi,
     T,
     E,
@@ -44,7 +43,7 @@ def accuracy():
     """
     Calculates accuracy and parameter estimation
     over an increasing number of lineages in a population for
-    a uncensored two-state model.
+    a uncensored lineages of 2, 3, and 4 states.
     We increase the desired number of cells in a lineage by
     the experiment time.
     """
@@ -52,40 +51,37 @@ def accuracy():
     pi2 = np.array([0.60, 0.40])
     T2 = np.array([[0.75, 0.25], [0.1, 0.90]])
     E2 = [StateDistPhase(0.99, 0.9, 20, 5, 10, 3), StateDistPhase(0.88, 0.75, 10, 2, 15, 4)]
-    
-    X_2 = [LineageTree.init_from_parameters(pi2, T2, E2, desired_num_cells=(2 ** 11) - 1)]
-    
+        
     pi3 = np.array([0.55, 0.35, 0.10]) 
     T3 = np.array([[0.75, 0.20, 0.05], [0.1, 0.85, 0.05], [0.1, 0.1, 0.8]])
     E3 = [StateDistPhase(0.99, 0.9, 20, 5, 10, 3), StateDistPhase(0.88, 0.75, 10, 2, 15, 4), StateDistPhase(0.77, 0.85, 15, 7, 20, 5)]
-    X_3 = [LineageTree.init_from_parameters(pi, T, E, desired_num_cells=(2 ** 11) - 1)]
     
     pi4 = np.array([0.55, 0.35, 0.06, 0.04])
     T4 = np.array([[0.70, 0.20, 0.05, 0.05], [0.1, 0.80, 0.06, 0.04], [0.1, 0.1, 0.6, 0.02], [0.1, 0.1, 0.5, 0.03]])
     E4 = [StateDistPhase(0.99, 0.9, 20, 5, 10, 3), StateDistPhase(0.88, 0.75, 10, 2, 15, 4), StateDistPhase(0.77, 0.85, 15, 7, 20, 5), StateDistPhase(0.66, 0.95, 17, 6, 15, 5)]
-    
-    X_4 = [LineageTree.init_from_parameters(pi4, T4, E4, desired_num_cells=(2 ** 11) - 1)]
-    
-  
-    #2 state population
+
+    # common for all three populations
     num_lineages = np.linspace(min_num_lineages, int(0.35 * max_num_lineages), num_data_points, dtype=int)
     experiment_times = np.linspace(1000, int(2.5 * 1000), num_data_points)
+
+    #2 state population
     list_of_populations2 = []
-    
+
     for indx, num in enumerate(num_lineages):
-        population2 = []
-    
-        for _ in range(10):
+        population = []
+        for _ in range(num):
+
             good2go = False
             while not good2go:
-                tmp_lineage2 = LineageTree.init_from_parameters(pi2, T2, E2, desired_num_cells=min_desired_num_cells, censor_condition=3, desired_experiment_time=experiment_times[indx])
-                good2go = lineage_good_to_analyze(tmp_lineage2)
-            
-            population2.append(tmp_lineage2)
-    
-    tree_obj, predicted_states, LL = run_Analyze_over(population2, 2)
-    results_dict = Results(tree_obj, predicted_states, LL)
-    accuracy_2 = results_dict["accuracy_after_switching"]
+                tmp_lineage = LineageTree.init_from_parameters(pi2, T2, E2, desired_num_cells=min_desired_num_cells, censor_condition=3, desired_experiment_time=experiment_times[indx])
+                good2go = lineage_good_to_analyze(tmp_lineage)
+
+            population.append(tmp_lineage)
+
+        # Adding populations into a holder for analysing
+        list_of_populations2.append(population)
+
+    cell_number_x2, _, accuracy2_after_switching, _, _, _ = commonAnalyze(list_of_populations2)
 
     #3 state population
     list_of_populations3 = []
@@ -121,4 +117,24 @@ def accuracy():
     results_dict = Results(tree_obj, predicted_states, LL)
     accuracy_4 = results_dict["accuracy_after_switching"]
 
-    return accuracy_2, accuracy_3, accuracy_4
+    # Create the dataframe for the data.
+    accuracy_df = pd.DataFrame(columns=["x2", "x3", "x4", "accuracy2", "accuracy3", "accuracy4"])
+    accuracy_df['x2'] = cell_number_x2
+    accuracy_df['accuracy2'] = accuracy2_after_switching
+    
+
+    return accuracy_df
+
+def figureMaker(accuracy_df):
+    """ This creates figure S12. Includes 3 subplots for accuracy of state assignment for 2, 3, and 4 states. """
+
+    i = 0
+    sns.regplot(x="x2", y="accuracy2", data=accuracy_df, ax=ax[i], lowess=True, marker='+', scatter_kws=scatter_kws_list[0])
+    ax[i].set_title("State Assignment Accuracy")
+    ax[i].set_ylabel("Accuracy [%]")
+    ax[i].set_ylim(bottom=25.0, top=101)
+
+    i += 1
+    
+    
+
