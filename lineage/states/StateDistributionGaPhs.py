0ed333fa02e3441d6d9d9f77398a49757e5370b0
Farnaz Mohammadi <farnazmd95@gmail.com>
2020-08-27 10:16:12 -0700

    Update StateDistributionGaPhs.py

--- lineage/states/StateDistributionGaPhs.py
+++ lineage/states/StateDistributionGaPhs.py
@@ -35,44 +35,48 @@ class StateDistribution:
         # distribution observations), so the likelihood of observing the multivariate observation is just the product of
         # the individual observation likelihoods.
 
-        bern_llG1 = 1
-        if not math.isnan(tuple_of_obs[0]):
-            # observed
-            assert tuple_of_obs[0] == 0 or tuple_of_obs[0] == 1
-            bern_llG1 = bern_pdf(tuple_of_obs[0], self.params[0])
+        tuple_of_obsG1 = list(zip(tuple_of_obs[0], tuple_of_obs[2], tuple_of_obs[4]))
+        tuple_of_obsG2 = list(zip(tuple_of_obs[1], tuple_of_obs[3], tuple_of_obs[5]))
+        G1_LL = self.G1.pdf(tuple_of_obsG1)
+        G2_LL = self.G1.pdf(tuple_of_obsG2)
+#         bern_llG1 = 1
+#         if not math.isnan(tuple_of_obs[0]):
+#             # observed
+#             assert tuple_of_obs[0] == 0 or tuple_of_obs[0] == 1
+#             bern_llG1 = bern_pdf(tuple_of_obs[0], self.params[0])
 
-        bern_llG2 = 1
-        if not math.isnan(tuple_of_obs[1]):
-            # observed
-            assert tuple_of_obs[1] == 0 or tuple_of_obs[1] == 1
-            bern_llG2 = bern_pdf(tuple_of_obs[1], self.params[1])
+#         bern_llG2 = 1
+#         if not math.isnan(tuple_of_obs[1]):
+#             # observed
+#             assert tuple_of_obs[1] == 0 or tuple_of_obs[1] == 1
+#             bern_llG2 = bern_pdf(tuple_of_obs[1], self.params[1])
 
-        gamma_llG1 = 1
-        if tuple_of_obs[4] == 1:
-            # uncensored
-            gamma_llG1 = gamma_pdf(tuple_of_obs[2], self.params[2], self.params[3])
-        elif tuple_of_obs[4] == 0:
-            # censored
-            gamma_llG1 = sp.gamma.sf(tuple_of_obs[2], a=self.params[2], scale=self.params[3])
-        else:
-            assert math.isnan(tuple_of_obs[4])
-            # G1 lifetime not observed
-            assert math.isnan(tuple_of_obs[2])
-            gamma_llG1 = 1
+#         gamma_llG1 = 1
+#         if tuple_of_obs[4] == 1:
+#             # uncensored
+#             gamma_llG1 = gamma_pdf(tuple_of_obs[2], self.params[2], self.params[3])
+#         elif tuple_of_obs[4] == 0:
+#             # censored
+#             gamma_llG1 = sp.gamma.sf(tuple_of_obs[2], a=self.params[2], scale=self.params[3])
+#         else:
+#             assert math.isnan(tuple_of_obs[4])
+#             # G1 lifetime not observed
+#             assert math.isnan(tuple_of_obs[2])
+#             gamma_llG1 = 1
 
-        gamma_llG2 = 1
-        if tuple_of_obs[5] == 1:
-            # uncensored
-            gamma_llG2 = gamma_pdf(tuple_of_obs[3], self.params[4], self.params[5])
-        elif tuple_of_obs[5] == 0:
-            # censored
-            gamma_llG2 = sp.gamma.sf(tuple_of_obs[3], a=self.params[4], scale=self.params[5])
-        elif math.isnan(tuple_of_obs[5]):
-            # unobserved
-            assert math.isnan(tuple_of_obs[3]) and math.isnan(tuple_of_obs[5]) and math.isnan(tuple_of_obs[1])
-            gamma_llG2 = 1
+#         gamma_llG2 = 1
+#         if tuple_of_obs[5] == 1:
+#             # uncensored
+#             gamma_llG2 = gamma_pdf(tuple_of_obs[3], self.params[4], self.params[5])
+#         elif tuple_of_obs[5] == 0:
+#             # censored
+#             gamma_llG2 = sp.gamma.sf(tuple_of_obs[3], a=self.params[4], scale=self.params[5])
+#         elif math.isnan(tuple_of_obs[5]):
+#             # unobserved
+#             assert math.isnan(tuple_of_obs[3]) and math.isnan(tuple_of_obs[5]) and math.isnan(tuple_of_obs[1])
+#             gamma_llG2 = 1
 
-        return bern_llG1 * bern_llG2 * gamma_llG1 * gamma_llG2
+        return G1_LL * G2_LL
 
     def estimator(self, list_of_tuples_of_obs, gammas, const):
         """ User-defined way of estimating the parameters given a list of the tuples of observations from a group of cells. """
@@ -96,13 +100,30 @@ class StateDistribution:
             shapeG2 = const[1]
 
         b1_mask = np.logical_not(np.isnan(bern_obsG1))
-        self.params[0] = bernoulli_estimator(bern_obsG1[b1_mask], gammas[b1_mask])
         b2_mask = np.logical_not(np.isnan(bern_obsG2))
-        self.params[1] = bernoulli_estimator(bern_obsG2[b2_mask], gammas[b2_mask])
         ga1_mask = np.logical_not(np.isnan(gamma_obsG1))
-        self.params[2], self.params[3] = gamma_estimator(gamma_obsG1[ga1_mask], gamma_censor_obsG1[ga1_mask], gammas[ga1_mask], shapeG1)
         ga2_mask = np.logical_not(np.isnan(gamma_obsG2))
-        self.params[4], self.params[5] = gamma_estimator(gamma_obsG2[ga2_mask], gamma_censor_obsG2[ga2_mask], gammas[ga2_mask], shapeG2)
+
+        list_of_tuples_of_obsG1 = zip(bern_obsG1, gamma_obsG1, gamma_censor_obsG1)
+        list_of_tuples_of_obsG2 = zip(bern_obsG2, gamma_obsG2, gamma_censor_obsG2)
+        
+        self.G1.estimator(list_of_tuples_of_obsG1, gammas[ga1_mask], const)
+        self.G2.estimator(list_of_tuples_of_obsG2, gammas[ga2_mask], const)
+
+        self.params[0] = self.G1.params[0]
+        self.params[1] = self.G2.params[0]
+        self.params[2] = self.G1.params[1]
+        self.params[3] = self.G2.params[1]
+        self.params[4] = self.G1.params[2]
+        self.params[5] = self.G2.params[2]
+#         b1_mask = np.logical_not(np.isnan(bern_obsG1))
+#         self.params[0] = bernoulli_estimator(bern_obsG1[b1_mask], gammas[b1_mask])
+#         b2_mask = np.logical_not(np.isnan(bern_obsG2))
+#         self.params[1] = bernoulli_estimator(bern_obsG2[b2_mask], gammas[b2_mask])
+#         ga1_mask = np.logical_not(np.isnan(gamma_obsG1))
+#         self.params[2], self.params[3] = gamma_estimator(gamma_obsG1[ga1_mask], gamma_censor_obsG1[ga1_mask], gammas[ga1_mask], shapeG1)
+#         ga2_mask = np.logical_not(np.isnan(gamma_obsG2))
+#         self.params[4], self.params[5] = gamma_estimator(gamma_obsG2[ga2_mask], gamma_censor_obsG2[ga2_mask], gammas[ga2_mask], shapeG2)
 
         assert not math.isnan(np.all(b1_mask)), f"b1 has nans after mask"
         assert not math.isnan(np.all(b2_mask)), f"b2 has nans after mask"
