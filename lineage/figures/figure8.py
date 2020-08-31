569ccff637600d5eb2679066b601ef24be44ee71
Aaron Meyer <ameyer@ucla.edu>
2020-08-31 08:18:09 -0700

    Fix.

--- lineage/figures/figure8.py
+++ lineage/figures/figure8.py
@@ -80,7 +80,7 @@ def run_AIC(relative_state_change, E, num_lineages_to_evaluate=10, censored=Fals
 
     # Storing AICs into array
     AICs = np.empty((len(desired_num_states), len(lineages)))
-    output = run_Analyze_over(lineages, desired_num_states)
+    output = run_Analyze_over([lineages] * len(desired_num_states), desired_num_states)
     for idx in range(len(desired_num_states)):
         AIC, _ = output[idx][0].get_AIC(output[idx][2], None)
         AICs[idx] = np.array([ind_AIC for ind_AIC in AIC])
