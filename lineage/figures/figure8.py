f7bc09438a0e02bf4f6194caa35e919d729920f7
farnazmdi <farnazmd95@gmail.com>
2020-09-01 09:07:50 -0700

    ylim

--- lineage/figures/figure8.py
+++ lineage/figures/figure8.py
@@ -110,7 +110,7 @@ def figure_maker(ax, AIC_Holder, true_state_no, upper_ylim, censored=False):
     ax.plot(desired_num_states, AIC_holder, "k", alpha=0.5)
     ax.set_ylabel("Normalized AIC")
     ax.set_yticks(np.linspace(0, upper_ylim, 5))
-    ax.set_ylim([0, upper_ylim])
+    ax.set_ylim([0, 1.1*upper_ylim])
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 
     # Adding title
