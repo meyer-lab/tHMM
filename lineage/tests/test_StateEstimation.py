bbf1deed5eeeb5e464192c409716c5b1f339def7
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-09-09 16:47:06 -0700

    Simplify estimator test. (#577)

--- lineage/tests/test_StateEstimation.py
+++ lineage/tests/test_StateEstimation.py
@@ -1,5 +1,5 @@
 """ Unit test file. """
-import unittest
+import pytest
 import numpy as np
 
 from ..LineageTree import LineageTree
@@ -7,41 +7,23 @@ from ..tHMM import tHMM
 from ..states.StateDistributionGamma import StateDistribution as gamma_state
 
 
-class TestModel(unittest.TestCase):
+@pytest.mark.parametrize("censored", [True, False])
+def test_estimationEvaluationGamma(censored):
     """
-    Unit test class for state distributions.
+    Evaluates the performance of fitting and the underlying estimator
+    by comparing the parameter estimates to their true values.
     """
+    pi = np.array([1])
+    T = np.array([[1]])
+    E_gamma = [gamma_state(bern_p=1., gamma_a=7, gamma_scale=4.5)]
 
-    def setUp(self):
-        """
-        Creates and fits one state lineages.
-        """
-        self.pi = np.array([1])
-        self.T = np.array([[1]])
-        self.E_gamma = [gamma_state(bern_p=1., gamma_a=7, gamma_scale=4.5)]
-        # Setting the bern_p to 1. ensures that all cells live and censoring is only
-        # due to living past the experiment time
+    if censored:
+        def gen(): return LineageTree.init_from_parameters(pi, T, E_gamma, 2**9, censor_condition=3, desired_experiment_time=100)
+    else:
+        def gen(): return LineageTree.init_from_parameters(pi, T, E_gamma, 2**9)
 
-    def test_estimationEvaluationGamma(self):
-        """
-        Evaluates the performance of fitting and the underlying estimator
-        by comparing the parameter estimates to their true values.
-        Gamma uncensored.
-        """
-        lineage_gamma = LineageTree.init_from_parameters(self.pi, self.T, self.E_gamma, 2**9)
-        solver_gamma = tHMM([lineage_gamma], 1)  # evaluating for one state
-        solver_gamma.fit()
-        self.assertGreater(2., np.linalg.norm(solver_gamma.estimate.E[0].params - self.E_gamma[0].params))
+    lineage_gamma = [gen() for _ in range(20)]
+    solver_gamma = tHMM(lineage_gamma, 1)  # evaluating for one state
+    solver_gamma.fit()
 
-    def test_estimationEvaluationGammaCensored(self):
-        """
-        Evaluates the performance of fitting and the underlying estimator
-        by comparing the parameter estimates to their true values.
-        Gamma censored.
-        """
-
-        def gen(): return LineageTree.init_from_parameters(self.pi, self.T, self.E_gamma, 2**9, censor_condition=3, desired_experiment_time=50)
-        lineage_gamma_censored = [gen() for _ in range(20)]
-        solver_gamma_censored = tHMM(lineage_gamma_censored, 1)  # evaluating for one state
-        solver_gamma_censored.fit()
-        self.assertGreater(5., np.linalg.norm(solver_gamma_censored.estimate.E[0].params - self.E_gamma[0].params))
+    assert solver_gamma.estimate.E[0].dist(E_gamma[0]) < 3.0
