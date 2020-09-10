67d0e73ec8e08b630b4eca8c169db9f31dfb948e
Aaron Meyer <ameyer@ucla.edu>
2020-09-10 10:05:26 -0700

    Add fixed shape test.

--- lineage/tests/test_StateEstimation.py
+++ lineage/tests/test_StateEstimation.py
@@ -7,7 +7,7 @@ from ..tHMM import tHMM
 from ..states.StateDistributionGamma import StateDistribution as gamma_state
 
 
-@pytest.mark.parametrize("censored", [True, False])
+@pytest.mark.parametrize("censored", [0, 3])
 @pytest.mark.parametrize("constant_shape", [True, False])
 def test_estimationEvaluationGamma(censored, constant_shape):
     """
@@ -19,15 +19,11 @@ def test_estimationEvaluationGamma(censored, constant_shape):
     E_gamma = [gamma_state(bern_p=1., gamma_a=7, gamma_scale=4.5)]
 
     if constant_shape:
-        E_gamma[0].const_shape = E_gamma[0].params[2]
+        E_gamma[0].const_shape = E_gamma[0].params[1]
 
-    if censored:
-        def gen(): return LineageTree.init_from_parameters(pi, T, E_gamma, 2**9, censor_condition=3, desired_experiment_time=100)
-    else:
-        def gen(): return LineageTree.init_from_parameters(pi, T, E_gamma, 2**9)
-
-    lineage_gamma = [gen() for _ in range(20)]
+    def gen(): return LineageTree.init_from_parameters(pi, T, E_gamma, 2**8, censor_condition=censored, desired_experiment_time=100)
+    lineage_gamma = [gen() for _ in range(30)]
     solver_gamma = tHMM(lineage_gamma, 1)  # evaluating for one state
     solver_gamma.fit()
 
-    assert solver_gamma.estimate.E[0].dist(E_gamma[0]) < 4.0
+    assert solver_gamma.estimate.E[0].dist(E_gamma[0]) < 3.0
