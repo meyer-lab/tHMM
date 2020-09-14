5e90bcb40fed509be188dba2dc64da115c93957d
farnazmdi <farnazmd95@gmail.com>
2020-09-14 15:48:26 -0700

    fixed for sure

--- lineage/figures/figure5.py
+++ lineage/figures/figure5.py
@@ -18,6 +18,22 @@ from .figureCommon import (
 # from ..Analyze import run_Analyze_over, run_Results_over
 from ..LineageTree import LineageTree
 
+# Creating a list of populations to analyze over
+num_lineages = np.linspace(min_num_lineages, max_num_lineages, num_data_points, dtype=int)
+experiment_times = np.linspace(1200, int(2.5 * 1000), num_data_points)
+
+list_of_populations = []
+for indx, num in enumerate(num_lineages):
+    population = []
+    for _ in range(num):
+        tmp_lineage = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=min_desired_num_cells, censor_condition=3, desired_experiment_time=experiment_times[indx])
+        if len(tmp_lineage.output_lineage) < 3:
+            pass
+        else:
+            population.append(tmp_lineage)
+
+    # Adding populations into a holder for analysing
+    list_of_populations.append(population)
 
 def makeFigure():
     """
@@ -27,12 +43,11 @@ def makeFigure():
     # Get list of axis objects
     ax, f = getSetup((13, 6.66), (3, 3))
     # dist_dist is 
-    figureMaker(ax, *accuracy(), dist_dist=True)
+    figureMaker(ax, *accuracy(), num_lineages=num_lineages, dist_dist=True)
 
     subplotLabel(ax)
 
     return f
-
 
 def accuracy():
     """
@@ -42,23 +57,5 @@ def accuracy():
     We increase the desired number of cells in a lineage by
     the experiment time.
     """
-
-
-    # Creating a list of populations to analyze over
-    num_lineages = np.linspace(min_num_lineages, max_num_lineages, num_data_points, dtype=int)
-    experiment_times = np.linspace(1200, int(2.5 * 1000), num_data_points)
-
-    list_of_populations = []
-    for indx, num in enumerate(num_lineages):
-        population = []
-        for _ in range(num):
-            tmp_lineage = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=min_desired_num_cells, censor_condition=3, desired_experiment_time=experiment_times[indx])
-            if len(tmp_lineage.output_lineage) < 3:
-                pass
-            else:
-                population.append(tmp_lineage)
-
-        # Adding populations into a holder for analysing
-        list_of_populations.append(population)
 
     return commonAnalyze(list_of_populations, 2)
