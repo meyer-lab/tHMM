beb4b60fa2605ca08cf02428e94d8d31dd744cb7
farnazmdi <farnazmd95@gmail.com>
2020-09-14 22:08:42 -0700

    figure11 shows emission distributions

--- lineage/figures/figure11.py
+++ lineage/figures/figure11.py
@@ -12,7 +12,7 @@ def makeFigure():
     """
     Makes figure 10.
     """
-    ax, f = getSetup((7, 12), (8, 2))
+    ax, f = getSetup((7, 6), (2, 2))
     data = [Gemcitabine_Control, Gem5uM, Gem10uM, Gem30uM, Lapatinib_Control, Lapt25uM, Lapt50uM, Lap250uM]
 
     lapatinib = []
@@ -26,12 +26,34 @@ def makeFigure():
 
     for idx, lapt_tHMMobj in enumerate(lapt_tHMMobj_list):
         for lin_indx, lin in enumerate(lapt_tHMMobj.X):
+            level1 = []
             for cell_indx, cell in enumerate(lin.output_lineage):
                 cell.state = lapt_states_list[idx][lin_indx][cell_indx]
-        lapatinib.append([lapt_tHMMobj.X[4], lapt_tHMMobj.X[7]])
+                lapatinib.append([cell.state, cell.obs[2], cell.obs[3]])
+
     for idx, gemc_tHMMobj in enumerate(gemc_tHMMobj_list):
         for lin_indx, lin in enumerate(gemc_tHMMobj.X):
             for cell_indx, cell in enumerate(lin.output_lineage):
                 cell.state = gemc_states_list[idx][lin_indx][cell_indx]
-        
-        gemcitabine.append([gemc_tHMMobj.X[0], gemc_tHMMobj.X[3]])
+                gemcitabine.append([cell.state, cell.obs[2], cell.obs[3]])
+
+    # plot
+    for i in range(2):
+        ax[i].scatter([a[0] for a in lapatinib], [a[i+1] for a in lapatinib], alpha=0.3, marker="+", c="#00ffff")
+        ax[i].set_ylabel("G1 phase lengths")
+        ax[i].set_xlabel("state")
+        ax[i].set_title("Lapatinib treatment")
+        ax[i].set_ylim(bottom=-20, top=180)
+        ax[i].xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax[1].set_ylabel("G2 phase lengths")
+
+    for i in range(2, 4):
+        ax[i].scatter([a[0] for a in gemcitabine], [a[i-1] for a in gemcitabine], alpha=0.3, marker="+", c="#feba4f")
+        ax[i].set_ylabel("G1 phase lengths")
+        ax[i].set_xlabel("state")
+        ax[i].set_title("Gemcitabine treatment")
+        ax[i].set_ylim(bottom=-20, top=180)
+        ax[i].xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax[3].set_ylabel("G2 phase lengths")
+
+    return f
