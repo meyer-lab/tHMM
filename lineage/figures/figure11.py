340d47a2aaf08f43d7b82b8aa25359dc45e7970c
farnazmdi <farnazmd95@gmail.com>
2020-09-15 20:00:20 -0700

    added stripplots

--- lineage/figures/figure11.py
+++ lineage/figures/figure11.py
@@ -1,7 +1,8 @@
 """ This file depicts the distribution of phase lengths versus the states. """
 import numpy as np
+import pandas as pd
 from matplotlib.ticker import MaxNLocator
-import itertools
+import seaborn as sns
 
 from ..Analyze import run_Analyze_over
 from ..data.Lineage_collections import Gemcitabine_Control, Gem5uM, Gem10uM, Gem30uM, Lapatinib_Control, Lapt25uM, Lapt50uM, Lap250uM
@@ -40,24 +41,32 @@ def makeFigure():
     Makes figure 11.
     """
     ax, f = getSetup((7, 6), (2, 2))
+    subplotLabel(ax)
+
+    Lap_data = pd.DataFrame(columns=["state", "G1 length", "G2 length"])
+    Lap_data["state"] = [a[0] for a in lapatinib]
+    Lap_data["G1 lengths"] = [a[1] for a in lapatinib]
+    Lap_data["G2 lengths"] = [a[2] for a in lapatinib]
+
+    Gem_data = pd.DataFrame(columns=["state", "G1 length", "G2 length"])
+    Gem_data["state"] = [a[0] for a in gemcitabine]
+    Gem_data["G1 lengths"] = [a[1] for a in gemcitabine]
+    Gem_data["G2 lengths"] = [a[2] for a in gemcitabine]
 
     # plot
-    for i in range(2):
-        ax[i].scatter([a[0] for a in lapatinib], [a[i+1] for a in lapatinib], alpha=0.3, marker="+", c="#00ffff")
-        ax[i].set_ylabel("G1 phase lengths")
-        ax[i].set_xlabel("state")
-        ax[i].set_title("Lapatinib treatment")
-        ax[i].set_ylim(bottom=-20, top=180)
-        ax[i].xaxis.set_major_locator(MaxNLocator(integer=True))
+    sns.stripplot(x="state", y="G1 lengths", data=Lap_data, linewidth=1, ax=ax[0], jitter=0.1)
+    sns.stripplot(x="state", y="G2 lengths", data=Lap_data, linewidth=1, ax=ax[1], jitter=0.1)
+    ax[0].set_ylabel("G1 phase lengths")
     ax[1].set_ylabel("G2 phase lengths")
-
-    for i in range(2, 4):
-        ax[i].scatter([a[0] for a in gemcitabine], [a[i-1] for a in gemcitabine], alpha=0.3, marker="+", c="#feba4f")
-        ax[i].set_ylabel("G1 phase lengths")
-        ax[i].set_xlabel("state")
-        ax[i].set_title("Gemcitabine treatment")
+    for i in range(4):
         ax[i].set_ylim(bottom=-20, top=180)
-        ax[i].xaxis.set_major_locator(MaxNLocator(integer=True))
+    for j in range(2):
+        ax[j].set_title("Lapatinib treatment")
+        ax[j+2].set_title("Gemcitabine treatment")
+    
+    sns.stripplot(x="state", y="G1 lengths", data=Gem_data, linewidth=1, ax=ax[2], jitter=0.1)
+    sns.stripplot(x="state", y="G2 lengths", data=Gem_data, linewidth=1, ax=ax[3], jitter=0.1)
+    ax[2].set_ylabel("G1 phase lengths")
     ax[3].set_ylabel("G2 phase lengths")
 
     return f
--- lineage/figures/figure12.py
+++ lineage/figures/figure12.py
@@ -1,9 +1,7 @@
 """ This file depicts the distribution of phase lengths versus the states. """
 import numpy as np
-from matplotlib.ticker import MaxNLocator
-import itertools
+import seaborn as sns
 
-from ..Analyze import run_Analyze_over
 from ..data.Lineage_collections import Gemcitabine_Control, Gem5uM, Gem10uM, Gem30uM, Lapatinib_Control, Lapt25uM, Lapt50uM, Lap250uM
 from .figureCommon import getSetup, subplotLabel
 from .figure11 import lapatinib12, gemcitabine12
@@ -13,24 +11,36 @@ def makeFigure():
     """
     Makes figure 12.
     """
-    ax, f = getSetup((7, 6), (2, 4))
-    concs = ["cntrl", "Lapt 25uM", "Lapt 50 uM", "Lapt 250 uM", "cntrl", "Gem 5uM", "Gem 10uM", "Gem 30uM"]
+    ax, f = getSetup((10, 10), (4, 4))
+    subplotLabel(ax)
 
-    # plot
+    concs = ["cntrl", "Lapt 25uM", "Lapt 50uM", "Lapt 250uM", "cntrl", "Gem 5uM", "Gem 10uM", "Gem 30uM"]
+    Lap = pd.DataFrame(columns=["state","cntrl G1", "Lapt 25uM G1", "Lapt 50uM G1", "Lapt 250uM G1", "cntrl G2", "Lapt 25uM G2", "Lapt 50uM G2", "Lapt 250uM G2"])
+    Gem = pd.DataFrame(columns=["state", "cntrl G1", "Gem 5uM G1", "Gem 10uM G1", "Gem 30uM G1", "cntrl G2", "Gem 5uM G2", "Gem 10uM G2", "Gem 30uM G2"])
+
+    # create the dataframe
     for i in range(4):
-        ax[i].scatter([a[0] for a in lapatinib12[i]], [a[1] for a in lapatinib12[i]], alpha=0.3, marker="+", c="#00ffff")
-        ax[i].set_ylabel("G1 phase lengths")
-        ax[i].set_xlabel("state")
-        ax[i].set_title(concs[i])
-        ax[i].set_ylim(bottom=-20, top=180)
-        ax[i].xaxis.set_major_locator(MaxNLocator(integer=True))
+        Lap["state"] = [a[0] for a in lapatinib12[i]]
+        Lap[str(concs[i])+" G1"] = [a[1] for a in lapatinib12[i]]
+        Lap[str(concs[i])+" G2"] = [a[2] for a in lapatinib12[i]]
+        Gem["state"] = [a[0] for a in gemcitabine12[i]]
+        Gem[str(concs[i+4])+" G1"] = [a[1] for a in gemcitabine12[i]]
+        Gem[str(concs[i+4])+" G2"] = [a[2] for a in gemcitabine12[i]]
 
-    for i in range(4, 8):
-        ax[i].scatter([a[0] for a in gemcitabine12[i-4]], [a[2] for a in gemcitabine12[i-4]], alpha=0.3, marker="+", c="#feba4f")
-        ax[i].set_ylabel("G2 phase lengths")
-        ax[i].set_xlabel("state")
+    # plot lapatinib
+    for i in range(4): # G1 lapatinib (first row), lapatinib G2 (second row)
+        sns.stripplot(x="state", y=str(concs[i]+" G1"), data=Lap, ax=ax[i], linewidth=1, jitter=0.1)
+        sns.stripplot(x="state", y=str(concs[i]+" G2"), data=Lap, ax=ax[i+4], linewidth=1, jitter=0.1)
         ax[i].set_title(concs[i])
-        ax[i].set_ylim(bottom=-20, top=180)
-        ax[i].xaxis.set_major_locator(MaxNLocator(integer=True))
+        ax[i].set_ylabel("G1 phase lengths")
+        ax[i+4].set_ylabel("G2 phase lengths")
+
+    # plot gemcitabine
+    for i in range(8, 12):
+        sns.stripplot(x="state", y=str(concs[i-4]+" G1"), data=Gem, ax=ax[i], linewidth=1, jitter=0.1)
+        sns.stripplot(x="state", y=str(concs[i-4]+" G2"), data=Gem, ax=ax[i+4], linewidth=1, jitter=0.1)
+        ax[i].set_title(concs[i])
+        ax[i].set_ylabel("G1 phase lengths")
+        ax[i+4].set_ylabel("G2 phase lengths")
 
     return f
