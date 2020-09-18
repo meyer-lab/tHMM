788dd7214769a1eed5169e45db96cd68a314d4e6
farnazmdi <farnazmd95@gmail.com>
2020-09-18 08:13:35 -0700

    edits

--- lineage/figures/figure11.py
+++ lineage/figures/figure11.py
@@ -7,7 +7,7 @@ from ..Analyze import run_Analyze_over
 from ..data.Lineage_collections import Gemcitabine_Control, Gem5uM, Gem10uM, Gem30uM, Lapatinib_Control, Lapt25uM, Lapt50uM, Lap250uM
 from .figureCommon import getSetup, subplotLabel
 
-concs = ["cntrl", "Lapt 25uM", "Lapt 50uM", "Lapt 250uM", "cntrl", "Gem 5uM", "Gem 10uM", "Gem 30uM"]
+concs = ["cntrl", "Lapt 25nM", "Lapt 50nM", "Lapt 250nM", "cntrl", "Gem 5nM", "Gem 10nM", "Gem 30nM"]
 data = [Lapatinib_Control, Lapt25uM, Lapt50uM, Lap250uM, Gemcitabine_Control, Gem5uM, Gem10uM, Gem30uM]
 
 # Run fitting
@@ -17,6 +17,20 @@ lapt_states_list = [oo[1] for oo in output[0: 4]]
 gemc_tHMMobj_list = [oo[0] for oo in output[4: 8]]
 gemc_states_list = [oo[1] for oo in output[4: 8]]
 
+def twice(tHMMobj, state):
+    g1 = []
+    g2 = []
+    for lin_indx, lin in enumerate(tHMMobj.X): # for each lineage list
+        for cell_indx, cell in enumerate(lin.output_lineage): # for each cell in the lineage
+            g1.append(cell.obs[2])
+            g2.append(cell.obs[3])
+
+    state = list(itertools.chain(*states)) + list(itertools.chain(*states))
+    phaseLength = g1 + g2
+    phase = len(g1) * ["G1"] + len(g2) * ["G2"]
+    return state, phaseLength, phase
+
+
 def makeFigure():
     """ Makes figure 11. """
 
@@ -25,16 +39,7 @@ def makeFigure():
 
     # lapatinib
     for idx, lapt_tHMMobj in enumerate(lapt_tHMMobj_list): # for each concentration data
-        lpt_g1 = []
-        lpt_g2 = []
-        for lin_indx, lin in enumerate(lapt_tHMMobj.X): # for each lineage list
-            for cell_indx, cell in enumerate(lin.output_lineage): # for each cell in the lineage
-                lpt_g1.append(cell.obs[2])
-                lpt_g2.append(cell.obs[3])
-
-        LAP_state = list(itertools.chain(*lapt_states_list[idx])) + list(itertools.chain(*lapt_states_list[idx]))
-        LAP_phaseLength = lpt_g1 + lpt_g2
-        Lpt_phase = len(lpt_g1) * ["G1"] + len(lpt_g2) * ["G2"]
+        LAP_state, LAP_phaseLength, Lpt_phase = twicw(lapt_tHMMobj, lapt_states_list[idx])
 
         # plot lapatinib
         sns.stripplot(x=LAP_state, y=LAP_phaseLength, hue=Lpt_phase, size=1, palette="Set2", linewidth=0.05, dodge=True, ax=ax[idx])
@@ -45,19 +50,12 @@ def makeFigure():
         ax[idx+4].set_ylabel("phase lengths")
         ax[idx].set_xlabel("state")
         ax[idx+4].set_xlabel("state")
+        ax[idx].set_yticks([0, 160])
+        ax[idx+4].set_yticks([0, 160])
 
     # gemcitabine
     for idx, gemc_tHMMobj in enumerate(gemc_tHMMobj_list):
-        gmc_g1 = []
-        gmc_g2 = []
-        for lin_indx, lin in enumerate(gemc_tHMMobj.X):
-            for cell_indx, cell in enumerate(lin.output_lineage):
-                gmc_g1.append(cell.obs[2])
-                gmc_g2.append(cell.obs[3])
-
-        GEM_state = list(itertools.chain(*gemc_states_list[idx])) + list(itertools.chain(*gemc_states_list[idx]))
-        GEM_phaseLength = gmc_g1 + gmc_g2
-        GEM_phase = len(gmc_g1) * ["G1"] + len(gmc_g2) * ["G2"]
+        GEM_state, GEM_phaseLength, GEM_phase = twicw(gemc_tHMMobj, gemc_states_list[idx])
         sns.stripplot(x=GEM_state, y=GEM_phaseLength, hue=GEM_phase, size=1, palette="Set2", linewidth=0.05, dodge=True, ax=ax[idx+4])
 
     return f
