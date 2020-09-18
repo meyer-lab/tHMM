b0817400e8e487b45a100c0e8d9230df2ba9e19a
farnazmdi <farnazmd95@gmail.com>
2020-09-17 22:31:22 -0700

    figure 11 done i guess

--- lineage/figures/figure0.py
+++ lineage/figures/figure0.py
@@ -11,11 +11,11 @@ def makeFigure():
     ax, f = getSetup((4,4), (1, 1))
     subplotLabel(ax)
 
-    a = pd.DataFrame(columns=["state", "length"])
-    a["state"] = [0, 0, 2, 3, 2, 1, 1, 3, 3, 4]
-    a["length"] = [np.nan, 10, -20, 30, np.nan, 50, 60, np.nan, 70, 80]
-
-    sns.stripplot(x="state", y="length", data=a, ax=ax[0])
+    # a = pd.DataFrame(columns=["state", "length"])
+    a = [0, 0, 2, 3, 2, 1, 1, 3, 3, 4]
+    b = [np.nan, 10, -20, 30, np.nan, 50, 60, np.nan, 70, 80]
+    c = ["a", "a", "a", "a", "a", "b", "b", "b", "b", "b"]
+    sns.stripplot(x=a, y=b, hue=c, ax=ax[0])
 
 
     return f
--- lineage/figures/figure11.py
+++ lineage/figures/figure11.py
@@ -19,54 +19,51 @@ lapt_states_list = [oo[1] for oo in output[0: 4]]
 gemc_tHMMobj_list = [oo[0] for oo in output[4: 8]]
 gemc_states_list = [oo[1] for oo in output[4: 8]]
 
-# lapatinib
-LAP = pd.DataFrame(columns=["state", "phase"])
-for idx, lapt_tHMMobj in enumerate(lapt_tHMMobj_list): # for each concentration data
-    lpt_g1 = []
-    lpt_g2 = []
-    for lin_indx, lin in enumerate(lapt_tHMMobj.X): # for each lineage list
-        for cell_indx, cell in enumerate(lin.output_lineage): # for each cell in the lineage
-            lpt_g1.append(cell.obs[2])
-            lpt_g2.append(cell.obs[3])
-
-    LAP["state "+str(concs[idx])] = list(itertools.chain(*lapt_states_list[idx]))
-    LAP["phase lengths "+str(concs[idx])] = lpt_g1 + lpt_g2
-    LAP["phase "+str(concs[idx])] = len(lpt_g1) * ["G1"] + len(lpt_g2) * ["G2"]
-
-# gemcitabine
-GEM = pd.DataFrame(columns=["state", "phase"])   
-for idx, gemc_tHMMobj in enumerate(gemc_tHMMobj_list):
-    gmc_g1 = []
-    gmc_g2 = []
-    for lin_indx, lin in enumerate(gemc_tHMMobj.X):
-        for cell_indx, cell in enumerate(lin.output_lineage):
-            gmc_g1.append(cell.obs[2])
-            gmc_g2.append(cell.obs[3])
-
-    GEM["state "+str(concs[idx+4])] = list(itertools.chain(*gemc_states_list[idx]))
-    GEM["phase lengths "+str(concs[idx+4])] = gmc_g1 + gmc_g2
-    GEM["phase "+str(concs[idx+4])] = len(gmc_g1) * ["G1"] + len(gmc_g2) * ["G2"]
-
 def makeFigure():
-    """
-    Makes figure 11.
-    """
+    """ Makes figure 11. """
+
     ax, f = getSetup((13.2, 6.66), (2, 4))
     subplotLabel(ax)
 
-    for i in range(4):
-        sns.stripplot(x="state "+str(concs[i]), y="phase lengths "+str(concs[i]), hue="phase "+str(concs[i]), data=LAP, size=1, palette="Set2", linewidth=0.05, dodge=True, ax=ax[i])
-        sns.stripplot(x="state "+str(concs[i+4]), y="phase lengths "+str(concs[i+4]), hue="phase "+str(concs[i+4]), data=GEM, size=1, palette="Set2", linewidth=0.05, dodge=True, ax=ax[i+4])
+    # lapatinib
+    for idx, lapt_tHMMobj in enumerate(lapt_tHMMobj_list): # for each concentration data
+        lpt_g1 = []
+        lpt_g2 = []
+        for lin_indx, lin in enumerate(lapt_tHMMobj.X): # for each lineage list
+            for cell_indx, cell in enumerate(lin.output_lineage): # for each cell in the lineage
+                lpt_g1.append(cell.obs[2])
+                lpt_g2.append(cell.obs[3])
 
-        ax[i].set_title(concs[i])
-        ax[i+4].set_title(concs[i+4])
-        ax[i].set_ylabel("phase lengths")
-        ax[i+4].set_ylabel("phase lengths")
+        LAP_state = list(itertools.chain(*lapt_states_list[idx])) + list(itertools.chain(*lapt_states_list[idx]))
+        LAP_phaseLength = lpt_g1 + lpt_g2
+        Lpt_phase = len(lpt_g1) * ["G1"] + len(lpt_g2) * ["G2"]
+        print(len(LAP_state), len(LAP_phaseLength), len(Lpt_phase))
+        # plot lapatinib
+        sns.stripplot(x=LAP_state, y=LAP_phaseLength, hue=Lpt_phase, size=1, palette="Set2", linewidth=0.05, dodge=True, ax=ax[idx])
+
+        ax[idx].set_title(concs[idx])
+        ax[idx+4].set_title(concs[idx+4])
+        ax[idx].set_ylabel("phase lengths")
+        ax[idx+4].set_ylabel("phase lengths")
 
         # this removes title of legends
-        handles, labels = ax[i].get_legend_handles_labels()
-        ax[i].legend(handles=handles[1:], labels=labels[1:])
-        handles, labels = ax[i+4].get_legend_handles_labels()
-        ax[i+4].legend(handles=handles[1:], labels=labels[1:])
+        handles, labels = ax[idx].get_legend_handles_labels()
+        ax[idx].legend(handles=handles[1:], labels=labels[1:])
+        handles, labels = ax[idx+4].get_legend_handles_labels()
+        ax[idx+4].legend(handles=handles[1:], labels=labels[1:])
+
+    # gemcitabine
+    for idx, gemc_tHMMobj in enumerate(gemc_tHMMobj_list):
+        gmc_g1 = []
+        gmc_g2 = []
+        for lin_indx, lin in enumerate(gemc_tHMMobj.X):
+            for cell_indx, cell in enumerate(lin.output_lineage):
+                gmc_g1.append(cell.obs[2])
+                gmc_g2.append(cell.obs[3])
+
+        GEM_state = list(itertools.chain(*gemc_states_list[idx])) + list(itertools.chain(*gemc_states_list[idx]))
+        GEM_phaseLength = gmc_g1 + gmc_g2
+        GEM_phase = len(gmc_g1) * ["G1"] + len(gmc_g2) * ["G2"]
+        sns.stripplot(x=GEM_state, y=GEM_phaseLength, hue=GEM_phase, size=1, palette="Set2", linewidth=0.05, dodge=True, ax=ax[idx+4])
 
     return f
