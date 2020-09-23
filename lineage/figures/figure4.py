0efb1fe8031381016632c7919acedf0de8610834
farnazmdi <farnazmd95@gmail.com>
2020-09-23 16:02:28 -0700

    fig 4

--- lineage/figures/figure4.py
+++ lineage/figures/figure4.py
@@ -27,17 +27,17 @@ scatter_state_1_kws = {
 }
 
 
-def regGen():
-    tmp = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=2**5 - 1)
+def regGen(num):
+    tmp = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=num)
     while len(tmp.output_lineage) < 5:
-        tmp = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=2**5 - 1)
+        tmp = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=num)
     return tmp
 
 
-def cenGen():
-    tmp = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=2**6 - 1, censor_condition=3, desired_experiment_time=250)
+def cenGen(num):
+    tmp = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=num, censor_condition=3, desired_experiment_time=200)
     while len(tmp.output_lineage) < 5:
-        tmp = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=2**6 - 1, censor_condition=3, desired_experiment_time=250)
+        tmp = LineageTree.init_from_parameters(pi, T, E2, desired_num_cells=num, censor_condition=3, desired_experiment_time=200)
     return tmp
 
 
@@ -48,11 +48,11 @@ def makeFigure():
     x_Sim, x_Cen, Accuracy_Sim, Accuracy_Cen, _, _ = accuracy()
 
     # Get list of axis objects
-    ax, f = getSetup((6.5, 4), (3, 2))
+    ax, f = getSetup((6, 4), (2, 3))
     figureMaker3(ax, x_Sim, x_Cen, Accuracy_Sim, Accuracy_Cen)
     ax[0].text(-0.2, 1.22, ascii_lowercase[0], transform=ax[0].transAxes, fontsize=16, fontweight="bold", va="top")
-    ax[1].text(-0.2, 1.22, ascii_lowercase[1], transform=ax[1].transAxes, fontsize=16, fontweight="bold", va="top")
-    ax[4].text(-0.2, 1.7, ascii_lowercase[2], transform=ax[4].transAxes, fontsize=16, fontweight="bold", va="top")
+    ax[2].text(-0.2, 1.22, ascii_lowercase[1], transform=ax[2].transAxes, fontsize=16, fontweight="bold", va="top")
+    ax[3].text(-0.2, 1.7, ascii_lowercase[2], transform=ax[3].transAxes, fontsize=16, fontweight="bold", va="top")
     ax[5].text(-0.2, 1.7, ascii_lowercase[3], transform=ax[5].transAxes, fontsize=16, fontweight="bold", va="top")
 
     return f
@@ -69,11 +69,12 @@ def accuracy():
 
     # Creating a list of populations to analyze over
     num_lineages = np.linspace(min_num_lineages, max_num_lineages, num_data_points, dtype=int)
+    num_cells = np.linspace(5, 31, num_data_points, dtype=int)
     list_of_fpi = [pi] * num_lineages.size
 
     # Adding populations into a holder for analysing
-    list_of_populations = [[regGen() for _ in range(num)] for num in num_lineages]
-    list_of_populationsSim = [[cenGen() for _ in range(num)] for num in num_lineages]
+    list_of_populations = [[regGen(num_cells[i]) for _ in range(num)] for i, num in enumerate(num_lineages)]
+    list_of_populationsSim = [[cenGen(num_cells[i]) for _ in range(num)] for i, num in enumerate(num_lineages)]
 
     x_Sim, _, output_Sim, _ = commonAnalyze(list_of_populationsSim, 2, list_of_fpi=list_of_fpi)
     x_Cen, _, output_Cen, _ = commonAnalyze(list_of_populations, 2, list_of_fpi=list_of_fpi)
@@ -96,28 +97,28 @@ def figureMaker3(ax, x_Sim, output_Sim, x_Cen, output_Cen, xlabel="Number of Cel
     accuracy_cen_df["State Assignment Accuracy"] = Accuracy_Cen
 
     i = 0
-    plotLineage(regGen(), axes=ax[i], censore=False)
+    plotLineage(regGen(31), axes=ax[i], censore=False)
     ax[i].axis('off')
 
     i = 1
-    plotLineage(cenGen(), axes=ax[i], censore=True)
+    plotLineage(regGen(31), axes=ax[i], censore=False)
     ax[i].axis('off')
 
     i = 2
-    plotLineage(regGen(), axes=ax[i], censore=False)
-    ax[i].axis('off')
-
-    i = 3
-    plotLineage(cenGen(), axes=ax[i], censore=True)
-    ax[i].axis('off')
-
-    i = 4
     ax[i].axhline(y=100, ls='--', c='k', alpha=0.5)
     sns.regplot(x="Cell number", y="State Assignment Accuracy", data=accuracy_sim_df, ax=ax[i], lowess=True, marker='+', scatter_kws=scatter_state_1_kws)
     ax[i].set_xlabel(xlabel)
     ax[i].set_ylim(bottom=50, top=101)
     ax[i].set_ylabel(r"State Accuracy [%]")
     ax[i].set_title("Full lineage data")
+
+    i = 3
+    plotLineage(cenGen(31), axes=ax[i], censore=True)
+    ax[i].axis('off')
+
+    i = 4
+    plotLineage(cenGen(31), axes=ax[i], censore=True)
+    ax[i].axis('off')
 
     i = 5
     ax[i].axhline(y=100, ls='--', c='k', alpha=0.5)
