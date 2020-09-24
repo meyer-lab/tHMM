f9a3c62fefe524ead3304340bbca7beeca1f94f6
farnazmdi <farnazmd95@gmail.com>
2020-09-24 16:28:32 -0700

    figure adjustments

--- genFigures.py
+++ genFigures.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
     if sys.argv[1] == '5':
         # Overlay Figure 5 cartoon
         overlayCartoon(fdir + 'figure5.svg',
-                       f'{cartoon_dir}/figure5.svg', 10, 5, scalee=0.91)
+                       f'{cartoon_dir}/figure5.svg', 10, 5, scalee=0.75)
 
     if sys.argv[1] == '6':
         # Overlay Figure 6 cartoon
