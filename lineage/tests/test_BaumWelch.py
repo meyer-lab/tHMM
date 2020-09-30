ebff3dc413019727ac745d603fb2d1f7485090f1
Aaron Meyer <ameyer@ucla.edu>
2020-09-30 09:51:30 -0700

    Simplify test_BW

--- lineage/tests/test_BaumWelch.py
+++ lineage/tests/test_BaumWelch.py
@@ -1,73 +1,28 @@
 """ Unit test file. """
-import unittest
+import pytest
 import numpy as np
 from ..BaumWelch import do_E_step, calculate_log_likelihood
 from ..LineageTree import LineageTree
 from ..tHMM import tHMM
-from ..states.StateDistributionGaPhs import StateDistribution as StateDistPhase
 from ..figures.figureCommon import pi, T, E
 
 
-class TestBW(unittest.TestCase):
-    """ Unit tests for Baum-Welch methods. """
+@pytest.mark.parametrize("cens", [0, 1, 2, 3])
+@pytest.mark.parametrize("nStates", [1, 2, 3])
+def test_BW(cens, nStates):
+    """ This tests that one step of Baum-Welch increases the likelihood of the fit. """
+    X = LineageTree.init_from_parameters(pi, T, E, desired_num_cells=(2 ** 7) - 1, desired_experimental_time=500, censor_condition=cens)
+    tHMMobj = tHMM([X], num_states=nStates)  # build the tHMM class with X
 
-    def setUp(self):
-        """ This setup will be used to test the model for the cases with 3 number of states. """
-        # ingredients for LineageTree with 3 states
-        self.pi = np.array([0.55, 0.35, 0.10])
-        self.T = np.array(
-            [[0.75, 0.20, 0.05], [0.1, 0.85, 0.05], [0.1, 0.1, 0.8]])
+    # Test cases below
+    # Get the likelihoods before fitting
+    _, NF, _, _ = do_E_step(tHMMobj)
+    LL_before = calculate_log_likelihood(NF)
+    assert np.isfinite(LL_before)
 
-        # Emissions
-        self.E = [StateDistPhase(0.99, 0.99, 20, 5, 10, 3),
-                  StateDistPhase(0.99, 0.99, 10, 2, 15, 4),
-                  StateDistPhase(0.99, 1.0, 15, 7, 20, 5)]
-
-    def commonTest(self, **kwargs):
-        """ This tests that one step of Baum-Welch increases the likelihood of the fit. """
-        X = LineageTree.init_from_parameters(pi, T, E, desired_num_cells=(2 ** 8) - 1, **kwargs)
-        tHMMobj = tHMM([X], num_states=2)  # build the tHMM class with X
-
-        X3s = LineageTree.init_from_parameters(self.pi, self.T, self.E, desired_num_cells=(2 ** 8) - 1, **kwargs)
-        tHMMobj3s = tHMM([X3s], num_states=3)
-
-        # Test cases below
-        # Get the likelihoods before fitting
-        _, NF, _, _ = do_E_step(tHMMobj)
-        LL_before = calculate_log_likelihood(NF)
-        self.assertTrue(np.isfinite(LL_before))
-
-        # For 3 states
-        _, NF3s, _, _ = do_E_step(tHMMobj3s)
-        LL_before3 = calculate_log_likelihood(NF3s)
-        self.assertTrue(np.isfinite(LL_before3))
-
-        # Get the likelihoods after fitting
-        _, _, NF_after, _, _, new_LL_list_after = tHMMobj.fit(max_iter=3)
-        LL_after = calculate_log_likelihood(NF_after)
-        self.assertTrue(np.isfinite(LL_after))
-        self.assertTrue(np.isfinite(new_LL_list_after))
-        self.assertGreater(LL_after, LL_before)
-
-        # for 3 states
-        _, _, NF_after3, _, _, new_LL_list_after3 = tHMMobj3s.fit(max_iter=3)
-        LL_after3 = calculate_log_likelihood(NF_after3)
-        self.assertTrue(np.isfinite(LL_after3))
-        self.assertTrue(np.isfinite(new_LL_list_after3))
-        self.assertGreater(LL_after3, LL_before3)
-
-    def test_step(self):
-        """ This tests that one step of Baum-Welch increases the likelihood of the fit. """
-        self.commonTest()
-
-    def test_step1(self):
-        """ This tests that one step of Baum-Welch increases the likelihood of the fit. """
-        self.commonTest(censor_condition=1)
-
-    def test_step2(self):
-        """ This tests that one step of Baum-Welch increases the likelihood of the fit. """
-        self.commonTest(censor_condition=2, desired_experimental_time=500)
-
-    def test_step3(self):
-        """ This tests that one step of Baum-Welch increases the likelihood of the fit. """
-        self.commonTest(censor_condition=3, desired_experimental_time=1000)
+    # Get the likelihoods after fitting
+    _, _, NF_after, _, _, new_LL_list_after = tHMMobj.fit(max_iter=3)
+    LL_after = calculate_log_likelihood(NF_after)
+    assert np.isfinite(LL_after)
+    assert np.isfinite(new_LL_list_after)
+    assert LL_after > LL_before
