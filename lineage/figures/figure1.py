7a440cc0944afb3ae355c2d09530d2505962ab77
Farnaz Mohammadi <farnazmd95@gmail.com>
2021-01-10 20:32:23 -0800

    fig 1 updated (#731)

--- lineage/figures/figure1.py
+++ lineage/figures/figure1.py
@@ -28,31 +28,31 @@ def figureMaker(ax):
     """
     Makes figure 1.
     """
-    plotLineage(Gem5uM[13], ax[0], censore=False)
-    ax[0].set_title("Gemcitabine 5 nM", fontsize=10)
-    ax[0].axis('off')
+    plotLineage(Gem5uM[13], ax[1], censore=False, color=False)
+    ax[1].set_title("Gemcitabine 5 nM", fontsize=10)
+    ax[1].axis('off')
 
-    plotLineage(Gem5uM[4], ax[2], censore=False)
-    ax[2].axis('off')
+    plotLineage(Gem5uM[4], ax[3], censore=False, color=False)
+    ax[3].axis('off')
 
-    plotLineage(Gem5uM[10], ax[4], censore=False)
-    ax[4].axis('off')
+    plotLineage(Gem5uM[10], ax[5], censore=False, color=False)
+    ax[5].axis('off')
 
     for cell in gem[3].output_lineage:
         cell.state = 0
     for cell in gem[9].output_lineage:
         cell.state = 1
-    gem[2].output_lineage[0].state = 1
+    gem[3].output_lineage[0].state = 1
 
-    plotLineage(gem[3], ax[1], censore=False)
-    ax[1].set_title("Control", fontsize=10)
-    ax[1].axis('off')
+    plotLineage(gem[3], ax[0], censore=False, color=False)
+    ax[0].set_title("Control", fontsize=10)
+    ax[0].axis('off')
 
-    plotLineage(gem[9], ax[3], censore=False)
-    ax[3].axis('off')
+    plotLineage(gem[9], ax[2], censore=False, color=False)
+    ax[2].axis('off')
 
-    plotLineage(gem[2], ax[5], censore=False)
-    ax[5].axis('off')
+    plotLineage(gem[2], ax[4], censore=False, color=False)
+    ax[4].axis('off')
 
-    ax[6].axis('off')
     ax[7].axis('off')
+    ax[6].axis('off')
--- lineage/figures/figure9.py
+++ lineage/figures/figure9.py
@@ -7,7 +7,7 @@ from ..data.Lineage_collections import Gemcitabine_Control, Gem5uM, Gem10uM, Gem
 
 from .figureCommon import getSetup, subplotLabel
 
-desired_num_states = np.arange(1, 6)
+desired_num_states = np.arange(1, 9)
 
 
 def makeFigure():
