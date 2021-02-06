040c8ef211c4abe7ddcbfdb86d1e61a4f1b084ce
farnazmdi <farnazmd95@gmail.com>
2021-02-06 09:46:39 -0800

    bounds and lineages

--- lineage/states/stateCommon.py
+++ lineage/states/stateCommon.py
@@ -160,8 +160,8 @@ def gamma_estimator_atonce(gamma_obs, time_cen, gamas, x0=None):
     if np.allclose(np.dot(A, x0), 0.0):
         x0 = np.array([20.0, 2.0, 3.0, 4.0, 5.0])
 
-    linc = LinearConstraint(A, lb=np.zeros(3), ub=np.ones(3) * 100.0)
-    bnds = Bounds(lb=np.zeros_like(x0), ub=np.ones_like(x0) * 100.0)
+    linc = LinearConstraint(A, lb=np.zeros(3), ub=np.ones(3) * 100.0, keep_feasible=True)
+    bnds = Bounds(lb=np.zeros_like(x0), ub=np.ones_like(x0) * 100.0, keep_feasible=True)
 
     options = {'xtol': 1e-12, 'gtol': 1e-12}
     res = minimize(nLL_atonce, x0=x0, jac=nLL_atonceJ, args=arrgs, method="trust-constr", bounds=bnds, constraints=[linc], options=options)
