b4bb44f32628b0fb8692b1bc5cf370a07b4c4472
farnazmdi <farnazmd95@gmail.com>
2021-02-07 12:19:50 -0800

    figure 100 edit

--- lineage/figures/figure100.py
+++ lineage/figures/figure100.py
@@ -1,15 +1,21 @@
 """ This is a file to put together 4 conditions of lapatinib together. """
 
-from .figureCommon import getSetup, subplotLabel
+from .figureCommon import getSetup
+
+from string import ascii_lowercase
+
 
 
 def makeFigure():
     """
     Makes figure 100.
     """
-
+    titles = ["Control", "25 nM", "50 nM", "250 nM"]
     ax, f = getSetup((10, 7), (1, 4))
-    subplotLabel(ax)
+
     for i in range(4):
         ax[i].axis('off')
+        ax[i].text(-0.2, 1.25, ascii_lowercase[i], transform=ax[i].transAxes, fontsize=16, fontweight="bold", va="top")
+        ax[i].text(0.3, 1.25, titles[i], transform=ax[i].transAxes, fontsize=16, va="top")
+
     return f
