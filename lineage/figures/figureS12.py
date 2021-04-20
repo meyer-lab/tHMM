4e42d25dc3779d9f41250f5b1b57a17c80b2b0ee
farnazmdi <farnazmd95@gmail.com>
2021-04-20 09:21:46 -0700

    changes for the first issue

--- lineage/figures/figureS12.py
+++ lineage/figures/figureS12.py
@@ -8,8 +8,8 @@ def makeFigure():
     Makes figure 150.
     """
 
-    titles = ["Control", "gemcitabine 5 nM", "gemcitabine 10 nM", "gemcitabine 30 nM"]
-    ax, f = getSetup((10, 10), (1, 4))
+    titles = ["Control", "Gemcitabine 5 nM", "Gemcitabine 10 nM", "Gemcitabine 30 nM"]
+    ax, f = getSetup((10, 11), (1, 4))
     for i in range(4):
         ax[i].axis('off')
         ax[i].text(-0.2, 1.25, ascii_lowercase[i], transform=ax[i].transAxes, fontsize=16, fontweight="bold", va="top")
