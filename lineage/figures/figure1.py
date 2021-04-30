ebf481e738351e5a3375add661955da87c426daf
farnazmdi <farnazmd95@gmail.com>
2021-04-29 23:31:06 -0700

    figure 1

--- lineage/figures/figure1.py
+++ lineage/figures/figure1.py
@@ -17,7 +17,7 @@ def makeFigure():
     Makes figure 1.
     """
     # Get list of axis objects
-    ax, f = getSetup((7.6, 3.1), (11, 3))
+    ax, f = getSetup((7.6, 3.1), (8, 3))
     figureMaker(ax)
     ax[0].text(-0.2, 1.7, ascii_lowercase[0], transform=ax[0].transAxes, fontsize=16, fontweight="bold", va="top")
     ax[1].text(-0.2, 1.7, ascii_lowercase[1], transform=ax[1].transAxes, fontsize=16, fontweight="bold", va="top")
@@ -25,24 +25,23 @@ def makeFigure():
 
     return f
 
-
 def figureMaker(ax):
     """
     Makes figure 1.
     """
-    indxs_control = [random.randint(0, (len(control) - 1)) for _ in range(10)]
-    indxs_gem = [random.randint(0, (len(Gem10uM) - 1)) for _ in range(10)]
+    indxs_gem = [random.randint(0, (len(Gem10uM) - 1)) for _ in range(7)]
+    indxs_c1 = [0, 2, 6, 8, 9, 10, 24]
+    indxs_c2 = [1, 4, 5, 13, 17, 18, 19]
     # titles
-    ax[0].set_title("Control", fontsize=10)
-    ax[1].set_title("Gemcitabine 10 nM", fontsize=10)
-    ax[1].set_title("Control - random", fontsize=10)
+    ax[0].set_title("Control 1", fontsize=10)
+    ax[1].set_title("Control 2", fontsize=10)
+    ax[2].set_title("Gemcitabine 10 nM - random", fontsize=10)
     # lineages
-    for i in range(10):
-        control[indxs_control[i]].state = np.nan
+    for i in range(7):
         Gem10uM[indxs_gem[i]].state = np.nan
-        plotLineage(control[2 * i], ax[3 * i], censore=False, color=False)
-        plotLineage(Gem10uM[2 * i], ax[3 * i + 1], censore=False, color=False)
-        plotLineage(control[indxs_control[i]], ax[3 * i + 2], censore=False, color=False)
+        plotLineage(control[indxs_c1[i]], ax[3 * i], censore=False, color=False)
+        plotLineage(control[indxs_c2[i]], ax[3 * i + 1], censore=False, color=False)
+        plotLineage(Gem10uM[indxs_gem[i]], ax[3 * i + 2], censore=False, color=False)
 
-    for i in range(33):
+    for i in range(24):
         ax[i].axis('off')
