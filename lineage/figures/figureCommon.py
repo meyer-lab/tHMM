69df8cf59df889b02980f35d041e9650c00c4595
aarmey <aarmey@users.noreply.github.com>
2021-09-18 18:02:44 +0000

    autopep8 action fixes

--- lineage/figures/figureCommon.py
+++ lineage/figures/figureCommon.py
@@ -81,7 +81,7 @@ def getSetup(figsize, gridd):
                 "ytick.direction": "inout",
                 "ytick.major.width": 1. / 4,  # major tick width in points
                 "ytick.minor.width": 0.5 / 4,  # minor tick width in points
-                "svg.fonttype": "none" # Keep as text
+                "svg.fonttype": "none"  # Keep as text
                 },
         )
 
