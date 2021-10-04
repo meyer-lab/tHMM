792ce50e946ea6a8eaf28b6942c443bf2346891d
jclagarde <jclagarde01@gmail.com>
2021-10-03 22:05:48 -0700

    importing python type hints

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,8 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
