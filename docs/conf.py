82ab205c2b31d97d032ae9846031250a83c820fa
Farnaz Mohammadi <farnazmd95@gmail.com>
2021-10-04 23:47:50 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,6 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.autodoc',
-    'sphinx_autodoc_typehints'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
