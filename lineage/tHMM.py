627eedf34be74a0822cf6eacfabd2df980cb9777
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2021-10-18 15:08:29 -0700

    Update tHMM.py

--- lineage/tHMM.py
+++ lineage/tHMM.py
@@ -8,7 +8,7 @@ import scipy.stats as sp
 from .UpwardRecursion import get_Emission_Likelihoods
 from .BaumWelch import do_E_step, calculate_log_likelihood, do_M_step, do_M_E_step, do_M_E_step_atonce
 from .Viterbi import get_leaf_deltas, get_nonleaf_deltas, Viterbi
-from typing import TypeVar, Generic
+
 
 class estimate:
     """Estimation class.
@@ -44,9 +44,8 @@ class estimate:
         else:
             self.E = self.fE
 
-tHMMclass = TypeVar('tHMMclass')
 
-class tHMM(Generic[tHMMclass]):
+class tHMM:
     """Main tHMM class.
     """
 
