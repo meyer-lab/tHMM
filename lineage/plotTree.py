6e77d4eaf9aefcc07804095733dd2198da1e3055
jclagarde <jclagarde01@gmail.com>
2021-11-04 11:55:21 -0700

    changed back

--- lineage/plotTree.py
+++ lineage/plotTree.py
@@ -4,8 +4,8 @@ import numpy as np
 from Bio.Phylo.BaseTree import Clade
 import networkx as nx
 
-cs = ['lightblue', 'lightgreen', 'orange', 'red', 'purple', 'olive', 'gray']
-stateColors = ['blue', 'green', 'orange', 'red', 'purple', 'olive', 'gray']
+cs = ['lightblue', 'orange', 'lightgreen', 'red', 'purple', 'olive', 'gray']
+stateColors = ['blue', 'orange', 'green', 'red', 'purple', 'olive', 'gray']
 
 
 def CladeRecursive(cell, a: list, censor: bool, color: bool):
