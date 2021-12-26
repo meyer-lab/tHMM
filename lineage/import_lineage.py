e75b5aaff24e2c95a479ae0b638748c067c6823a
farnazm <farnazmd95@gmail.com>
2021-12-26 00:03:18 -0800

    slight improvement I suppose

--- lineage/import_lineage.py
+++ lineage/import_lineage.py
@@ -1,6 +1,7 @@
 """ This file includes functions to import the new lineage data. """
 import pandas as pd
 import numpy as np
+from .CellVar import CellVar as c
 
 def read_lineage_data():
     """ Reading the data and extracting lineages with their corresponding observations. """
@@ -12,35 +13,59 @@ def read_lineage_data():
 
         if not(lineage.empty): # if the lineage Id exists, do the rest, if not, pass
             unique_trackIDs = lineage["trackId"].unique() # the length of this shows the number of cells in this lineage
+            parent_trackIDs = lineage["parentTrackId"].unique() # self-explanatory!
 
             # the root parent cell
             parent_cell = c(parent=None, gen=1)
             parent_cell.obs = [0, 0, 0, 0] # [fate, lifetime, diameter, censored?]
-            parent_cell.obs[1] = 0.5*(np.max(lin1.loc[lin1['trackId'] == unique_trackIDs[0]]['frame']) - np.min(lin1.loc[lin1['trackId'] == unique_trackIDs[0]]['frame']))
-            parent_cell.obs[2] = np.mean(lin1.loc[lin1['trackId'] == unique_trackIDs[0]]['Diameter_0'])
+            parent_cell.obs[1] = 0.5*(np.max(lineage.loc[lineage['trackId'] == unique_trackIDs[0]]['frame']) - np.min(lineage.loc[lineage['trackId'] == unique_trackIDs[0]]['frame']))
+            parent_cell.obs[2] = np.mean(lineage.loc[lineage['trackId'] == unique_trackIDs[0]]['Diameter_0'])
             parent_cell.obs[3] = 1 # certainly left-censored.
 
+            lineage.drop(lineage.loc[lineage['trackId'] == unique_trackIDs[0]].index, inplace=True)
             ### we can add any other feature here...
             lineage_list = [parent_cell]
-            for val in unique_trackIDs[1:]: # leave out the very first element which belongs to the mother cell
-                daughter = c(parent=parent_cell, gen=parent_cell.gen+1)
-
-                #CELL FATE: TO BE FIGURED OUT...
-                daughter.obs = [0, 0, 0, 0]
-                daughter.obs[0] = 0 #? I don't know yet. If 0 means died, if 1 means divided, if nan means we don't know
-                daughter.obs[1] = 0.5*(np.max(lin1.loc[lin1['trackId'] == val]['frame']) - np.min(lin1.loc[lin1['trackId'] == val]['frame']))
-                daughter.obs[2] = np.mean(lin1.loc[lin1['trackId'] == val]['Diameter_0'])
-
-                if np.isnan(daughter.obs[0]):
-                    # TO BE DETERMINED...
-                    daughter.obs[3] = 1 # means censored, indeed!
-                else:
-                    daughter.obs[3] = 0 # means not censored.
-
-                lineage_list.append(daughter)
+            while not(lineage.empty):
+                daughter.left, lineage = recurssive(parent_cell, lineage)
+                daughter.right, lineage = recurssive(parent_cell, lineage)
+                lineage_list.append(daughter.left)
+                lineage_list.append(daughter.right)
                 parent_cell = daughter
-        else: # means the lineageId doesn't exist, so pass
-            pass
+
         population.append(lineage_list)
         
     return population
+
+def recurssive(parent_cell, lineage):
+
+    unique_trackIDs = lineage["trackId"].unique()
+    parent_trackIDs = lineage["parentTrackId"].unique()
+
+    if unique_trackIDs[0] in parent_trackIDs: # the cell of interest has divided
+        daughter = c(parent=parent_cell, gen=parent_cell.gen+1)
+
+        if len(unique_trackIDs) > 0: # only one of the daughters exist
+            # left daughter
+            daughter.left.obs = [0, 0, 0, 0]
+            daughter.left.obs[0] = 0 #? I don't know yet. If 0 means died, if 1 means divided, if nan means we don't know
+            daughter.left.obs[1] = 0.5*(np.max(lineage.loc[lineage['trackId'] == unique_trackIDs[0]]['frame']) - np.min(lineage.loc[lineage['trackId'] == unique_trackIDs[0]]['frame']))
+            daughter.left.obs[2] = np.mean(lineage.loc[lineage['trackId'] == unique_trackIDs[0]]['Diameter_0'])
+            daughter.left.obs[3] = 1 # TO BE DETERMINED
+            # remove the parent cell from the lineage and try recurssion
+            lineage.drop(lineage.loc[lineage['trackId'] == unique_trackIDs[0]].index, inplace=True)
+
+        if len(unique_trackIDs) > 1: # both daughters exist
+            # right daughter
+            daughter.right.obs = [0, 0, 0, 0]
+            daughter.right.obs[0] = 0 #? I don't know yet. If 0 means died, if 1 means divided, if nan means we don't know
+            daughter.right.obs[1] = 0.5*(np.max(lineage.loc[lineage['trackId'] == unique_trackIDs[1]]['frame']) - np.min(lineage.loc[lineage['trackId'] == unique_trackIDs[1]]['frame']))
+            daughter.right.obs[2] = np.mean(lineage.loc[lineage['trackId'] == unique_trackIDs[1]]['Diameter_0'])
+            daughter.right.obs[3] = 1 # TO BE DETERMINED
+            lineage.drop(lineage.loc[lineage['trackId'] == unique_trackIDs[1]].index, inplace=True)
+        else:
+            daughter.right = None
+
+    else: # the cell of interest has not divided
+        return # nada
+
+    return daughter, lineage
