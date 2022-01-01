c69b334d5210ddca1ace3b14e93829527ef6e5ec
farnazm <farnazmd95@gmail.com>
2021-12-31 17:04:05 -0800

    nearly done

--- lineage/import_lineage.py
+++ lineage/import_lineage.py
@@ -4,11 +4,27 @@ import itertools
 import numpy as np
 from .CellVar import CellVar as c
 
+############################
+# importing AU565 data (new)
+############################
 # path = "lineage/data/LineageData/AU02101_A3_field_1_RP_50_CSV-Table.csv"
+# df = pd.read_csv(path)
 
-def read_lineage_data(path):
-    """ Reading the data and extracting lineages with their corresponding observations. """
+#######################
+# importing MCF10A data
+#######################
+# partof_path = "lineage/data/MCF10A/"
+# df = prepare_MCF10A(path)
+
+def prepare_MCF10A(path):
     df = pd.read_csv(path)
+    # extract useful information
+    data = df[["TID", "lineage", "motherID", "generation", "treatment", "tmin", "average_velocity"]]
+    dd = data.rename(columns={'TID': 'trackId', 'lineage': 'lineageId', 'motherID': 'parentTrackId'})
+    return dd
+
+def read_lineage_data(df, cell_line: str):
+    """ Reading the data and extracting lineages and assigning their corresponding observations. """
 
     population = []
     # loop over "lineageId"s
@@ -24,27 +40,50 @@ def read_lineage_data(path):
             pid = [[0]] # root parent's parent id
             for j in unique_parent_trackIDs:
                 if j != 0:
-                    pid.append(np.count_nonzero(lineage["parentTrackId"]== j) * [j])
+                    pid.append(2 * [j])
             parent_ids = list(itertools.chain(*pid))
 
             # create the root parent cell and assign obsrvations
             parent_cell = c(parent=None, gen=1)
-            parent_cell = assign_observs(parent_cell, lineage, unique_cell_ids[0])
+            if cell_line == "MCF10A":
+                parent_cell = assign_observs_MCF10A(parent_cell, lineage, unique_cell_ids[0])
 
-            # create a list to store cells belonging to a lineage
-            lineage_list = [parent_cell]
-            for k, val in enumerate(unique_cell_ids):
-                if val in parent_ids: # if the id of a cell exists in the parent ids, it means the cell divides
-                    parent_index = [indx for indx, value in enumerate(parent_ids) if value == val] # find whose mother it is
-                    assert len(parent_index) == 2 # make sure has two children
-                    lineage_list[k].left = c(parent=lineage_list[k], gen=lineage_list[k].gen+1)
-                    lineage_list[k].left = assign_observs(lineage_list[k].left, lineage, unique_cell_ids[parent_index[0]])
-                    lineage_list[k].right = c(parent=lineage_list[k], gen=lineage_list[k].gen+1)
-                    lineage_list[k].right = assign_observs(lineage_list[k].right, lineage, unique_cell_ids[parent_index[1]])
+                # create a list to store cells belonging to a lineage
+                lineage_list = [parent_cell]
+                for k, val in enumerate(unique_cell_ids):
+                    if val in parent_ids: # if the id of a cell exists in the parent ids, it means the cell divides
+                        parent_index = [indx for indx, value in enumerate(parent_ids) if value == val] # find whose mother it is
+                        assert len(parent_index) == 2 # make sure has two children
+                        lineage_list[k].left = c(parent=lineage_list[k], gen=lineage_list[k].gen+1)
+                        lineage_list[k].left = assign_observs_MCF10A(lineage_list[k].left, lineage, unique_cell_ids[parent_index[0]])
+                        lineage_list[k].right = c(parent=lineage_list[k], gen=lineage_list[k].gen+1)
+                        lineage_list[k].right = assign_observs_MCF10A(lineage_list[k].right, lineage, unique_cell_ids[parent_index[1]])
 
-                    lineage_list.append(lineage_list[k].left)
-                    lineage_list.append(lineage_list[k].right)
+                        lineage_list.append(lineage_list[k].left)
+                        lineage_list.append(lineage_list[k].right)
 
+            elif cell_line == "AU565":
+                parent_cell = assign_observs_AU565(parent_cell, lineage, unique_cell_ids[0])
+
+                # create a list to store cells belonging to a lineage
+                lineage_list = [parent_cell]
+                for k, val in enumerate(unique_cell_ids):
+                    if val in parent_ids: # if the id of a cell exists in the parent ids, it means the cell divides
+                        parent_index = [indx for indx, value in enumerate(parent_ids) if value == val] # find whose mother it is
+                        assert len(parent_index) == 2 # make sure has two children
+                        lineage_list[k].left = c(parent=lineage_list[k], gen=lineage_list[k].gen+1)
+                        lineage_list[k].left = assign_observs_AU565(lineage_list[k].left, lineage, unique_cell_ids[parent_index[0]])
+                        lineage_list[k].right = c(parent=lineage_list[k], gen=lineage_list[k].gen+1)
+                        lineage_list[k].right = assign_observs_AU565(lineage_list[k].right, lineage, unique_cell_ids[parent_index[1]])
+
+                        lineage_list.append(lineage_list[k].left)
+                        lineage_list.append(lineage_list[k].right)
+
+            else:
+                raise ValueError('The cell line does not exist yet! Please choose between AU565 and MCF10A')
+
+        print(len(unique_cell_ids))
+        print(len(lineage_list))
         assert len(lineage_list) == len(unique_cell_ids)
         # if both observations are zero, remove the cell
         for n, cell in enumerate(lineage_list):
@@ -59,7 +98,7 @@ def read_lineage_data(path):
         
     return population
 
-def assign_observs(cell, lineage: list, uniq_id: int):
+def assign_observs_AU565(cell, lineage: list, uniq_id: int):
     """Given a cell, the lineage, and the unique id of the cell, it assigns the observations of that cell, and returns it."""
     # initialize
     cell.obs = [1, 0, 0, 0]
@@ -90,3 +129,79 @@ def assign_observs(cell, lineage: list, uniq_id: int):
         cell.obs[2] = np.mean(diam)
 
     return cell
+
+
+def assign_observs_MCF10A(cell, lineage: list, uniq_id: int):
+    """Given a cell, the lineage, and the unique id of the cell, it assigns the observations of that cell, and returns it."""
+    # initialize
+    cell.obs = [1, 0, 0, 0] # [fate, lifetime, avg_velocity, censored?]
+    parent_id = lineage["parentTrackId"].unique()
+    # cell fate: die = 0, divide = 1
+    if not(uniq_id in parent_id): # if the cell has not divided, means either died or reached experiment end time
+        if np.max(lineage.loc[lineage["trackId"] == uniq_id]["tmin"]) == 2880: # means reached end of experiment
+            cell.obs[0] = np.nan # don't know
+            cell.obs[3] = 1 # censored
+        else: # means cell died before experiment ended
+            cell.obs[0] = 0 # died
+            cell.obs[3] = 0 # not censored
+
+    if cell.gen == 1: # it is root parent
+        cell.obs[3] = 1 # meaning it is left censored in its lifetime
+
+    # cell's lifetime
+    cell.obs[1] = (np.max(lineage.loc[lineage['trackId'] == uniq_id]['tmin']) - np.min(lineage.loc[lineage['trackId'] == uniq_id]['tmin'])) / 60
+    # cell's velocity
+    cell.obs[2] = np.mean(lineage.loc[lineage['trackId'] == uniq_id]['average_velocity'])
+
+    return cell
+
+def MCF10A(condition:str):
+    """ Creates the population of lineages for each condition.
+    Conditions include: PBS, EGF-treated, HGF-treated, OSM-treated. """
+    if condition == "PBS":
+        d1 = prepare_MCF10A("lineage/data/MCF10A/PBS_1.csv")
+        data1 = read_lineage_data(d1, "MCF10A")
+        d2 = prepare_MCF10A("lineage/data/MCF10A/PBS_2.csv")
+        data2 = read_lineage_data(d2, "MCF10A")
+        return data1 + data2
+
+    elif condition == "EGF":
+        d1 = prepare_MCF10A("lineage/data/MCF10A/EGF_1.csv")
+        data1 = read_lineage_data(d1, "MCF10A")
+        d2 = prepare_MCF10A("lineage/data/MCF10A/EGF_2.csv")
+        data2 = read_lineage_data(d2, "MCF10A")
+        d3 = prepare_MCF10A("lineage/data/MCF10A/EGF_3.csv")
+        data3 = read_lineage_data(d3, "MCF10A")
+        return data1 + data2 + data3
+    
+    elif condition == "HGF":
+        d1 = prepare_MCF10A("lineage/data/MCF10A/HGF_1.csv")
+        data1 = read_lineage_data(d1, "MCF10A")
+        d2 = prepare_MCF10A("lineage/data/MCF10A/HGF_2.csv")
+        data2 = read_lineage_data(d2, "MCF10A")
+        d3 = prepare_MCF10A("lineage/data/MCF10A/HGF_3.csv")
+        data3 = read_lineage_data(d3, "MCF10A")
+        d4 = prepare_MCF10A("lineage/data/MCF10A/HGF_4.csv")
+        data4 = read_lineage_data(d4, "MCF10A")
+        d5 = prepare_MCF10A("lineage/data/MCF10A/HGF_5.csv")
+        data5 = read_lineage_data(d5, "MCF10A")
+        return data1 + data2 + data3 + data4 + data5
+
+    elif condition == "OSM":
+        d1 = prepare_MCF10A("lineage/data/MCF10A/OSM_1.csv")
+        data1 = read_lineage_data(d1, "MCF10A")
+        d2 = prepare_MCF10A("lineage/data/MCF10A/OSM_2.csv")
+        data2 = read_lineage_data(d2, "MCF10A")
+        d3 = prepare_MCF10A("lineage/data/MCF10A/OSM_3.csv")
+        data3 = read_lineage_data(d3, "MCF10A")
+        d4 = prepare_MCF10A("lineage/data/MCF10A/OSM_4.csv")
+        data4 = read_lineage_data(d4, "MCF10A")
+        d5 = prepare_MCF10A("lineage/data/MCF10A/OSM_5.csv")
+        data5 = read_lineage_data(d5, "MCF10A")
+        d5 = prepare_MCF10A("lineage/data/MCF10A/OSM_6.csv")
+        data6 = read_lineage_data(d5, "MCF10A")
+        return data1 + data2 + data3 + data4 + data5 + data6
+    
+    else:
+        raise ValueError("condition does not exist. choose between [PBS, EGF, HGF, OSM]")
+
