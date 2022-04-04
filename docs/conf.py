0609249360d8fa34eb8f0925e410d8dd913a180e
farnazm <farnazmd95@gmail.com>
2022-04-04 16:58:53 -0700

    not sure if this works

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,9 @@ templates_path = ['_templates']
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = 'python'
-autoapi_generate_api_docs = False
+
+# autoapi_generate_api_docs = False
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
