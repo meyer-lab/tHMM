aaa12c0da5536808170196b082e1b50fd756ac5e
farnazm <farnazmd95@gmail.com>
2022-04-22 19:19:14 -0700

    didnt throw error this timegit add -A

--- lineage/figures/common.py
+++ lineage/figures/common.py
@@ -328,15 +328,10 @@ def commonAnalyze(list_of_populations, num_states, xtype="length", **kwargs):
     list_of_fpi = kwargs.get("list_of_fpi", [None] * len(list_of_populations))
     list_of_fT = kwargs.get("list_of_fT", [None] * len(list_of_populations))
     list_of_fE = kwargs.get("list_of_fE", [None] * len(list_of_populations))
-    if num_states == 2:
-        predicted_num_states = kwargs.get("predicted_num_states", 2)
-    elif num_states == 3:
-        predicted_num_states = kwargs.get("predicted_num_states", 3)
-    elif num_states == 4:
-        predicted_num_states = kwargs.get("predicted_num_states", 4)
+
     parallel = kwargs.get("parallel", True)
     # Analyzing the lineages in the list of populations (parallelized function)
-    output = run_Analyze_over(list_of_populations, predicted_num_states, parallel=parallel,
+    output = run_Analyze_over(list_of_populations, num_states, parallel=parallel,
                               list_of_fpi=list_of_fpi, list_of_fT=list_of_fT, list_of_fE=list_of_fE)
 
     # Collecting the results of analyzing the lineages
