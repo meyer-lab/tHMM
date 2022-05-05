1c8e064370fd37784a584defd4a5a60c56387109
Farnaz Mohammadi <farnazmd95@gmail.com>
2022-05-05 13:37:26 -0700

    Update test_BaumWelch.py

--- lineage/tests/test_BaumWelch.py
+++ lineage/tests/test_BaumWelch.py
@@ -2,7 +2,7 @@
 import pytest
 import numpy as np
 from sklearn.metrics import rand_score
-from ..BaumWelch import do_E_step, calculate_log_likelihood
+from ..BaumWelch import do_E_step, do_M_E_step, calculate_log_likelihood
 from ..LineageTree import LineageTree
 from ..tHMM import tHMM
 from ..figures.common import pi, T, E
@@ -43,3 +43,20 @@ def test_E_step(cens):
     true_states = [cell.state for cell in tHMMobj.X[0].output_lineage]
 
     assert rand_score(true_states, pred_states[0]) >= 0.95
+
+@pytest.mark.parametrize("cens", [0, 2])
+def test_M_step(cens):
+    X = LineageTree.init_from_parameters(pi, T, E, desired_num_cells=(2 ** 10) - 1, desired_experimental_time=400, censor_condition=cens)
+    tHMMobj = tHMM([X], num_states=2) 
+    gammas = [np.zeros((len(lineage.output_lineage), tHMMobj.num_states)) for lineage in tHMMobj.X]
+
+    # create the gamma matrix (N x K) that shows the probability of a cell n being in state k from the true state assignments.
+    for idx, g_lin in enumerate(gammas):
+        for i in range(g_lin.shape[0]):
+            g_lin[i, tHMMobj.X[idx].output_lineage[i].state] = 1
+
+    do_M_E_step(tHMMobj, gammas)
+    # test bernoulli
+    np.testing.assert_allclose(tHMMobj.estimate.E[0].params[0], E[0].params[0], rtol=0.1)
+    # gamma parameters
+    np.testing.assert_allclose(tHMMobj.estimate.E[0].params[1:], E[0].params[1:], rtol=0.5)
