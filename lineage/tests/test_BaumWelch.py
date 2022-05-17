81f292810292fe6952f7cf9f527406ee070a627f
Farnaz Mohammadi <farnazmdi@ucla.edu>
2022-05-16 21:33:30 -0700

    fixed

--- lineage/tests/test_BaumWelch.py
+++ lineage/tests/test_BaumWelch.py
@@ -3,7 +3,8 @@ import pytest
 import numpy as np
 import pickle
 from sklearn.metrics import rand_score
-from ..BaumWelch import do_E_step, do_M_E_step, calculate_log_likelihood
+from ..states.StateDistributionGaPhs import StateDistribution as phaseStateDist
+from ..BaumWelch import do_E_step, do_M_E_step, calculate_log_likelihood, calculate_stationary
 from ..LineageTree import LineageTree
 from ..tHMM import tHMM
 from ..figures.common import pi, T, E, E2
@@ -43,24 +44,35 @@ pi3 = gmc[1].estimate.pi
 @pytest.mark.parametrize("cens", [0, 3])
 def test_E_step(cens):
     """ This tests that given the true model parameters, can it estimate the states correctly."""
+    T = np.array([[0.6, 0.1, 0.1, 0.1, 0.1], [0.05, 0.8, 0.05, 0.05, 0.05], [0.01, 0.1, 0.7, 0.09, 0.1], [0.1, 0.1, 0.05, 0.7, 0.05], [0.1, 0.1, 0.05, 0.05, 0.7]], dtype=float)
+
+    # pi: the initial probability vector
+    pi = calculate_stationary(T)
+
+    state0 = phaseStateDist(0.99, 0.95, 50, 0.2, 100, 0.1)
+    state1 = phaseStateDist(0.95, 0.9, 75, 0.2, 150, 0.1)
+    state2 = phaseStateDist(0.9, 0.85, 100, 0.2, 200, 0.1)
+    state3 = phaseStateDist(0.92, 0.95, 150, 0.2, 250, 0.1)
+    state4 = phaseStateDist(0.99, 0.85, 200, 0.2, 300, 0.1)
+    E = [state0, state1, state2, state3, state4]
     population = []
     for _ in range(200):
         # make sure we have enough cells in the lineage.
-        X = LineageTree.init_from_parameters(pi3, T3, E3, desired_num_cells=(2 ** 9) - 1, desired_experimental_time=300, censor_condition=cens)
+        X = LineageTree.init_from_parameters(pi, T, E, desired_num_cells=(2 ** 6) - 1, desired_experimental_time=150, censor_condition=cens)
         while len(X.output_lineage) < 5:
-            X = LineageTree.init_from_parameters(pi3, T3, E3, desired_num_cells=(2 ** 9) - 1, desired_experimental_time=300, censor_condition=cens)
+            X = LineageTree.init_from_parameters(pi, T, E, desired_num_cells=(2 ** 6) - 1, desired_experimental_time=150, censor_condition=cens)
         population.append(X)
 
     tHMMobj = tHMM(population, num_states=gmc[1].num_states)  # build the tHMM class with X
-    tHMMobj.estimate.pi = pi3
-    tHMMobj.estimate.T = T3
-    tHMMobj.estimate.E = E3
+    tHMMobj.estimate.pi = pi
+    tHMMobj.estimate.T = T
+    tHMMobj.estimate.E = E
 
     do_E_step(tHMMobj)
     pred_states = tHMMobj.predict()
     true_states = [cell.state for cell in tHMMobj.X[0].output_lineage]
 
-    assert rand_score(true_states, pred_states[0]) >= 0.85
+    assert rand_score(true_states, pred_states[0]) >= 0.9
 
 
 @pytest.mark.parametrize("cens", [0, 3])
