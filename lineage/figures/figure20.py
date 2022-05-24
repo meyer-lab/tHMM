c1655a28759ba0f2b0cd9d03932de4eeac953f72
Farnaz Mohammadi <farnazmdi@ucla.edu>
2022-05-23 21:49:54 -0700

    dataframe censored

--- lineage/figures/figure20.py
+++ lineage/figures/figure20.py
@@ -69,44 +69,60 @@ def makeFigure():
 def get_lifetime_gens(population, obs_ix):
 
     first_gens = []
+    cens_1 =[]
     second_gens = []
+    cens_2 = []
     third_gens = []
+    cens_3 = []
     forth_gens = []
+    cens_4 = []
     fifth_gens = []
+    cens_5 = []
     for lineage in population:
         gens = sorted({cell.gen for cell in lineage.output_lineage})  # appending the generation of cells in the lineage
         for gen in gens:
             level = [cell.obs[obs_ix] for cell in lineage.output_lineage if (cell.gen == gen and cell.observed)]
+            cens = [cell.obs[obs_ix+2] for cell in lineage.output_lineage if (cell.gen == gen and cell.observed)]
             if gen == 1:
                 first_gens.append(level)
+                cens_1.append(cens)
             elif gen == 2:
                 second_gens.append(level)
+                cens_2.append(cens)
             elif gen == 3:
                 third_gens.append(level)
+                cens_3.append(cens)
             elif gen == 4:
                 forth_gens.append(level)
+                cens_4.append(cens)
             else:
                 fifth_gens.append(level)
+                cens_5.append(cens)
 
-    return list(it.chain(*first_gens)), list(it.chain(*second_gens)), list(it.chain(*third_gens)), list(it.chain(*forth_gens)), list(it.chain(*fifth_gens))
+    return [list(it.chain(*first_gens)), list(it.chain(*second_gens)), list(it.chain(*third_gens)), list(it.chain(*forth_gens)), list(it.chain(*fifth_gens))], [list(it.chain(*cens_1)), list(it.chain(*cens_2)), list(it.chain(*cens_3)), list(it.chain(*cens_4)), list(it.chain(*cens_5))]
 
 
-def corr(all_gens, degree):
+def corr(all_gens, all_cens, degree):
     """ To calculate the correlation between mother-daughter cells, it creates the second array with repeated values the same size as the first.
     degree determines whether it is between mother-daughter cells, or between grandmother-daughter cells, or higher.
     """
     array1 = []
+    cens1 = []
     for ix1, gen in enumerate(all_gens[degree:]):
         array1.append(np.repeat(all_gens[ix1], 2 ** degree))
+        cens1.append(np.repeat(all_cens[ix1], 2 ** degree))
 
     arr2 = []
+    cens2 = []
     for i in range(degree, len(all_gens)):
         arr2 += all_gens[i]
+        cens2 += all_cens[i]
 
-    arr1 = list(it.chain(*array1))
+    arr1 = list(it.chain(*array1[1:]))
+    c1 = list(it.chain(*cens1[1:]))
     assert(len(arr1) == len(arr2))
 
-    df = pd.DataFrame({"gen1" : arr1, "gen2" : arr2})
+    df = pd.DataFrame({"gen1" : arr1, "cen1": c1, "gen2" : arr2, "cen2": cens2})
 
     return sp.spearmanr(arr1, arr2).correlation, df
 
@@ -120,31 +136,36 @@ def repeat_corr(drug, ix):
     for i in range(4):
         populations += [LineageTree.init_from_parameters(drug[i].estimate.pi, drug[i].estimate.T, drug[i].estimate.E, 2 ** num_gens - 1) for _ in range(num_trials)]
 
-    all_gens = get_lifetime_gens(populations, ix)
+    all_gens, cen = get_lifetime_gens(populations, ix)
 
-    return corr(all_gens, 1)[0], corr(all_gens, 2)[0], corr(all_gens, 3)[0]
+    return corr(all_gens[0], cen[0], 1)[0], corr(all_gens[0], cen[0], 2)[0], corr(all_gens[0], cen[0], 3)[0]
 
-def save_df(drug):
+def save_df():
     """ Save the arrays that are used for calculating the correlation into dataframes, in the form of column1:gen1, column2: gen2. 
     This functions does this for gen1 & 2, gen 1 & 3, gen 1 & 4, and for both G1 and S-G2 cell lifetimes. """
-    populations = []
+    pik1 = open("lapatinibs.pkl", "rb")
+    lapt_tHMMobj_list = []
     for i in range(4):
-        populations += [LineageTree.init_from_parameters(drug[i].estimate.pi, drug[i].estimate.T, drug[i].estimate.E, 2 ** num_gens - 1) for _ in range(num_trials)]
+        lapt_tHMMobj_list.append(pickle.load(pik1))
+
+    populations = []
+    for tHMMobj in lapt_tHMMobj_list:
+        populations += tHMMobj.X
 
     # G1
-    all_gens1 = get_lifetime_gens(populations, 2)
-    df1 = corr(all_gens1, 1)[1]
-    df2 = corr(all_gens1, 2)[1]
-    df3 = corr(all_gens1, 3)[1]
+    all_gens1, all_cens1 = get_lifetime_gens(populations, 2)
+    df1 = corr(all_gens1, all_cens1, 1)[1]
+    df2 = corr(all_gens1[0], all_cens1[0], 2)[1]
+    df3 = corr(all_gens1[0], all_cens1[0], 3)[1]
     df1.to_csv(r'df_g1_gen12.csv', index=False)
     df2.to_csv(r'df_g1_gen13.csv', index=False)
     df3.to_csv(r'df_g1_gen14.csv', index=False)
 
     # S-G2
-    all_gens2 = get_lifetime_gens(populations, 3)
-    df11 = corr(all_gens2, 1)[1]
-    df12 = corr(all_gens2, 2)[1]
-    df13 = corr(all_gens2, 3)[1]
+    all_gens2, all_cens2 = get_lifetime_gens(populations, 3)
+    df11 = corr(all_gens2[0], all_cens2[0], 1)[1]
+    df12 = corr(all_gens2[0], all_cens2[0], 2)[1]
+    df13 = corr(all_gens2[0], all_cens2[0], 3)[1]
     df11.to_csv(r'df_g2_gen12.csv', index=False)
     df12.to_csv(r'df_g2_gen13.csv', index=False)
     df13.to_csv(r'df_g2_gen14.csv', index=False)
