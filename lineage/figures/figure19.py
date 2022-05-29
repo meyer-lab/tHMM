9607f85477a234765766eb735c39d1199e3d7496
Farnaz Mohammadi <farnazmdi@ucla.edu>
2022-05-29 11:38:57 -0700

    edits

--- lineage/figures/figure19.py
+++ lineage/figures/figure19.py
@@ -19,7 +19,7 @@ pi = calculate_stationary(T)
 
 # bern, gamma_a, gamma_scale
 state0 = StateDistribution(0.99, 100, 0.1)
-state1 = StateDistribution(0.75, 80, 0.5)
+state1 = StateDistribution(0.99, 80, 0.5)
 E = [state0, state1]
 
 def makeFigure():
@@ -28,51 +28,24 @@ def makeFigure():
     """
     ax, f = getSetup((4, 4), (1, 1))
 
-    likelihoods = np.zeros((7, 10))
-    for i in range(10):
-        likelihoods[:, i] = all_cv()
+    # create a population
+    complete_lineages = [LineageTree.init_from_parameters(pi, T, E, 127) for _ in range(50)]
 
-    ax[0].scatter(desired_num_states, likelihoods)
-
-    return f
-
-def all_cv():
-    """ find out the likelihoods for various masking percentages."""
-    likelihood = []
-    for i in desired_num_states:
-        likelihood.append(cv(i))
-
-        print("likelihood for states ", likelihood)
-    return likelihood
-
-def cv(num_states):
-    """Simplest case of cross validation."""
-
-    # create a lineage
-    complete_lineages = [LineageTree.init_from_parameters(pi, T, E, 31) for _ in range(10)]
-    true_states_by_lineage = [[cell.state for cell in complete_lineage.output_lineage] for complete_lineage in complete_lineages]
-
-    # hide some percentage of observations
-    lineages, indexes = [], []
+    # create training data by hiding 20% of cells in each lineage
+    train_lineages, hidden_indexes, hidden_obs = [], [], []
     for complete_lin in complete_lineages:
-        lineage, hide_index = hide_observation(complete_lin, 0.1)
-        lineages.append(lineage)
-        indexes.append(hide_index)
+        lineage, hide_index, hide_obs = hide_observation(complete_lin, 0.2)
+        train_lineages.append(lineage)
+        hidden_indexes.append(hide_index)
+        hidden_obs.append(hide_obs)
 
-    # fit the not-hidden data from the lineages to the model
-    tHMMobj_list, LL = Analyze_list([lineages], num_states)
+    ll = []
+    for i in desired_num_states:
+        ll.append(crossval(train_lineages, hidden_indexes, hidden_obs, i))
+    print(ll)
 
-    # assign the predicted states to each cell
-    states_list = [tHMMobj.predict() for tHMMobj in tHMMobj_list]
-
-    for idx, tHMMobj in enumerate(tHMMobj_list):
-        for lin_indx, lin in enumerate(tHMMobj.X):
-            for cell_indx, cell in enumerate(lin.output_lineage):
-                cell.state = states_list[idx][lin_indx][cell_indx]
-
-    # print the total likelihood of the observations to the assigned compared to the other state
-    lls = calc_likelihood(tHMMobj_list[0], complete_lineages, indexes)[0]
-    return lls
+    ax[0].plot(desired_num_states, ll)
+    return f
 
 
 def hide_observation(lineage, percentage):
@@ -81,34 +54,39 @@ def hide_observation(lineage, percentage):
 
     new_lineage = deepcopy(lineage)
     num_cells = len(lineage.output_lineage)
-    hide_index = sp.multinomial.rvs(n=int(percentage * num_cells), p=[1 / num_cells]*num_cells, size=1)[0]
+    # create the indexes for hidden observations
+    hide_index = np.zeros(num_cells)
+    hide_index[:int(num_cells*percentage)] = 1
+    np.random.shuffle(hide_index)
 
+    obss = []
     for ix, cell in enumerate(new_lineage.output_lineage):
         if hide_index[ix] == 1: # means we hide the cell lifetime
+            obss.append(cell.obs)
             cell.obs = -1 * np.ones(len(cell.obs))
 
-    return new_lineage, hide_index
+    return new_lineage, hide_index, obss
 
 
-def calc_likelihood(tHMMobj, complete_lineages, hidden_index):
-    """ Calculates the likelihood of cell's observation to the state it is assigned to, and sums over all for each lineage. """
+def crossval(train_lineages, hidden_indexes, hidden_obs, num_states):
 
-    # find the list of hidden observations with the same order find their corresponding estimated state
-    hidden_obs = []
+    # fit training data
+    tHMMobj_list, LL = Analyze_list([train_lineages], num_states)
+
+    # predict states of hidden cells
+    states_list = tHMMobj_list[0].predict()
+
+    # hidden states
     hidden_states = []
-    for i, complete_lin in enumerate(complete_lineages):
-        tmp1, tmp2 = [], []
-        for ix, cell in enumerate(complete_lin.output_lineage):
-            if hidden_index[i][ix] == 1:
-                tmp1.append(cell.obs)
-                tmp2.append(tHMMobj.X[i].output_lineage[ix].state)
-            hidden_obs.append(tmp1)
-            hidden_states.append(tmp2)
+    for i, st in enumerate(states_list):
+        tmp1 = []
+        for ii, c_st in enumerate(st):
+            if hidden_indexes[i][ii] == 1:
+                tmp1.append(c_st)
+            hidden_states.append(tmp1)
 
-    # calculate the likelihood of the observation, to the state it is assigned to
     Ls = 0
     for i, hid in enumerate(hidden_obs):
         for ix, obs in enumerate(hid):
-            Ls -= np.log(tHMMobj.estimate.E[hidden_states[i][ix]].pdf(np.array(obs)[np.newaxis, :]))
-
+            Ls += tHMMobj_list[0].estimate.E[hidden_states[i][ix]].pdf(np.array(hidden_obs[i][ix])[np.newaxis, :])
     return Ls
