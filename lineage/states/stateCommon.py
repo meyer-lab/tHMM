8bdd7e5920aa368e85f88831038b9238e010dbef
Farnaz Mohammadi <farnazmd95@gmail.com>
2022-05-31 20:57:29 -0700

    any different?

--- lineage/states/stateCommon.py
+++ lineage/states/stateCommon.py
@@ -72,7 +72,7 @@ def gamma_estimator(gamma_obs: list[np.ndarray], time_cen: list[np.ndarray], gam
         A = np.zeros((3, 5))  # is a matrix that contains the constraints. the number of rows shows the number of linear constraints.
         np.fill_diagonal(A[:, 1:], -1.0)
         np.fill_diagonal(A[:, 2:], 1.0)
-        linc = [LinearConstraint(A, lb=np.zeros(3), ub=np.full(3, np.inf))]
+        linc = [LinearConstraint(A, lb=np.zeros(3), ub=np.full(3, 100))]
         if np.allclose(np.dot(A, x0), 0.0):
             x0 = np.array([200.0, 0.2, 0.4, 0.6, 0.8])
     else:
