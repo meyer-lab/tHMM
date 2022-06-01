Fixed underflow issue

lineage/states/stateCommon.py
@@ -2,11 +2,7 @@
 
 import numpy as np
 from scipy.stats import gamma
-from scipy.optimize import minimize, LinearConstraint
-
-
-def softplus(x):
-    return np.log1p(np.exp(-np.abs(x))) + np.maximum(x, 0)
+from scipy.optimize import minimize, LinearConstraint, Bounds
 
 
 def basic_censor(cell):
@@ -31,23 +27,20 @@ def basic_censor(cell):
 
 def nLL_atonce(x: np.ndarray, gamma_obs: list[np.ndarray], time_cen: list[np.ndarray], gammas: list[np.ndarray]):
     """ uses the nLL_atonce and passes the vector of scales and the shared shape parameter. """
-    xx = softplus(x)
+    assert np.all(x >= 0.0)
     outt = 0.0
     for i in range(len(x) - 1):
-        outt -= np.dot(gammas[i] * time_cen[i], gamma.logpdf(gamma_obs[i], a=xx[0], scale=xx[i + 1]))
-        outt -= np.dot(gammas[i] * (1 - time_cen[i]), gamma.logsf(gamma_obs[i], a=xx[0], scale=xx[i + 1]))
+        outt -= np.dot(gammas[i] * time_cen[i], gamma.logpdf(gamma_obs[i], a=x[0], scale=x[i + 1]))
+
+        # Log is prone to underflow, so index out values that don't matter
+        with np.errstate(invalid='ignore'):
+            llsf = gammas[i] * gamma.logsf(gamma_obs[i], a=x[0], scale=x[i + 1])
+        outt -= np.sum(llsf[time_cen[i] == 0.0])
 
     return outt
 
 
-def softplus_inv(x):
-    x = np.clip(x, -np.inf, 1000.0)
-    x = x.astype(np.float128)
-    x = np.log(np.exp(x) - 1.0)
-    return x.astype(float)
-
-
-def gamma_estimator(gamma_obs: list[np.ndarray], time_cen: list[np.ndarray], gammas: list[np.ndarray], x0=None):
+def gamma_estimator(gamma_obs: list[np.ndarray], time_cen: list[np.ndarray], gammas: list[np.ndarray], x0: np.ndarray):
     """
     This is a weighted, closed-form estimator for two parameters
     of the Gamma distribution for estimating shared shape and separate scale parameters of several drug concentrations at once.
@@ -65,19 +58,21 @@ def gamma_estimator(gamma_obs: list[np.ndarray], time_cen: list[np.ndarray], gam
 
     arrgs = (gamma_obs, time_cen, gammas)
 
-    if x0 is None:
-        x0 = np.array([200.0, 0.2, 0.4, 0.6, 0.8])
-
     if len(gamma_obs) == 4:  # for constrained optimization
         A = np.zeros((3, 5))  # is a matrix that contains the constraints. the number of rows shows the number of linear constraints.
         np.fill_diagonal(A[:, 1:], -1.0)
         np.fill_diagonal(A[:, 2:], 1.0)
-        linc = [LinearConstraint(A, lb=np.zeros(3), ub=np.full(3, 100))]
+        linc = [LinearConstraint(A, lb=np.zeros(3), ub=np.full(3, np.inf))]
         if np.allclose(np.dot(A, x0), 0.0):
             x0 = np.array([200.0, 0.2, 0.4, 0.6, 0.8])
     else:
         linc = list()
 
-    res = minimize(nLL_atonce, x0=softplus_inv(x0), args=arrgs, method="trust-constr", constraints=linc)
+    bnd = Bounds(np.full_like(x0, 0.01), np.full_like(x0, 400.0), keep_feasible=True)
+    opt = {"xtol": 1e-9}
+
+    with np.errstate(all='raise'):
+        res = minimize(nLL_atonce, x0=x0, args=arrgs, bounds=bnd, method="trust-constr", constraints=linc, options=opt)
+
     assert res.success or ("maximum number of function evaluations is exceeded" in res.message)
-    return softplus(res.x)
+    return res.x