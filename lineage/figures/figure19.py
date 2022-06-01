8d96c14a1e025ea73a7eda10417b5c9ec4c818c4
Farnaz Mohammadi <farnazmdi@ucla.edu>
2022-06-01 10:17:51 -0700

    edits

--- lineage/figures/figure19.py
+++ lineage/figures/figure19.py
@@ -1,12 +1,15 @@
 """ To plot a summary of cross validation. """
 from .common import getSetup
+from concurrent.futures import ProcessPoolExecutor
 import numpy as np
 from ..LineageTree import LineageTree
 from ..figures.common import pi, T, E
 from ..CrossVal import hide_for_population, crossval
 from ..Lineage_collections import Gemcitabine_Control, Gem5uM, Gem10uM, Gem30uM, Lapatinib_Control, Lapt25uM, Lapt50uM, Lap250uM
 
-desired_num_states = np.arange(1, 8)
+desired_num_states = np.arange(1, 5)
+
+exe = ProcessPoolExecutor()
 
 def makeFigure():
     """
@@ -14,24 +17,25 @@ def makeFigure():
     """
     ax, f = getSetup((8, 4), (1, 2))
 
-    complete_population = [Lapatinib_Control + Gemcitabine_Control, Lapt25uM, Lapt50uM, Lap250uM]
+    complete_population = [[Lapatinib_Control + Gemcitabine_Control, Lapt25uM, Lapt50uM, Lap250uM], [Gemcitabine_Control + Lapatinib_Control, Gem5uM, Gem10uM, Gem30uM]]
+
+    prom_holder = []
+    for idx, population in enumerate(complete_population):
+        prom_holder.append(exe.submit(parallel_LL, population))
+
+    output = [prom.result() for prom in prom_holder]
+
+    ax[0].plot(desired_num_states, output[0])
+    ax[0].set_title("lapatinib")
+    ax[1].plot(desired_num_states, output[1])
+    ax[1].set_title("gemcitabine")
+    return f
+
+def parallel_LL(complete_population):
     # create training data by hiding 20% of cells in each lineage
-    train_population, hidden_indexes, hidden_obs = hide_for_population(complete_population, 0.2)
+    train_population, hidden_indexes, hidden_obs = hide_for_population(complete_population, 0.25)
 
     ll = []
     for i in desired_num_states:
         ll.append(crossval(train_population, hidden_indexes, hidden_obs, i))
-
-    complete_population = [Gemcitabine_Control + Lapatinib_Control, Gem5uM, Gem10uM, Gem30uM]
-    # create training data by hiding 20% of cells in each lineage
-    train_population, hidden_indexes, hidden_obs = hide_for_population(complete_population, 0.2)
-
-    ll2 = []
-    for i in desired_num_states:
-        ll2.append(crossval(train_population, hidden_indexes, hidden_obs, i))
-
-    ax[0].plot(desired_num_states, ll)
-    ax[0].set_title("lapatinib")
-    ax[1].plot(desired_num_states, ll2)
-    ax[1].set_title("gemcitabine")
-    return f
+    return ll
