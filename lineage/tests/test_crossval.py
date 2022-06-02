8715e94095e24dec4399f9bd413b21e3954c8391
Farnaz Mohammadi <farnazmd95@gmail.com>
2022-06-02 10:34:58 -0700

    Update test_crossval.py

--- lineage/tests/test_crossval.py
+++ lineage/tests/test_crossval.py
@@ -7,7 +7,7 @@ from ..CrossVal import crossval, hide_for_population
 @pytest.mark.parametrize("censored", [0, 3])
 @pytest.mark.parametrize("num_cells", [7, 15])
 def test_cv(censored, num_cells):
-    complete_lineages = [[LineageTree.init_from_parameters(pi, T, E, num_cells, censor_condition=censored, desired_experiment_time=150) for _ in range(10)] for _ in range(4)]
+    complete_lineages = [[LineageTree.init_from_parameters(pi, T, E, num_cells, censor_condition=censored, desired_experiment_time=150) for _ in range(20)] for _ in range(4)]
 
     train_lineages, hidden_indexes, hidden_obs = hide_for_population(complete_lineages, 0.2)
 
