af872532acf4c82af1283e4ba85f34a86ae3ec72
Aaron Meyer <ameyer@ucla.edu>
2022-06-09 07:19:54 -0700

    Clean up

--- lineage/crossval.py
+++ lineage/crossval.py
@@ -2,16 +2,14 @@
 from scipy.stats import bernoulli
 from copy import deepcopy
 
+
 def hide_observation(lineages: list, percentage: float) -> list:
     """ Taking a list of lineages and the percentage of cells want to be masked, it marks those x% negative."""
-
     new_lineages = deepcopy(lineages)
-    for i, new_lineage in enumerate(new_lineages):
-        for ix, cell in enumerate(new_lineage.output_lineage):
-
-            bern = bernoulli.rvs(p=percentage, size=1)
-            if bern == 1:
-                # negate the cell observations at those specific indexes
+    for new_lineage in new_lineages:
+        for cell in new_lineage.output_lineage:
+            if bernoulli.rvs(p=percentage, size=1):
+                # negate the cell observations to mask them
                 cell.obs = [-1 * o for o in cell.obs]
 
     return new_lineages
