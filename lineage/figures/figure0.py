1d2dd1f724520b41b4af54324f4fe9e017bd44e3
Farnaz Mohammadi <farnazmdi@ucla.edu>
2023-06-20 11:25:54 -0700

    some more edits

--- lineage/figures/figure0.py
+++ lineage/figures/figure0.py
@@ -17,12 +17,14 @@ def makeFigure():
     c2 = AllLapatinib[2]
     c3 = AllLapatinib[3]
 
-    ts = np.arange(192, step=1)
+    ts = np.arange(96, step=0.5)
 
     Gs = leaves_to_root(control[0], ts)
     for lineage in control[1:]:
         Gs += leaves_to_root(lineage, ts)
-    ax[0].plot(Gs)
+    ax[0].plot(Gs[:, 0], label="G1")
+    ax[0].plot(Gs[:, 1], label="SG2")
+    ax[0].legend()
 
     Gs1 = leaves_to_root(c1[0], ts)
     for lineage in c1[1:]:
@@ -49,24 +51,27 @@ def leaves_to_root(lin, ts):
 
     t_cur = 0.0
 
-    # randomly choose one of leaf cells and traverse back to roots.
+    # randomly choose one of leaf cells.
     leaves_idx = get_leaves_idx(lin.output_lineage)
     lineage = lin.output_lineage
     random_leaf_idx = random.choice(leaves_idx)
+
     cells = [lineage[random_leaf_idx]]
 
+    # traverse back to root from the randomly chosen leaf
     for c in cells:
         if c.parent is not None:
             cells.append(c.parent)
 
-    cells.reverse()
+    cells.reverse() # order from root to leaf
+
     for ii, cell in enumerate(cells): # the number of generations
         for phase in range(2):
             bern_obs = cell.obs[phase]
-            gamma_obs = cell.obs[phase+2]
+            gamma_obs = np.nan_to_num(cell.obs[phase+2])
             idx = (ts > t_cur) & (ts < t_cur + gamma_obs)
 
-            counts[idx, phase] += 2 ** ii
+            counts[idx, phase] += 2 ** (ii+1)
             t_cur += gamma_obs
 
             if t_cur > ts[-1]:
